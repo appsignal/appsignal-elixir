8ef8c7558227f76475616e4da7603047e7251601
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-08-16 16:52:00 +0200

    Record ecto.query events for running transactions

--- lib/appsignal/ecto.ex
+++ lib/appsignal/ecto.ex
@@ -13,12 +13,21 @@ defmodule Appsignal.Ecto do
 
   require Logger
 
-  @micro_seconds :erlang.convert_time_unit(1, :micro_seconds, :native)
+  alias Appsignal.{Transaction, TransactionRegistry}
 
+  @nano_seconds :erlang.convert_time_unit(1, :nano_seconds, :native)
 
   def log(entry) do
-    t = entry.query_time / @micro_seconds
-    Logger.info "Ecto log: #{entry.query}, #{t}"
+    # See if we have a transaction registered for the current process
+    case TransactionRegistry.lookup(self) do
+      nil ->
+        # skip
+        :ok
+      %Transaction{} = transaction ->
+        # record the event
+        duration = trunc((entry.queue_time + entry.query_time + entry.decode_time) / @nano_seconds)
+        Transaction.record_event(transaction, "ecto.query", "", entry.query, duration, 1)
+    end
     entry
   end
 
