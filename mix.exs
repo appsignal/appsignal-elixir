c0f8e285a8de5c64324d469fd2711d5d332c8548
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-08-16 17:02:03 +0200

    Release 0.0.9

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Appsignal.Mixfile do
 
   def project do
     [app: :appsignal,
-     version: "0.0.8",
+     version: "0.0.9",
      name: "AppSignal",
      description: description(),
      package: package(),
