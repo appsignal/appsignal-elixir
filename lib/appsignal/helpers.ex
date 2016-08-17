e9ca8efae0d3a32c43b1de6b66525b97a26c4c19
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-08-17 17:16:39 +0200

    Add `instrumented do ... end` macro to wrap around functions

--- lib/appsignal/helpers.ex
+++ lib/appsignal/helpers.ex
@@ -1,6 +1,6 @@
 defmodule Appsignal.Helpers do
   @moduledoc """
-  Helper functions to instrument your application
+  Helper functions and macros to instrument function calls.
   """
 
   alias Appsignal.{Transaction, TransactionRegistry}
@@ -32,14 +32,14 @@ defmodule Appsignal.Helpers do
   following:
 
   ```
-  import Appsignal.Helpers, only: [instrument: 4]
+    import Appsignal.Helpers, only: [instrument: 4]
 
-  def index(conn, _params) do
+    def index(conn, _params) do
       result = instrument(conn, "net.http", "Some slow backend call", fn() ->
-                   Backend.get_result()
-               end
+        Backend.get_result()
+      end
       json conn, result
-  end
+    end
   ```
 
   """
@@ -63,5 +63,80 @@ defmodule Appsignal.Helpers do
     Transaction.finish_event(transaction, name, title, body, body_format)
     result
   end
+
+  @doc """
+  Automatically instrument all function definitions in the nested block using a given category
+
+  All `def` statements that are passed in into the macro are
+  transformed to call `Appsignal.Helpers.instrument/6` automatically.
+
+  ```
+  defmodule MyInstrumentedModule do
+    import Appsignal.Helpers
+
+    instrumented do
+
+      def bar(arg) do
+        # code to be instrumented
+      end
+
+      # more functions...
+    end
+  end
+  ```
+
+  Whenever `MyInstrumentedModule.bar()` is called now, it will be
+  using `instrument/6` to record an Appsignal event. The name of the
+  event is the same as the function name. When a category (atom) is
+  given, the category is postfixed to the function name, so given the
+  following code:
+
+  ```
+    instrumented :http do
+
+      def load_data(arg) do
+        # code to be instrumented
+      end
+    end
+  ```
+
+  events will be recorded under the event name `load_data.http` whenever `load_data()` is called.
+
+  """
+  defmacro instrumented(category, code) do
+    expand_instrumented(".#{category}", code)
+  end
+
+  @doc """
+  Automatically instrument all function definitions in the nested block
+
+  See `instrumented/2`
+  """
+  defmacro instrumented(code) do
+    expand_instrumented("", code)
+  end
+
+
+  defp expand_instrumented(postfix, [do: {:def, _meta, _rest} = deftuple]) do
+    [do: instrument_def(deftuple, postfix)]
+  end
+
+  defp expand_instrumented(postfix, [do: {:__block__, arg, tuples}]) do
+    [do: {:__block__, arg, tuples |> Enum.map(&(instrument_def(&1, postfix)))}]
+  end
+
+  defp instrument_def({:def, defmeta, a=[{name, _, _}=defname, [do: doblock]]}, postfix) do
+    name = Atom.to_string(name)
+    {:def,
+     defmeta,
+     [defname,
+      [do: quote do
+         Appsignal.Helpers.instrument(self(), unquote(name) <> unquote(postfix), Atom.to_string(__MODULE__) <> "." <> unquote(name), fn() -> unquote(doblock) end)
+       end]]}
+  end
+  defp instrument_def(code, _postfix) do
+    code
+  end
+
 
 end
---
+++ test/appsignal_function_instrumentation_macro_test.exs
@@ -0,0 +1,60 @@
+defmodule AppsignalFunctionInstrumentationMacroTest do
+  use ExUnit.Case
+  import Mock
+
+  alias Appsignal.Transaction
+
+  defmodule Foo do
+    import Appsignal.Helpers
+
+    instrumented do
+
+      def bar(arg) do
+        nested(arg, arg)
+      end
+
+      def nested(_arg1, _arg2) do
+      end
+
+    end
+  end
+
+  test_with_mock "instrument module function", Appsignal.Transaction, [:passthrough], [] do
+    t = Transaction.start("bar", :http_request)
+
+    Foo.bar(123)
+
+    assert called Transaction.start_event(t)
+    assert called Transaction.finish_event(t, "bar", "Elixir.AppsignalFunctionInstrumentationMacroTest.Foo.bar", "", 0)
+    assert called Transaction.finish_event(t, "nested", "Elixir.AppsignalFunctionInstrumentationMacroTest.Foo.nested", "", 0)
+
+  end
+
+
+  defmodule Foo2 do
+    import Appsignal.Helpers
+
+    instrumented :http do
+
+      def bar(arg) do
+        nested(arg, arg)
+      end
+
+      def nested(_arg1, _arg2) do
+      end
+
+    end
+  end
+
+  test_with_mock "instrument module function with category", Appsignal.Transaction, [:passthrough], [] do
+    t = Transaction.start("bar", :http_request)
+
+    Foo2.bar(123)
+
+    assert called Transaction.start_event(t)
+    assert called Transaction.finish_event(t, "bar.http", "Elixir.AppsignalFunctionInstrumentationMacroTest.Foo2.bar", "", 0)
+    assert called Transaction.finish_event(t, "nested.http", "Elixir.AppsignalFunctionInstrumentationMacroTest.Foo2.nested", "", 0)
+
+  end
+
+end
