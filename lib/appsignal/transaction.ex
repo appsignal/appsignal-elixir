7597cc2dffe72454cb6167fee6519739d9bd37e8
Jeff Kreeftmeijer <jeff@kreeftmeijer.nl>
2016-10-12 16:33:39 +0200

    Add Transaction.filter_values/2

--- lib/appsignal/transaction.ex
+++ lib/appsignal/transaction.ex
@@ -262,6 +262,23 @@ defmodule Appsignal.Transaction do
     "#{:inet_parse.ntoa host}:#{port}"
   end
 
+  def filter_values(%{__struct__: mod} = struct, _filter_params) when is_atom(mod) do
+    struct
+  end
+  def filter_values(%{} = map, filter_params) do
+    Enum.into map, %{}, fn{k, v} ->
+      if is_binary(k) and String.contains?(k, filter_params) do
+        {k, "[FILTERED]"}
+      else
+        {k, filter_values(v, filter_params)}
+      end
+    end
+  end
+  def filter_values([_|_] = list, filter_params) do
+    Enum.map(list, &filter_values(&1, filter_params))
+  end
+  def filter_values(other, _filter_params), do: other
+
   @doc """
   Given the transaction and a %PlugConn{}, try to set the Phoenix controller module / action in the transaction.
   """
