eafb61e982e219f071aad048fab2fb864ace257b
Arjan Scherpenisse <arjan@scherpenisse.net>
2016-11-11 10:09:02 +0100

    Documentation on instrumentation decorators

--- lib/appsignal/instrumentation/decorators.ex
+++ lib/appsignal/instrumentation/decorators.ex
@@ -1,13 +1,40 @@
 defmodule Appsignal.Instrumentation.Decorators do
+  @moduledoc """
+  Instrumentation decorators
+
+  This module contains various [function
+  decorators](https://github.com/arjan/decorator) for instrumenting
+  function calls.
+
+  `@decorate transaction_event` - when a function decorated like this
+  is called, it will add an event onto the transaction's timeline. The
+  name of the event will be the name of the function that's decorated.
+
+  `@decorate transaction_event(:category)` - when a function decorated
+  like this is called, it will add an event onto the transaction's
+  timeline. The name of the event will be the name of the function
+  that's decorated. In addition, the event will be grouped into the
+  given `:category`.
+
+  `@decorate channel_action` - this decorator is meant to be put
+  before the `handle_in/3` function of a Phoenix.Channel. See
+  `Appsignal.Phoenix.Channel` for more information on how to
+  instrument channel events.
+
+  """
+
+
   use Decorator.Define,
     transaction_event: 0,
     transaction_event: 1,
     channel_action: 0
 
+  @doc false
   def transaction_event(category, body, context) do
     decorate_event(".#{category}", body, context)
   end
 
+  @doc false
   def transaction_event(body, context) do
     decorate_event("", body, context)
   end
@@ -22,7 +49,7 @@ defmodule Appsignal.Instrumentation.Decorators do
     end
   end
 
-
+  @doc false
   def channel_action(body, context = %{args: [action, _payload, socket]}) do
     quote do
       Appsignal.Phoenix.Channel.channel_action(
