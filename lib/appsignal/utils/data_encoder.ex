d9ded5bb513d65d6e9ae6346635652d655dcb24e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-01-04 16:08:35 +0100

    DataEncoder encodes a map with a string key and value

--- lib/appsignal/utils/data_encoder.ex
+++ lib/appsignal/utils/data_encoder.ex
@@ -5,8 +5,13 @@ defmodule Appsignal.Utils.DataEncoder do
 
   alias Appsignal.Nif
 
-  def encode(_) do
+  def encode(data) do
     {:ok, resource} = Nif.data_map_new
+    Enum.each(data, fn(item) -> encode(resource, item) end)
     resource
+  end
+
+  def encode(resource, {key, value}) do
+    Nif.data_set_string(resource, key, value)
   end
 end
--- test/utils/data_encoder_test.exs
+++ test/utils/data_encoder_test.exs
@@ -7,4 +7,9 @@ defmodule Appsignal.Utils.DataEncoderTest do
     resource = DataEncoder.encode(%{})
     assert {:ok, '{}'} == Nif.data_to_json(resource)
   end
+
+  test "encode a map with a string key and value" do
+    resource = DataEncoder.encode(%{"foo" => "bar"})
+    assert {:ok, '{"foo":"bar"}'} == Nif.data_to_json(resource)
+  end
 end
