85302074ba4ca64d0ab2335fe889a3b7d5af088d
Tom de Bruijn <tom@tomdebruijn.com>
2017-02-01 16:53:17 +0100

    Release 0.11.3
    Thijs Cadier (1):
    - Update musl version to fix DNS search issue
      (a8e6f23e4b03561c6922ca62213ef61cb94682ee)

--- mix.exs
+++ mix.exs
@@ -13,7 +13,7 @@ defmodule Appsignal.Mixfile do
 
   def project do
     [app: :appsignal,
-     version: "0.11.2",
+     version: "0.11.3",
      name: "AppSignal",
      description: description(),
      package: package(),
