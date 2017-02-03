3a5716daad184423d3546bd88de223dcda547a4f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-03 14:27:25 +0100

    Poison is an optional dependency

--- mix.exs
+++ mix.exs
@@ -58,7 +58,7 @@ defmodule Appsignal.Mixfile do
 
   defp deps do
     [
-      {:poison, "~> 2.1"},
+      {:poison, ">= 0.0.0", optional: true},
       {:decorator, "~> 1.0"},
       {:phoenix, "~> 1.2.0", optional: true, only: [:prod, :test_phoenix]},
       {:mock, "~> 0.1.1", only: [:test, :test_phoenix, :test_no_nif]},
