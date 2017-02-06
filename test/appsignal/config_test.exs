33b3b74238be0bc116989cf7e8558ccf7dfd0219
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-06 12:48:48 +0100

    Remove automatic :running_in_container setting

--- test/appsignal/config_test.exs
+++ test/appsignal/config_test.exs
@@ -370,7 +370,6 @@ defmodule Appsignal.ConfigTest do
       filter_parameters: nil,
       ignore_actions: [],
       ignore_errors: [],
-      running_in_container: false,
       send_params: true,
       skip_session_data: false,
       valid: false,
