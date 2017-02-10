a95651f08dfda2c1a1b943f68ad8d798ff8a90dd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-10 12:35:18 +0100

    Mock Nif.loaded?, include Bypass, skip tests

--- test/mix/test_helper.exs
+++ test/mix/test_helper.exs
@@ -1,4 +1,4 @@
 :error_logger.tty(false)
-ExUnit.configure(exclude: [pending: true])
-ExUnit.start()
+excludes = [String.to_atom("skip_env_#{Mix.env}"), pending: true]
+ExUnit.start(exclude: excludes)
 {:ok, _} = Application.ensure_all_started(:bypass)
