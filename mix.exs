ed632bc9b695f7581742ff107a6156984928da54
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-02-24 11:47:13 +0100

    Bump to 1.0.0-rc.1

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Appsignal.Mixfile do
 
   def project do
     [app: :appsignal,
-     version: "0.13.0",
+     version: "1.0.0-rc.1",
      name: "AppSignal",
      description: description(),
      package: package(),
