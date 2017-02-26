3066c7709eaa5e8c3f8065ad8a139c8dd761a0e5
Tom de Bruijn <tom@tomdebruijn.com>
2017-02-26 14:05:31 +0100

    Don't add ex_doc to runtime
    As recommended by ex_doc in the README:
    https://github.com/elixir-lang/ex_doc
    
    What does runtime do?
    
    `:runtime` - whether the dependency is part of runtime applications.
    Defaults to true which automatically adds the application to the list of
    apps that are started automatically and included in releases
    
    Source: https://hexdocs.pm/mix/Mix.Tasks.Deps.html

--- mix.exs
+++ mix.exs
@@ -83,7 +83,7 @@ defmodule Appsignal.Mixfile do
       {:phoenix, "~> 1.2.0", optional: true, only: [:prod, :test_phoenix]},
       {:mock, "~> 0.2.1", only: [:test, :test_phoenix, :test_no_nif]},
       {:bypass, "~> 0.5", only: [:test, :test_phoenix, :test_no_nif]},
-      {:ex_doc, "~> 0.12", only: :dev}
+      {:ex_doc, "~> 0.12", only: :dev, runtime: false}
     ]
   end
 end
