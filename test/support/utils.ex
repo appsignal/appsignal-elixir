b7604c20ed187cbbe64b87a73262cd2cfa7f063f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-04-14 16:12:04 +0200

    Remove AppsignalTest.Utils.clear_env/0

--- test/support/utils.ex
+++ test/support/utils.ex
@@ -1,67 +1,71 @@
 defmodule AppsignalTest.Utils do
-  def clear_env do
-    Application.delete_env(:appsignal, :config)
-
-    System.get_env
-    |> Enum.filter(
-      fn({"APPSIGNAL_" <> _, _}) -> true;
-      ({"_APPSIGNAL_" <> _, _}) -> true;
-      ({"APP_REVISION", _}) -> true;
-      ({"DYNO", _}) -> true;
-      (_) -> false end
-    ) |> Enum.each(fn({key, _}) ->
-      System.delete_env(key)
-    end)
-  end
-
-  def with_config_for(app, config, function) do
-    before = put_merged_config_for(app, config)
+  def with_frozen_environment(function) do
+    environment = freeze_environment()
     result = function.()
-    Application.put_env(app, :config, before)
+    unfreeze_environment(environment)
     result
   end
 
+  def freeze_environment do
+    {
+      Application.get_env(:appsignal, :config, %{}),
+      System.get_env
+    }
+  end
+
+  def unfreeze_environment({config, env}) do
+    Application.put_env(:appsignal, :config, config)
+    reset_env(env)
+  end
+
   def with_config(config, function) do
-    with_config_for(:appsignal, config, function)
+    with_config(:appsignal, config, function)
+  end
+
+  def with_config(app, config, function) do
+    with_frozen_environment(fn() ->
+      put_merged_config_for(app, config)
+      function.()
+    end)
   end
 
   defp put_merged_config_for(app, config) do
-    before = Application.get_env(app, :config, %{})
-    Application.put_env(app, :config, Map.merge(before, config))
-    before
+    config = app
+    |> Application.get_env(:config, %{})
+    |> Map.merge(config)
+
+    Application.put_env(app, :config, config)
   end
 
   def setup_with_config(config) do
-    before = put_merged_config_for(:appsignal, config)
+    environment = freeze_environment()
+    put_merged_config_for(:appsignal, config)
 
     ExUnit.Callbacks.on_exit fn() ->
-      Application.put_env(:appsignal, :config, before)
+      unfreeze_environment(environment)
     end
   end
 
   def with_env(env, function) do
-    before = put_merged_env(env)
-    result = function.()
-    reset_env(before)
-    result
+    with_frozen_environment(fn() ->
+      put_merged_env(env)
+      function.()
+    end)
   end
 
   def setup_with_env(env) do
-    before = put_merged_env(env)
+    environment = freeze_environment()
+    put_merged_env(env)
 
     ExUnit.Callbacks.on_exit fn() ->
-      reset_env(before)
+      unfreeze_environment(environment)
     end
   end
 
   defp put_merged_env(env) do
-    before = System.get_env
-
-    before
+    System.get_env
     |> Map.merge(env)
     |> System.put_env
-
-    before
   end
 
   defp reset_env(before) do
