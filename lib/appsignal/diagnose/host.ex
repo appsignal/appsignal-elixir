4cc279a518e2381c0b4db35b0a9713250d197251
Tom de Bruijn <tom@tomdebruijn.com>
2017-04-20 12:52:59 +0200

    Send diagnostics data report to AppSignal (#199)
    This change will allow users to send the diagnostics report to
    AppSignal. We'll ask before sending if the user wants this, but because
    we see how useful this can be when debugging an issue it sends the
    report by default. When you press enter, no input, "Y" (yes) is
    submitted.
    
    Why this change?
    We've seen that in every support request we get we'll ask for the
    diagnostics report. To make it easier for everyone this change allows
    users to send it directly to AppSignal so our support team can review it
    without valuable data getting lost by formatting by our support system
    for example.
    
    We also provide the user with a support token when they send the report.
    This can be useful for a user if they see a problem with the output and
    want to dive right in with the report. They can contact us with the
    support token, after which we can immediately review the report.

---
+++ lib/appsignal/diagnose/host.ex
@@ -0,0 +1,15 @@
+defmodule Appsignal.Diagnose.Host do
+  @system Application.get_env(:appsignal, :appsignal_system, Appsignal.System)
+  @nif Application.get_env(:appsignal, :appsignal_nif, Appsignal.Nif)
+
+  def info do
+    %{
+      architecture: to_string(:erlang.system_info(:system_architecture)),
+      language_version: System.version,
+      otp_version: System.otp_release,
+      heroku: @system.heroku?,
+      root: @system.root?,
+      running_in_container: @nif.running_in_container?
+    }
+  end
+end
---
+++ lib/appsignal/diagnose/library.ex
@@ -0,0 +1,14 @@
+defmodule Appsignal.Diagnose.Library do
+  @appsignal_version Mix.Project.config[:version]
+  @agent_version Mix.Project.config[:agent_version]
+  @nif Application.get_env(:appsignal, :appsignal_nif, Appsignal.Nif)
+
+  def info do
+    %{
+      language: "elixir",
+      agent_version: @agent_version,
+      package_version: @appsignal_version,
+      extension_loaded: @nif.loaded?
+    }
+  end
+end
