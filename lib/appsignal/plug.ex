273b7f685d06f3c2ce73a0b58b5867a3d45be116
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-06-18 15:16:44 +0200

    Plug support without Phoenix (#202)
    * Add tests for Appsignal.phoenix?/0
    
    * Add Appsignal.plug?/0
    
    * Use plug?/0 instead of phoenix?/0 for plug functions
    
    * Rename Appsignal.Phoenix.Plug to Appsignal.Plug
    
    * Add Appsignal.Plug.extract_action/1
    
    * Appsignal.Plug.extract_action/1 handles Phoenix conns
    
    * Deprecate Transaction.try_set_action
    
    * Use Appsignal.Plug.extract_action
    
    * Use Appsignal.Plug instead of Appsignal.Phoenix.Plug
    
    * Use proper controller and action names in Appsignal.PlugTest
    
    * Appsignal.Plug.call/2 catches RuntimeErrors
    
    * Add Plug.extract_error_metadata/1, deprecate /3
    
    * Use extract_error_metadata/1 in Plug.call/2
    
    * Use Plug.ErrorHandler.__catch__/4 instead of raising in Plug.call/2
    
    * Plug.call/2 handles bad request errors
    
    * Plug.call/2 sets error action names, metadata and finishes transactions
    
    * Add Appsignal.Plug.finish_with_conn/2
    
    * Remove Appsignal.Phoenix.call/2
    
    * Plug.extract_action/1 ignores Phoenix conns without actions
    
    * Appsignal.Plug ignores uncalled conns
    
    * Add Plug as an optional dependency

---
+++ lib/appsignal/plug.ex
@@ -0,0 +1,91 @@
+if Appsignal.plug? do
+  defmodule Appsignal.Plug do
+    @moduledoc """
+    Plug handler for Phoenix requests
+    """
+
+    defmacro __using__(_) do
+      quote do
+        @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
+
+        def call(conn, opts) do
+          id = Logger.metadata()[:request_id] || @transaction.generate_id()
+          transaction = @transaction.start(id, :http_request)
+
+          try do
+            super(conn, opts)
+          catch
+            kind, error ->
+              Plug.ErrorHandler.__catch__(conn, kind, error, fn(conn, _exception) ->
+                case Appsignal.Plug.extract_error_metadata(error) do
+                  {reason, message} ->
+                    transaction
+                    |> @transaction.set_error(reason, message, System.stacktrace)
+                    |> finish_with_conn(conn)
+                  nil -> conn
+                end
+              end)
+          else
+            conn -> finish_with_conn(transaction, conn)
+          end
+        end
+
+        defp finish_with_conn(transaction, conn) do
+          try_set_action(transaction, conn)
+          if @transaction.finish(transaction) == :sample do
+            @transaction.set_request_metadata(transaction, conn)
+          end
+
+          :ok = @transaction.complete(transaction)
+          conn
+        end
+
+        defp try_set_action(transaction, conn) do
+          case Appsignal.Plug.extract_action(conn) do
+            nil -> nil
+            action -> @transaction.set_action(transaction, action)
+          end
+        end
+      end
+    end
+
+    @doc """
+    Returns a tuple with the exception's reason and message unless the error has
+    a status code under 500.
+    """
+    def extract_error_metadata(%{plug_status: status}) when status < 500 do
+      nil
+    end
+    def extract_error_metadata(%Plug.Conn.WrapperError{reason: reason = %{}}) do
+      Appsignal.ErrorHandler.extract_reason_and_message(reason, "HTTP request error")
+    end
+    def extract_error_metadata(reason) do
+      Appsignal.ErrorHandler.extract_reason_and_message(reason, "HTTP request error")
+    end
+
+    @doc false
+    def extract_error_metadata(reason, conn, stack) do
+      IO.warn "Appsignal.Plug.extract_error_metadata/3 is deprecated. Use Appsignal.Plug.extract_error_metadata/1 instead."
+      {reason, message} = extract_error_metadata(reason)
+      {reason, message, stack, conn}
+    end
+
+    def extract_action(%Plug.Conn{private: %{phoenix_action: action, phoenix_controller: controller}}) do
+      merge_action_and_controller(action, controller)
+    end
+    def extract_action(%Plug.Conn{private: %{phoenix_endpoint: _}}), do: nil
+    def extract_action(%Plug.Conn{method: method, request_path: path}) do
+      "#{method} #{path}"
+    end
+
+    defp merge_action_and_controller(action, controller) when is_atom(controller) do
+      merge_action_and_controller(
+        action,
+        controller |> Atom.to_string |> String.trim_leading("Elixir.")
+      )
+    end
+    defp merge_action_and_controller(action, controller) do
+      "#{controller}##{action}"
+    end
+  end
+end
