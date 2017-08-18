3f6d98d41e260c28f1dccc57eabd47de5f339b5b
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-18 10:47:11 +0200

    Trust Exception.format_stacktrace_entry/1 in BacktraceTest

--- test/appsignal/backtrace_test.exs
+++ test/appsignal/backtrace_test.exs
@@ -2,20 +2,21 @@ defmodule Appsignal.BacktraceTest do
   use ExUnit.Case, async: true
   doctest Appsignal.Backtrace
 
+  @match_line {:elixir_translator, :guard_op, 2, [file: 'src/elixir_translator.erl', line: 317]}
+
   test "formats stacktrace lines" do
-    assert Appsignal.Backtrace.from_stacktrace([
-      {:elixir_translator, :guard_op, 2,
-        [file: 'src/elixir_translator.erl', line: 317]}
-    ]) == ["(elixir) src/elixir_translator.erl:317: :elixir_translator.guard_op/2"]
+    assert Appsignal.Backtrace.from_stacktrace([@match_line]) == [
+      Exception.format_stacktrace_entry(@match_line)
+    ]
   end
 
   test "removes error lines" do
-    assert Appsignal.Backtrace.from_stacktrace([
-      {:erl_internal, :op_type, [:get_stacktrace, 0],
-        [file: 'erl_internal.erl', line: 212]},
-      {:elixir_translator, :guard_op, 2,
-        [file: 'src/elixir_translator.erl', line: 317]},
-    ]) == ["(elixir) src/elixir_translator.erl:317: :elixir_translator.guard_op/2"]
+    stacktrace = [{:erl_internal, :op_type, [:get_stacktrace, 0],
+        [file: 'erl_internal.erl', line: 212]}, @match_line]
+
+    assert Appsignal.Backtrace.from_stacktrace(stacktrace) == [
+      Exception.format_stacktrace_entry(@match_line)
+    ]
   end
 
   test "handles lists of binaries" do
