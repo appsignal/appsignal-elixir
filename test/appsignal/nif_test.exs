5dfa4b386b8f528abfdff5d28477ba9ba2e276ee
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-08-24 11:26:38 +0200

    Expect references from Nif.start_transaction on OTP 20
    > "All uses of the magic binary kludge has been replaced by uses of erlang
    > references [since OTP-14205]."
    -- http://erlang.org/download/otp_src_20.0-rc1.readme

--- test/appsignal/nif_test.exs
+++ test/appsignal/nif_test.exs
@@ -9,9 +9,16 @@ defmodule Appsignal.NifTest do
     assert :ok = Appsignal.Nif.stop
   end
 
-  test "making a transaction" do
-    assert {:ok, transaction} = Appsignal.Nif.start_transaction("transaction id", "http_request")
-    assert is_binary(transaction)
+  if System.otp_release >= "20" do
+    test "starting transaction returns a reference to the transaction resource" do
+      assert {:ok, transaction} = Appsignal.Nif.start_transaction("transaction id", "http_request")
+      assert is_reference(transaction)
+    end
+  else
+    test "starting transaction returns a resource term" do
+      assert {:ok, transaction} = Appsignal.Nif.start_transaction("transaction id", "http_request")
+      assert is_binary(transaction)
+    end
   end
 
   if not(Mix.env in [:test_no_nif]) do
