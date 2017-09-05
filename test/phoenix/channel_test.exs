8634d4f43d469f20184cf14f0cc5354e8c439bad
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-09-05 16:38:31 +0200

    Don't crash with unbound channel payloads (#253)
    Since 0ab1e198f4e8b99d114f7098a7eb34f339091655,
    Appsignal.Instrumentation.Decorators.channel_action/2 uses
    Appsignal.Phoenix.Channel.channel_action/5 to add the payload to the
    transaction as params. Since this doesn't work when the payload is
    unbound, this patch adds a fallback to channel_action/4.

--- test/phoenix/channel_test.exs
+++ test/phoenix/channel_test.exs
@@ -7,6 +7,11 @@ defmodule UsingAppsignalPhoenixChannel do
     {:reply, {:ok, payload}, socket}
   end
 
+  @decorate channel_action()
+  def handle_in("decorated_with_unbound_payload", _, socket) do
+    {:reply, :ok, socket}
+  end
+
   def handle_in("instrumented" = action, payload, socket) do
     channel_action(__MODULE__, action, socket, payload, fn ->
       {:reply, {:ok, payload}, socket}
@@ -50,6 +55,27 @@ defmodule Appsignal.Phoenix.ChannelTest do
         transport: Phoenix.Transports.WebSocket
       },
       "params" => %{"body" => "Hello, world!"}
+    } == FakeTransaction.sample_data
+    assert [%Appsignal.Transaction{id: "123"}] = FakeTransaction.finished_transactions
+    assert [%Appsignal.Transaction{id: "123"}] = FakeTransaction.completed_transactions
+  end
+
+  test "instruments a channel action with a decorator and an unbound payload", %{socket: socket} do
+    UsingAppsignalPhoenixChannel.handle_in("decorated_with_unbound_payload", %{"body" => "Hello, world!"}, socket)
+
+    assert [{"123", :channel}] == FakeTransaction.started_transactions
+    assert "UsingAppsignalPhoenixChannel#decorated_with_unbound_payload" == FakeTransaction.action
+    assert %{
+      "environment" => %{
+        channel: PhoenixChatExampleWeb.RoomChannel,
+        endpoint: PhoenixChatExampleWeb.Endpoint,
+        handler: PhoenixChatExampleWeb.UserSocket,
+        id: 1,
+        ref: 2,
+        topic: "room:lobby",
+        transport: Phoenix.Transports.WebSocket
+      },
+      "params" => %{}
     } == FakeTransaction.sample_data
     assert [%Appsignal.Transaction{id: "123"}] = FakeTransaction.finished_transactions
     assert [%Appsignal.Transaction{id: "123"}] = FakeTransaction.completed_transactions
