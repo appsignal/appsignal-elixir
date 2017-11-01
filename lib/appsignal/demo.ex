4ccf26b651515610d3247c27c2d56ca8820f1bc6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-11-01 11:54:32 +0100

    Add typespecs to Demo.create_transaction_(error|performance)_request/0

--- lib/appsignal/demo.ex
+++ lib/appsignal/demo.ex
@@ -1,6 +1,6 @@
 defmodule Appsignal.DemoBehaviour do
-  @callback create_transaction_error_request :: nil
-  @callback create_transaction_performance_request :: nil
+  @callback create_transaction_error_request :: :ok
+  @callback create_transaction_performance_request :: :ok
 end
 
 defmodule Appsignal.Demo do
@@ -9,6 +9,7 @@ defmodule Appsignal.Demo do
   @behaviour Appsignal.DemoBehaviour
 
   @doc false
+  @spec create_transaction_error_request :: :ok
   def create_transaction_error_request do
     create_demo_transaction()
     |> Appsignal.Transaction.set_error(
@@ -20,6 +21,7 @@ defmodule Appsignal.Demo do
   end
 
   @doc false
+  @spec create_transaction_performance_request :: :ok
   def create_transaction_performance_request do
     transaction = create_demo_transaction()
 
