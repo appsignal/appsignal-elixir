0aaf52be774eba53e592b460d076e95b3eade332
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-11-21 17:51:54 +0100

    Convert FakeOS to a TestAgent

--- test/support/fake_os.ex
+++ test/support/fake_os.ex
@@ -1,13 +1,5 @@
 defmodule FakeOS do
-  def start_link do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
-  end
+  use TestAgent, %{type: {:unix, :linux}}
 
-  def set(key, value) do
-    Agent.update(__MODULE__, &Map.put(&1, key, value))
-  end
-
-  def type do
-    Agent.get(__MODULE__, &Map.get(&1, :type, {:unix, :linux}))
-  end
+  def type, do: get(__MODULE__, :type)
 end
