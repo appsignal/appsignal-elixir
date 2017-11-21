53bbbe07c8479b8ad444455b78abbfef41641f50
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2017-11-21 11:56:06 +0100

    TestAgent waits for processes to be DOWN on_exit
    Make sure the spawned process receives DOWN after its parent process
    is finished (as per https://elixirforum.com/t/3794/5), with a 500 ms
    timeout.

--- test/support/test_agent.ex
+++ test/support/test_agent.ex
@@ -1,12 +1,24 @@
 defmodule TestAgent do
   defmacro __using__(default_state) do
     quote do
+      import ExUnit.Assertions
+
       def start_link do
-        Agent.start_link(fn() ->
+        {:ok, pid} = Agent.start_link(fn() ->
             unquote(default_state) |> Enum.into(%{})
           end,
           name: __MODULE__
         )
+
+        # Make sure the spawned process receives DOWN after its parent process
+        # is finished (as per https://elixirforum.com/t/3794/5), with a 500 ms
+        # timeout.
+        ExUnit.Callbacks.on_exit(fn() ->
+          ref = Process.monitor(pid)
+          assert_receive {:DOWN, ^ref, _, _, _}, 500
+        end)
+
+        {:ok, pid}
       end
 
       def get(pid_or_module, key) do
