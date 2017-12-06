377332dcde25abda99368e45df114e38d3a4afdd
Tom de Bruijn <tom@tomdebruijn.com>
2017-12-06 10:18:10 +0100

    ErrorHandler unwraps Plug.Conn.WrapperError (#281)
    Tested this in the plug_test by raising the wrapped error. We couldn't
    find a way to raise the unwrapped error, e.g. `nil.undefined_function`
    and make the test pass.
    
    Closes #245.

--- lib/appsignal/error_handler.ex
+++ lib/appsignal/error_handler.ex
@@ -114,6 +114,11 @@ defmodule Appsignal.ErrorHandler do
   Also returns a 'message' which is supposed to contain extra error information.
   """
   @spec extract_reason_and_message(any(), binary()) :: {any(), binary()}
+  def extract_reason_and_message(%Plug.Conn.WrapperError{reason: reason, kind: kind, stack: stacktrace}, message) do
+    kind
+    |> Exception.normalize(reason, stacktrace)
+    |> extract_reason_and_message(message)
+  end
   def extract_reason_and_message(reason, message) when is_binary(reason) do
     {reason, message}
   end
