21794343ed0cd88668506baa3642f41164e1e0b7
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-02-14 14:14:23 +0100

    $ mix format test/appsignal/plug_test.exs

--- test/appsignal/plug_test.exs
+++ test/appsignal/plug_test.exs
@@ -2,22 +2,29 @@ defmodule UsingAppsignalPlug do
   def call(%Plug.Conn{private: %{phoenix_action: :exception}}, _opts) do
     raise("Exception!")
   end
+
   def call(%Plug.Conn{private: %{phoenix_action: :timeout}}, _opts) do
     Task.async(fn -> :timer.sleep(10) end) |> Task.await(1)
   end
+
   def call(%Plug.Conn{private: %{phoenix_action: :bad_request}}, _opts) do
     raise %Plug.BadRequestError{}
   end
+
   def call(%Plug.Conn{private: %{phoenix_action: :undef}} = conn, _opts) do
     raise %Plug.Conn.WrapperError{
-      kind: :error, reason: :undef, stack: System.stacktrace, conn: conn
+      kind: :error,
+      reason: :undef,
+      stack: System.stacktrace(),
+      conn: conn
     }
   end
+
   def call(%Plug.Conn{} = conn, _opts) do
     conn |> Plug.Conn.assign(:called?, true)
   end
 
-  defoverridable [call: 2]
+  defoverridable call: 2
 
   use Appsignal.Plug
 end
@@ -27,16 +34,17 @@ defmodule Appsignal.PlugTest do
   alias Appsignal.FakeTransaction
 
   setup do
-    {:ok, fake_transaction} = FakeTransaction.start_link
+    {:ok, fake_transaction} = FakeTransaction.start_link()
     [fake_transaction: fake_transaction]
   end
 
   describe "for a :sample transaction" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> UsingAppsignalPlug.call(%{})
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :index)
+        |> UsingAppsignalPlug.call(%{})
 
       [conn: conn]
     end
@@ -50,21 +58,23 @@ defmodule Appsignal.PlugTest do
     end
 
     test "sets the transaction's action name", %{fake_transaction: fake_transaction} do
-      assert "AppsignalPhoenixExample.PageController#index"
-        == FakeTransaction.action(fake_transaction)
+      assert "AppsignalPhoenixExample.PageController#index" ==
+               FakeTransaction.action(fake_transaction)
     end
 
     test "finishes the transaction", %{fake_transaction: fake_transaction} do
       assert [%Appsignal.Transaction{}] = FakeTransaction.finished_transactions(fake_transaction)
     end
 
-    test "sets the transaction's request metadata", %{conn: conn, fake_transaction: fake_transaction} do
+    test "sets the transaction's request metadata", %{
+      conn: conn,
+      fake_transaction: fake_transaction
+    } do
       assert conn == FakeTransaction.request_metadata(fake_transaction)
     end
 
     test "completes the transaction", %{fake_transaction: fake_transaction} do
-      assert [%Appsignal.Transaction{}] =
-        FakeTransaction.completed_transactions(fake_transaction)
+      assert [%Appsignal.Transaction{}] = FakeTransaction.completed_transactions(fake_transaction)
     end
   end
 
@@ -72,10 +82,11 @@ defmodule Appsignal.PlugTest do
     setup %{fake_transaction: fake_transaction} do
       FakeTransaction.update(fake_transaction, :finish, :no_sample)
 
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> UsingAppsignalPlug.call(%{})
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :index)
+        |> UsingAppsignalPlug.call(%{})
 
       [conn: conn]
     end
@@ -87,9 +98,10 @@ defmodule Appsignal.PlugTest do
 
   describe "for a transaction with a Phoenix endpoint, but no action" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_endpoint, MyEndpoint)
-      |> UsingAppsignalPlug.call(%{})
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_endpoint, MyEndpoint)
+        |> UsingAppsignalPlug.call(%{})
 
       [conn: conn]
     end
@@ -101,40 +113,46 @@ defmodule Appsignal.PlugTest do
 
   describe "for a transaction with an error" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :exception)
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :exception)
 
-      :ok = try do
-        UsingAppsignalPlug.call(conn, %{})
-      catch
-        :error, %RuntimeError{message: "Exception!"} -> :ok
-        type, reason -> {type, reason}
-      end
+      :ok =
+        try do
+          UsingAppsignalPlug.call(conn, %{})
+        catch
+          :error, %RuntimeError{message: "Exception!"} -> :ok
+          type, reason -> {type, reason}
+        end
 
       [conn: conn]
     end
 
     test "sets the transaction error", %{fake_transaction: fake_transaction} do
-      assert [{
-        %Appsignal.Transaction{},
-        "RuntimeError",
-        "HTTP request error: Exception!",
-        _stack
-      }] = FakeTransaction.errors(fake_transaction)
+      assert [
+               {
+                 %Appsignal.Transaction{},
+                 "RuntimeError",
+                 "HTTP request error: Exception!",
+                 _stack
+               }
+             ] = FakeTransaction.errors(fake_transaction)
     end
 
     test "sets the transaction's action name", %{fake_transaction: fake_transaction} do
-      assert "AppsignalPhoenixExample.PageController#exception"
-        == FakeTransaction.action(fake_transaction)
+      assert "AppsignalPhoenixExample.PageController#exception" ==
+               FakeTransaction.action(fake_transaction)
     end
 
     test "finishes the transaction", %{fake_transaction: fake_transaction} do
-      assert [%Appsignal.Transaction{}] =
-        FakeTransaction.finished_transactions(fake_transaction)
+      assert [%Appsignal.Transaction{}] = FakeTransaction.finished_transactions(fake_transaction)
     end
 
-    test "sets the transaction's request metadata", %{conn: conn, fake_transaction: fake_transaction} do
+    test "sets the transaction's request metadata", %{
+      conn: conn,
+      fake_transaction: fake_transaction
+    } do
       assert conn == FakeTransaction.request_metadata(fake_transaction)
     end
 
@@ -145,20 +163,22 @@ defmodule Appsignal.PlugTest do
 
   describe "for a transaction with a bad request error" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :bad_request)
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :bad_request)
 
       [conn: conn]
     end
 
     test "does not set the transaction error", %{conn: conn, fake_transaction: fake_transaction} do
-      :ok = try do
-        UsingAppsignalPlug.call(conn, %{})
-      catch
-        :error, %Plug.BadRequestError{} -> :ok
-        type, reason -> {type, reason}
-      end
+      :ok =
+        try do
+          UsingAppsignalPlug.call(conn, %{})
+        catch
+          :error, %Plug.BadRequestError{} -> :ok
+          type, reason -> {type, reason}
+        end
 
       assert [] = FakeTransaction.errors(fake_transaction)
     end
@@ -166,159 +186,163 @@ defmodule Appsignal.PlugTest do
 
   describe "for a wrapped undefined error" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :undef)
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :undef)
 
       [conn: conn]
     end
 
     test "sets the transaction error", %{conn: conn, fake_transaction: fake_transaction} do
-      :ok = try do
-        UsingAppsignalPlug.call(conn, %{})
-      rescue
-        Plug.Conn.WrapperError -> :ok
-      end
+      :ok =
+        try do
+          UsingAppsignalPlug.call(conn, %{})
+        rescue
+          Plug.Conn.WrapperError -> :ok
+        end
 
-      assert [{
-        %Appsignal.Transaction{},
-        "UndefinedFunctionError",
-        "HTTP request error: undefined function",
-        _stack
-      }] = FakeTransaction.errors(fake_transaction)
+      assert [
+               {
+                 %Appsignal.Transaction{},
+                 "UndefinedFunctionError",
+                 "HTTP request error: undefined function",
+                 _stack
+               }
+             ] = FakeTransaction.errors(fake_transaction)
     end
   end
 
   describe "for a transaction with an timeout" do
     setup do
-      conn = %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :timeout)
+      conn =
+        %Plug.Conn{}
+        |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+        |> Plug.Conn.put_private(:phoenix_action, :timeout)
 
       [conn: conn]
     end
 
     test "sets the transaction error", %{conn: conn, fake_transaction: fake_transaction} do
-      :ok = try do
-        UsingAppsignalPlug.call(conn, %{})
-      catch
-        :exit, {:timeout, {Task, :await, _}} -> :ok
-        type, reason -> {type, reason}
-      end
+      :ok =
+        try do
+          UsingAppsignalPlug.call(conn, %{})
+        catch
+          :exit, {:timeout, {Task, :await, _}} -> :ok
+          type, reason -> {type, reason}
+        end
 
-      assert [{
-        %Appsignal.Transaction{},
-        ":timeout",
-        "HTTP request error: {:timeout, {Task, :await, [%Task{owner: " <> _,
-        _stack
-      }] = FakeTransaction.errors(fake_transaction)
+      assert [
+               {
+                 %Appsignal.Transaction{},
+                 ":timeout",
+                 "HTTP request error: {:timeout, {Task, :await, [%Task{owner: " <> _,
+                 _stack
+               }
+             ] = FakeTransaction.errors(fake_transaction)
     end
   end
 
   describe "extracting error metadata" do
     test "with a RuntimeError" do
-      assert Appsignal.Plug.extract_error_metadata(%RuntimeError{})
-        == {"RuntimeError", "HTTP request error: runtime error"}
+      assert Appsignal.Plug.extract_error_metadata(%RuntimeError{}) ==
+               {"RuntimeError", "HTTP request error: runtime error"}
     end
 
     test "with a Plug.Conn.WrapperError" do
       error = %Plug.Conn.WrapperError{reason: %RuntimeError{}}
 
-      assert Appsignal.Plug.extract_error_metadata(error)
-        == {"RuntimeError", "HTTP request error: runtime error"}
+      assert Appsignal.Plug.extract_error_metadata(error) ==
+               {"RuntimeError", "HTTP request error: runtime error"}
     end
 
     test "with an error tuple" do
-      error = {:timeout,
-       {Task, :await,
-        [%Task{owner: self(), pid: self(), ref: make_ref()},
-         1]}}
+      error = {:timeout, {Task, :await, [%Task{owner: self(), pid: self(), ref: make_ref()}, 1]}}
 
-      assert Appsignal.Plug.extract_error_metadata(error)
-        == {":timeout", "HTTP request error: #{inspect(error)}"}
+      assert Appsignal.Plug.extract_error_metadata(error) ==
+               {":timeout", "HTTP request error: #{inspect(error)}"}
     end
 
     test "ignores errors with a plug_status < 500" do
-      assert Appsignal.Plug.extract_error_metadata(%Plug.BadRequestError{})
-        == nil
+      assert Appsignal.Plug.extract_error_metadata(%Plug.BadRequestError{}) == nil
     end
   end
 
   describe "extracting action names" do
     test "from a Plug conn" do
-      assert Appsignal.Plug.extract_action(
-        %Plug.Conn{method: "GET", request_path: "/foo"}
-      ) == "GET /foo"
+      assert Appsignal.Plug.extract_action(%Plug.Conn{method: "GET", request_path: "/foo"}) ==
+               "GET /foo"
     end
 
     test "from a Plug conn with a Phoenix endpoint, but no controller or action" do
       assert %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_endpoint, MyEndpoint)
-      |> Appsignal.Plug.extract_action == nil
+             |> Plug.Conn.put_private(:phoenix_endpoint, MyEndpoint)
+             |> Appsignal.Plug.extract_action() == nil
     end
 
     test "from a Plug conn with a Phoenix controller and action" do
       assert %Plug.Conn{}
-      |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
-      |> Plug.Conn.put_private(:phoenix_action, :index)
-      |> Appsignal.Plug.extract_action == "AppsignalPhoenixExample.PageController#index"
+             |> Plug.Conn.put_private(:phoenix_controller, AppsignalPhoenixExample.PageController)
+             |> Plug.Conn.put_private(:phoenix_action, :index)
+             |> Appsignal.Plug.extract_action() == "AppsignalPhoenixExample.PageController#index"
     end
   end
 
   describe "extracting request metadata" do
     test "from a Plug conn" do
-      assert Appsignal.Plug.extract_meta_data(
-        %Plug.Conn{
-          method: "GET",
-          request_path: "/foo",
-          resp_headers: [{"x-request-id", "kk4hk5sis7c3b56t683nnmdig632c9ot"}]
-        }
-      ) == %{
-        "method" => "GET",
-        "path" => "/foo",
-        "request_id" => "kk4hk5sis7c3b56t683nnmdig632c9ot"
-      }
+      assert Appsignal.Plug.extract_meta_data(%Plug.Conn{
+               method: "GET",
+               request_path: "/foo",
+               resp_headers: [{"x-request-id", "kk4hk5sis7c3b56t683nnmdig632c9ot"}]
+             }) == %{
+               "method" => "GET",
+               "path" => "/foo",
+               "request_id" => "kk4hk5sis7c3b56t683nnmdig632c9ot"
+             }
     end
   end
 
   describe "extracting sample data" do
     setup do
-      %{conn: %Plug.Conn{
-        params: %{"foo" => "bar"},
-        host: "www.example.com",
-        method: "GET",
-        script_name: ["foo", "bar"],
-        request_path: "/foo/bar",
-        port: 80,
-        query_string: "foo=bar",
-        peer: {{127, 0, 0, 1}, 12345},
-        scheme: :http,
-        req_headers: [{"accept", "text/html"}]
-      }}
-    end
-
-    test "from a Plug conn", %{conn: conn} do
-      assert Appsignal.Plug.extract_sample_data(conn) == %{
-        "params" => %{"foo" => "bar"},
-        "environment" => %{
-          "host" => "www.example.com",
-          "method" => "GET",
-          "script_name" => ["foo", "bar"],
-          "request_path" => "/foo/bar",
-          "port" => 80,
-          "query_string" => "foo=bar",
-          "peer" => "127.0.0.1:12345",
-          "request_uri" => "http://www.example.com:80/foo/bar",
-          "req_headers.accept" => "text/html"
+      %{
+        conn: %Plug.Conn{
+          params: %{"foo" => "bar"},
+          host: "www.example.com",
+          method: "GET",
+          script_name: ["foo", "bar"],
+          request_path: "/foo/bar",
+          port: 80,
+          query_string: "foo=bar",
+          peer: {{127, 0, 0, 1}, 12345},
+          scheme: :http,
+          req_headers: [{"accept", "text/html"}]
         }
       }
     end
 
+    test "from a Plug conn", %{conn: conn} do
+      assert Appsignal.Plug.extract_sample_data(conn) == %{
+               "params" => %{"foo" => "bar"},
+               "environment" => %{
+                 "host" => "www.example.com",
+                 "method" => "GET",
+                 "script_name" => ["foo", "bar"],
+                 "request_path" => "/foo/bar",
+                 "port" => 80,
+                 "query_string" => "foo=bar",
+                 "peer" => "127.0.0.1:12345",
+                 "request_uri" => "http://www.example.com:80/foo/bar",
+                 "req_headers.accept" => "text/html"
+               }
+             }
+    end
+
     test "with a param that should be filtered out", %{conn: conn} do
-      AppsignalTest.Utils.with_config(%{filter_parameters: ["password"]}, fn() ->
+      AppsignalTest.Utils.with_config(%{filter_parameters: ["password"]}, fn ->
         conn = %{conn | params: %{"password" => "secret"}}
+
         assert %{"params" => %{"password" => "[FILTERED]"}} =
-          Appsignal.Plug.extract_sample_data(conn)
+                 Appsignal.Plug.extract_sample_data(conn)
       end)
     end
   end
@@ -344,19 +368,19 @@ defmodule Appsignal.PlugTest do
       }
 
       assert Appsignal.Plug.extract_request_headers(conn) == %{
-        "req_headers.content-length" => "1024",
-        "req_headers.accept" => "text/html",
-        "req_headers.accept-charset" => "utf-8",
-        "req_headers.accept-encoding" => "gzip, deflate",
-        "req_headers.accept-language" => "en-us",
-        "req_headers.cache-control" => "no-cache",
-        "req_headers.connection" => "keep-alive",
-        "req_headers.user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3...",
-        "req_headers.from" => "webmaster@example.org",
-        "req_headers.referer" => "http://localhost:4001/",
-        "req_headers.range" => "bytes=0-1023",
-        "req_headers.x-real-ip" => "179.146.231.170"
-      }
+               "req_headers.content-length" => "1024",
+               "req_headers.accept" => "text/html",
+               "req_headers.accept-charset" => "utf-8",
+               "req_headers.accept-encoding" => "gzip, deflate",
+               "req_headers.accept-language" => "en-us",
+               "req_headers.cache-control" => "no-cache",
+               "req_headers.connection" => "keep-alive",
+               "req_headers.user-agent" => "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3...",
+               "req_headers.from" => "webmaster@example.org",
+               "req_headers.referer" => "http://localhost:4001/",
+               "req_headers.range" => "bytes=0-1023",
+               "req_headers.x-real-ip" => "179.146.231.170"
+             }
     end
   end
 end
