c155a8b100786ac65c7c3b8fcb96cd593e41d532
Tom de Bruijn <tom@tomdebruijn.com>
2018-05-08 14:04:58 +0200

    Release 1.6.1

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Appsignal.Mixfile do
 
   def project do
     [app: :appsignal,
-     version: "1.6.0",
+     version: "1.6.1",
      name: "AppSignal",
      description: description(),
      package: package(),
