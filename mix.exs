1192144431c462bbca48fe1c540e10baebb09d31
Tom de Bruijn <tom@tomdebruijn.com>
2018-05-29 15:38:12 +0200

    Bump to 1.7.0-alpha.4

--- mix.exs
+++ mix.exs
@@ -32,7 +32,7 @@ defmodule Appsignal.Mixfile do
 
   def project do
     [app: :appsignal,
-     version: "1.7.0-alpha.3",
+     version: "1.7.0-alpha.4",
      name: "AppSignal",
      description: description(),
      package: package(),
