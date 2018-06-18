af64dc54b40766cb2fb9afac1dd1b7d09b3e1acd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-06-18 15:19:10 +0200

    Add tests for non-existing and unreadable certs

--- test/appsignal/transmitter_test.exs
+++ test/appsignal/transmitter_test.exs
@@ -2,6 +2,7 @@ defmodule Appsignal.TransmitterTest do
   use ExUnit.Case
   alias Appsignal.{Transmitter, Config}
   import AppsignalTest.Utils
+  import ExUnit.CaptureLog
 
   test "uses the default CA certificate" do
     path = Config.ca_file_path()
@@ -10,17 +11,34 @@ defmodule Appsignal.TransmitterTest do
              Transmitter.request(:get, "https://example.com")
   end
 
-  test "uses the configured ca certificate" do
-    path = "foo.pem"
-    with_config(%{ca_file_path: path}, fn() ->
+  test "uses the configured CA certificate" do
+    path = "priv/cacert.pem"
+
+    with_config(%{ca_file_path: path}, fn ->
       assert [_method, _url, _headers, _body, [ssl_options: [cacertfile: ^path]]] =
-              Transmitter.request(:get, "https://example.com")
+               Transmitter.request(:get, "https://example.com")
     end)
   end
 
-  test "logs a warning when the ca certificate file is not readable" do
+  test "logs a warning when the CA certificate file does not exist" do
+    path = "test/fixtures/does_not_exist.pem"
+
+    with_config(%{ca_file_path: path}, fn ->
+      assert capture_log(fn ->
+        assert [_method, _url, _headers, _body, []] =
+                Transmitter.request(:get, "https://example.com")
+      end) =~ "[warn]  Ignoring non-existing or unreadable ca_file_path (test/fixtures/does_not_exist.pem): :enoent"
+    end)
   end
 
-  test "logs a warning when the ca certificate file does not exist" do
+  test "logs a warning when the CA certificate file is not readable" do
+    path = "test/fixtures/unreadable.pem"
+
+    with_config(%{ca_file_path: path}, fn ->
+      assert capture_log(fn ->
+        assert [_method, _url, _headers, _body, []] =
+                Transmitter.request(:get, "https://example.com")
+      end) =~ "[warn]  Ignoring non-existing or unreadable ca_file_path (test/fixtures/unreadable.pem): \"File access is :none\""
+    end)
   end
 end
