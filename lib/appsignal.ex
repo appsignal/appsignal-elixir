546cf8d9f0f588b7a75b78b4c600191d1969dd2d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-07-19 14:48:24 +0200

    Don't call System.stracktrace() from Appsignal.send_error/1-7

--- lib/appsignal.ex
+++ lib/appsignal.ex
@@ -140,15 +140,13 @@ defmodule Appsignal do
       Appsignal.send_error(%RuntimeError{})
       Appsignal.send_error(%RuntimeError{}, "Oops!")
       Appsignal.send_error(%RuntimeError{}, "", System.stacktrace())
-      Appsignal.send_error(%RuntimeError{}, "", nil, %{foo: "bar"})
-      Appsignal.send_error(%RuntimeError{}, "", nil, %{}, %Plug.Conn{})
-      Appsignal.send_error(%RuntimeError{}, "", nil, %{}, nil, fn(transaction) ->
+      Appsignal.send_error(%RuntimeError{}, "", [], %{foo: "bar"})
+      Appsignal.send_error(%RuntimeError{}, "", [], %{}, %Plug.Conn{})
+      Appsignal.send_error(%RuntimeError{}, "", [], %{}, nil, fn(transaction) ->
         Appsignal.Transaction.set_sample_data(transaction, "key", %{foo: "bar"})
       end)
   """
-  def send_error(reason, message \\ "", stack \\ nil, metadata \\ %{}, conn \\ nil, fun \\ fn(t) -> t end, namespace \\ :http_request) do
-    stack = stack || System.stacktrace()
-
+  def send_error(reason, message \\ "", stack \\ [], metadata \\ %{}, conn \\ nil, fun \\ fn(t) -> t end, namespace \\ :http_request) do
     transaction = Appsignal.Transaction.create("_" <> Appsignal.Transaction.generate_id(), namespace)
     fun.(transaction)
     {reason, message} = Appsignal.ErrorHandler.extract_reason_and_message(reason, message)
