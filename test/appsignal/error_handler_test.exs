ab8e75628cbe1df85ce07bcb2719c6982b964851
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2018-11-24 22:51:25 +0100

    Move ErrorLoggerForwarder to test/support

--- test/appsignal/error_handler_test.exs
+++ test/appsignal/error_handler_test.exs
@@ -1,18 +1,3 @@
-defmodule ErrorLoggerForwarder do
-  def init(pid) do
-    {:ok, pid}
-  end
-
-  def handle_event(event, pid) do
-    send(pid, event)
-    {:ok, pid}
-  end
-
-  def handle_info(_, state) do
-    {:ok, state}
-  end
-end
-
 defmodule Appsignal.ErrorHandlerTest do
   @moduledoc """
   Test the actual Appsignal.ErrorHandler
