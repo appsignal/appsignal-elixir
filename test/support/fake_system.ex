3862194c27a985d16cf4eb59603e58888af90dda
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-01-08 16:43:08 +0100

    Drop Mock dependency, use FakeTransaction, FakeSystem and Transaction.to_map/1 instead (#437)
    * Add FakeSystem, use it instead of Mock in Appsignal.HelperTest
    
    * Use FakeTransaction in AppsignalHelpersTest
    
    * Use FakeTransaction in DecoratorTest
    
    * Use FakeTransaction in DemoTest
    
    * Use Transaction.to_map/1 in TransactionTest
    
    * Remove Mock from dependencies
    
    * Move AppSignal-specific mock modules into their own subdirectory

--- test/support/fake_system.ex
+++ test/support/fake_system.ex
@@ -1,14 +1,7 @@
-defmodule Appsignal.FakeSystem do
-  @behaviour Appsignal.SystemBehaviour
-  use TestAgent, %{uid: 999, heroku: false, root: false}
+defmodule FakeSystem do
+  use TestAgent, %{cmd: fn _, _, _ -> raise "oh no!" end}
 
-  def root?, do: get(__MODULE__, :root)
-
-  def heroku?, do: get(__MODULE__, :heroku)
-
-  def uid, do: get(__MODULE__, :uid)
-
-  def priv_dir, do: get(__MODULE__, :priv_dir)
-
-  def agent_platform, do: get(__MODULE__, :agent_platform)
+  def cmd(command, args, opts) do
+    get(__MODULE__, :cmd).(command, args, opts)
+  end
 end
