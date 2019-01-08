30a769a18ce7018b41872f9ef9f9d188f7f4fa60
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-01-08 16:43:08 +0100

    Drop Mock dependency, use FakeTransaction, FakeSystem and Transaction.to_map/1 instead (#437)
    * Add FakeSystem, use it instead of Mock in Appsignal.HelperTest
    
    * Use FakeTransaction in AppsignalHelpersTest
    
    * Use FakeTransaction in DecoratorTest
    
    * Use FakeTransaction in DemoTest
    
    * Use Transaction.to_map/1 in TransactionTest
    
    * Remove Mock from dependencies
    
    * Move AppSignal-specific mock modules into their own subdirectory

--- mix.exs
+++ mix.exs
@@ -122,7 +122,6 @@ defmodule Appsignal.Mixfile do
       {:decorator, "~> 1.2.3"},
       {:plug, ">= 1.1.0", optional: true},
       {:phoenix, ">= 1.2.0", optional: true, only: [:prod, :test_phoenix, :dev]},
-      {:mock, "~> 0.3.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:bypass, "~> 0.6.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:plug_cowboy, "~> 1.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:ex_doc, "~> 0.12", only: :dev, runtime: false},
--- test/mix/helpers_test.exs
+++ test/mix/helpers_test.exs
@@ -1,22 +1,21 @@
 :code.delete(Mix.Appsignal.Helper)
 Application.put_env(:appsignal, :os, FakeOS)
+Application.put_env(:appsignal, :mix_system, FakeSystem)
 {_, _} = Code.eval_file("mix_helpers.exs")
 
 defmodule Mix.Appsignal.HelperTest do
   use ExUnit.Case
-  import Mock
   import AppsignalTest.Utils
 
   setup do
+    {:ok, fake_system} = FakeSystem.start_link()
     {:ok, fake_os} = FakeOS.start_link()
-    [fake_os: fake_os]
+    [fake_os: fake_os, fake_system: fake_system]
   end
 
   describe ".agent_platform" do
     test "agent_platform returns libc build when the system detection doesn't work" do
-      with_mock System, [:passthrough], cmd: fn _, _, _ -> raise "oh no!" end do
-        assert Mix.Appsignal.Helper.agent_platform() == "linux"
-      end
+      assert Mix.Appsignal.Helper.agent_platform() == "linux"
     end
 
     test "returns the musl build when using the APPSIGNAL_BUILD_FOR_MUSL env var" do
@@ -25,58 +24,64 @@ defmodule Mix.Appsignal.HelperTest do
       end)
     end
 
-    test "returns the musl build when on a musl system" do
-      with_mock System, [:passthrough],
-        cmd: fn _, _, _ -> {"musl libc (x86_64)\nVersion 1.1.16", 1} end do
-        assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
-      end
+    test "returns the musl build when on a musl system", %{fake_system: fake_system} do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"musl libc (x86_64)\nVersion 1.1.16", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
     end
 
-    test "returns the libc build when on a libc linux system" do
-      with_mocks([
-        {System, [:passthrough],
-         [cmd: fn _, _, _ -> {"ldd (Debian GLIBC 2.15-18+deb8u7) 2.15", 1} end]}
-      ]) do
-        assert Mix.Appsignal.Helper.agent_platform() == "linux"
-      end
+    test "returns the libc build when on a libc linux system", %{fake_system: fake_system} do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd (Debian GLIBC 2.15-18+deb8u7) 2.15", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux"
     end
 
-    test "returns the musl build when on an old libc linux system" do
-      with_mocks([
-        {System, [:passthrough],
-         [cmd: fn _, _, _ -> {"ldd (Debian GLIBC 2.14-18+deb8u7) 2.14", 1} end]}
-      ]) do
-        assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
-      end
+    test "returns the musl build when on an old libc linux system", %{fake_system: fake_system} do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd (Debian GLIBC 2.14-18+deb8u7) 2.14", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
     end
 
-    test "returns the musl build when on a very old libc linux system" do
-      with_mocks([
-        {System, [:passthrough],
-         [cmd: fn _, _, _ -> {"ldd (Debian GLIBC 2.5-18+deb8u7) 2.5", 1} end]}
-      ]) do
-        assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
-      end
+    test "returns the musl build when on a very old libc linux system", %{
+      fake_system: fake_system
+    } do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd (Debian GLIBC 2.5-18+deb8u7) 2.5", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
     end
 
-    test "returns the darwin build when on a darwin system", %{fake_os: fake_os} do
+    test "returns the darwin build when on a darwin system", %{
+      fake_os: fake_os,
+      fake_system: fake_system
+    } do
       FakeOS.update(fake_os, :type, {:unix, :darwin})
 
-      with_mocks([
-        {System, [:passthrough], [cmd: fn _, _, _ -> {"ldd: command not found", 1} end]}
-      ]) do
-        assert Mix.Appsignal.Helper.agent_platform() == "darwin"
-      end
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd: command not found", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "darwin"
     end
 
-    test "returns the darwin build when on a freebsd system", %{fake_os: fake_os} do
+    test "returns the darwin build when on a freebsd system", %{
+      fake_os: fake_os,
+      fake_system: fake_system
+    } do
       FakeOS.update(fake_os, :type, {:unix, :freebsd})
 
-      with_mocks([
-        {System, [:passthrough], [cmd: fn _, _, _ -> {"ldd: illegal option -- -", 1} end]}
-      ]) do
-        assert Mix.Appsignal.Helper.agent_platform() == "freebsd"
-      end
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd: illegal option -- -", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "freebsd"
     end
   end
 end
