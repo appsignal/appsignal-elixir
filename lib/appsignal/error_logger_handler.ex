846b1324772aa0c39420c21bb4f513451bce42fb
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-02-07 18:32:06 +0100

    Remove ErrorLoggerHandler.match_report/1

--- lib/appsignal/error_logger_handler.ex
+++ lib/appsignal/error_logger_handler.ex
@@ -7,12 +7,17 @@ defmodule Appsignal.ErrorLoggerHandler do
   end
 
   def handle_event({:error_report, _gleader, {pid, :crash_report, [report | _]}}, state) do
-    case match_report(report) do
-      {error, stack} ->
-        ErrorHandler.handle_error(pid, error, stack)
-
-      _ ->
-        :ok
+    try do
+      {_kind, error, stack} = report[:error_info]
+      ErrorHandler.handle_error(pid, error, stack)
+    rescue
+      exception ->
+        Logger.warn(fn ->
+          """
+          AppSignal: Failed to match error report: #{Exception.message(exception)}
+          #{inspect(report[:error_info])}
+          """
+        end)
     end
 
     {:ok, state}
@@ -24,22 +29,5 @@ defmodule Appsignal.ErrorLoggerHandler do
 
   def handle_info(_, state) do
     {:ok, state}
-  end
-
-  defp match_report(report) do
-    try do
-      {_kind, error, stack} = report[:error_info]
-      {error, stack}
-    rescue
-      exception ->
-        Logger.warn(fn ->
-          """
-          AppSignal: Failed to match error report: #{Exception.message(exception)}
-          #{inspect(report[:error_info])}
-          """
-        end)
-
-        :nomatch
-    end
   end
 end
