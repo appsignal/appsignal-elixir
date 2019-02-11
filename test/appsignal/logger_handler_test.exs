bf11746505eef799e041fb595873b541fa2c4705
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-02-11 14:16:21 +0100

    Only define LoggerHandlerTest on OTP >= 21

--- test/appsignal/logger_handler_test.exs
+++ test/appsignal/logger_handler_test.exs
@@ -1,53 +1,57 @@
-defmodule Appsignal.LoggerHandlerTest do
-  use ExUnit.Case, async: false
-  alias Appsignal.{Transaction, FakeTransaction}
+if System.otp_release() >= "21" do
+  defmodule Appsignal.LoggerHandlerTest do
+    use ExUnit.Case, async: false
+    alias Appsignal.{Transaction, FakeTransaction}
 
-  setup do
-    Appsignal.remove_report_handler()
-    :logger.add_handler(:appsignal, Appsignal.LoggerHandler, %{})
+    setup do
+      Appsignal.remove_report_handler()
+      :logger.add_handler(:appsignal, Appsignal.LoggerHandler, %{})
 
-    on_exit(fn ->
-      :logger.remove_handler(:appsignal)
-      Appsignal.add_report_handler()
-    end)
-
-    {:ok, fake_transaction} = FakeTransaction.start_link()
-    [fake_transaction: fake_transaction]
-  end
-
-  test "handles an error report", %{fake_transaction: fake_transaction} do
-    pid =
-      :proc_lib.spawn(fn ->
-        self()
-        |> inspect
-        |> FakeTransaction.start(:http_request)
-        |> FakeTransaction.set_action("AppsignalErrorHandlerTest#test")
-
-        :erlang.error(:error_http_request)
+      on_exit(fn ->
+        :logger.remove_handler(:appsignal)
+        Appsignal.add_report_handler()
       end)
 
-    :timer.sleep(20)
+      {:ok, fake_transaction} = FakeTransaction.start_link()
+      [fake_transaction: fake_transaction]
+    end
 
-    [{transaction, reason, _message, _stack}] = FakeTransaction.errors(fake_transaction)
+    test "handles an error report", %{fake_transaction: fake_transaction} do
+      pid =
+        :proc_lib.spawn(fn ->
+          self()
+          |> inspect
+          |> FakeTransaction.start(:http_request)
+          |> FakeTransaction.set_action("AppsignalErrorHandlerTest#test")
 
-    assert transaction.id == inspect(pid)
-    assert reason == ":error_http_request"
-  end
+          :erlang.error(:error_http_request)
+        end)
 
-  test "does not send error reports for ignored processes", %{fake_transaction: fake_transaction} do
-    :proc_lib.spawn(fn ->
-      Appsignal.TransactionRegistry.ignore(self())
-      :timer.sleep(50)
+      :timer.sleep(20)
 
-      :erlang.error(:error_ignored)
-    end)
+      [{transaction, reason, _message, _stack}] = FakeTransaction.errors(fake_transaction)
 
-    :timer.sleep(100)
+      assert transaction.id == inspect(pid)
+      assert reason == ":error_http_request"
+    end
 
-    refute fake_transaction
-           |> FakeTransaction.errors()
-           |> Enum.any?(fn error ->
-             match?({%Transaction{}, ":error_ignored", _, _}, error)
-           end)
+    test "does not send error reports for ignored processes", %{
+      fake_transaction: fake_transaction
+    } do
+      :proc_lib.spawn(fn ->
+        Appsignal.TransactionRegistry.ignore(self())
+        :timer.sleep(50)
+
+        :erlang.error(:error_ignored)
+      end)
+
+      :timer.sleep(100)
+
+      refute fake_transaction
+             |> FakeTransaction.errors()
+             |> Enum.any?(fn error ->
+               match?({%Transaction{}, ":error_ignored", _, _}, error)
+             end)
+    end
   end
 end
