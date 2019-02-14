24bca4a626e78c527455aaa8e3a4633e3b377de8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-02-14 10:57:11 +0100

    Use :logger instead of :error_logger on OTP >= 21 (#454)
    * Use ErrorLoggerHandler as :error_logger report handler
    
    * Move :error_logger handling to ErrorLoggerHandler
    
    * Move Transaction lookup back to ErrorHandler
    
    * Remove ErrorLoggerHandler.match_report/1
    
    * Hide deprecated callbacks from ErrorHandler
    
    * Add @moduledoc to ErrorLoggerHandler, hide methods from docs
    
    * Remove alias from ErrorLoggerHandler
    
    * Deprecated ErrorHandler.handle_event/2
    
    * Add Appsignal.LoggerHandler to handle :logger reports
    
    * Add @moduledoc to LoggerHandler
    
    * Add Appsignal.(add|remove)_report_handler/0
    
    add_report_handler is used from Appsignal.start/2. Both
    add_report_handler and remove_report_handler are used from
    ErrorLoggerHandlerTest and LoggerHandlerTest.
    
    * Only define LoggerHandlerTest on OTP >= 21
    
    * Move error report setups to (Error)LoggerHandler
    
    * Only define LoggerHandler on OTP >= 21
    
    * Don't refer to LoggerHandler on OTP < 21
    
    * Move report handler to a module attribute
    
    * fixup! Move error report setups to (Error)LoggerHandler

--- lib/appsignal.ex
+++ lib/appsignal.ex
@@ -23,6 +23,12 @@ defmodule Appsignal do
                  Appsignal.Transaction
                )
 
+  if System.otp_release() >= "21" do
+    @report_handler Appsignal.LoggerHandler
+  else
+    @report_handler Appsignal.ErrorLoggerHandler
+  end
+
   @doc """
   Application callback function
   """
@@ -30,8 +36,7 @@ defmodule Appsignal do
     import Supervisor.Spec, warn: false
 
     initialize()
-
-    :error_logger.add_report_handler(Appsignal.ErrorHandler)
+    add_report_handler()
 
     children = [
       worker(Appsignal.TransactionRegistry, [])
@@ -94,6 +99,12 @@ defmodule Appsignal do
         )
     end
   end
+
+  @doc false
+  def add_report_handler, do: @report_handler.add()
+
+  @doc false
+  def remove_report_handler, do: @report_handler.remove()
 
   @doc """
   Set a gauge for a measurement of some metric.
