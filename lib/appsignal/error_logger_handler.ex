3fc6ddfd2d92bdfbb2e7e2ab761eae6535f438de
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-02-14 10:57:11 +0100

    Use :logger instead of :error_logger on OTP >= 21 (#454)
    * Use ErrorLoggerHandler as :error_logger report handler
    
    * Move :error_logger handling to ErrorLoggerHandler
    
    * Move Transaction lookup back to ErrorHandler
    
    * Remove ErrorLoggerHandler.match_report/1
    
    * Hide deprecated callbacks from ErrorHandler
    
    * Add @moduledoc to ErrorLoggerHandler, hide methods from docs
    
    * Remove alias from ErrorLoggerHandler
    
    * Deprecated ErrorHandler.handle_event/2
    
    * Add Appsignal.LoggerHandler to handle :logger reports
    
    * Add @moduledoc to LoggerHandler
    
    * Add Appsignal.(add|remove)_report_handler/0
    
    add_report_handler is used from Appsignal.start/2. Both
    add_report_handler and remove_report_handler are used from
    ErrorLoggerHandlerTest and LoggerHandlerTest.
    
    * Only define LoggerHandlerTest on OTP >= 21
    
    * Move error report setups to (Error)LoggerHandler
    
    * Only define LoggerHandler on OTP >= 21
    
    * Don't refer to LoggerHandler on OTP < 21
    
    * Move report handler to a module attribute
    
    * fixup! Move error report setups to (Error)LoggerHandler

---
+++ lib/appsignal/error_logger_handler.ex
@@ -0,0 +1,59 @@
+defmodule Appsignal.ErrorLoggerHandler do
+  @moduledoc """
+  Error handler to send crash reports to AppSignal.
+
+  AppSignal automatically adds `Appsignal.ErrorLoggerHandler` to Erlang's
+  `:error_logger` as a report handler to receive error reports. It extracts the
+  error and stacktrace from the report and sends it over to
+  `Appsignal.ErrorHandler` to be reported to AppSignal.
+  """
+
+  require Logger
+
+  @doc """
+  Add `Appsignal.ErrorLoggerHandler` as a report handler for `:error_logger`.
+  """
+  def add do
+    :error_logger.add_report_handler(Appsignal.ErrorLoggerHandler)
+  end
+
+  @doc """
+  Remove `AppSignal.ErrorLoggerHandler` report handler from `:error_logger`.
+  """
+  def remove do
+    :error_logger.delete_report_handler(Appsignal.ErrorLoggerHandler)
+  end
+
+  @doc false
+  def init(state) do
+    {:ok, state}
+  end
+
+  @doc false
+  def handle_event({:error_report, _gleader, {pid, :crash_report, [report | _]}}, state) do
+    try do
+      {_kind, error, stack} = report[:error_info]
+      Appsignal.ErrorHandler.handle_error(pid, error, stack)
+    rescue
+      exception ->
+        Logger.warn(fn ->
+          """
+          AppSignal: Failed to match error report: #{Exception.message(exception)}
+          #{inspect(report[:error_info])}
+          """
+        end)
+    end
+
+    {:ok, state}
+  end
+
+  @doc false
+  def handle_event(_event, state) do
+    {:ok, state}
+  end
+
+  @doc false
+  def handle_info(_, state) do
+    {:ok, state}
+  end
+end
