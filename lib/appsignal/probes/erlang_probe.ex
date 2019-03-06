92c97c2de81ea1f1b9b0e3e9da25afe859797074
Jeroen van Baarsen <jeroenvanbaarsen@gmail.com>
2019-03-06 16:00:49 +0100

    Add Erlang Probe
    Currently we support the following set of data:
    
    * IO, tagged as Input, and Output
    * Scheduler Metrics, tagged as Total, and Online
    * Process metrics, tagged as Limit, and Count
    * Memory, tagged as: total, processes, processes_used, system, atom,
      atom_used, binary, code, ets
    
    In the future we might want to expand this with Scheduler utilization
    and maybe more memory allocations.

---
+++ lib/appsignal/probes/erlang_probe.ex
@@ -0,0 +1,37 @@
+defmodule Appsignal.Probes.ErlangProbe do
+  def call do
+    io_metrics()
+    scheduler_metrics()
+    process_metrics()
+    memory_metrics()
+  end
+
+  defp io_metrics do
+    {{:input, input}, {:output, output}} = :erlang.statistics(:io)
+    Appsignal.set_gauge("erlang_io", Kernel.div(input, 1024), %{kind: "input"})
+    Appsignal.set_gauge("erlang_io", Kernel.div(output, 1024), %{kind: "output"})
+  end
+
+  defp scheduler_metrics do
+    Appsignal.set_gauge("erlang_schedulers", :erlang.system_info(:schedulers), %{kind: "Total"})
+
+    Appsignal.set_gauge(
+      "erlang_schedulers",
+      :erlang.system_info(:schedulers_online),
+      %{kind: "Online"}
+    )
+  end
+
+  defp process_metrics() do
+    Appsignal.set_gauge("erlang_processes", :erlang.system_info(:process_limit), %{kind: "Limit"})
+    Appsignal.set_gauge("erlang_processes", :erlang.system_info(:process_count), %{kind: "Count"})
+  end
+
+  defp memory_metrics() do
+    memory = :erlang.memory()
+
+    Enum.each(memory, fn {key, value} ->
+      Appsignal.set_gauge("erlang_memory", Kernel.div(value, 1024), %{kind: key})
+    end)
+  end
+end
