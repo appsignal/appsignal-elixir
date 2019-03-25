appsignal/appsignal-elixir

commit f319d28d9f4b3f6cd427aae973c7b6caa1d4886b

Drop Mock dependency, use FakeTransaction, FakeSystem and Transaction.to_map/1 instead (#437)
* Add FakeSystem, use it instead of Mock in Appsignal.HelperTest

* Use FakeTransaction in AppsignalHelpersTest

* Use FakeTransaction in DecoratorTest

* Use FakeTransaction in DemoTest

* Use Transaction.to_map/1 in TransactionTest

* Remove Mock from dependencies

* Move AppSignal-specific mock modules into their own subdirectory

lib/appsignal/instrumentation/decorators.ex
@@ -36,6 +36,8 @@ defmodule Appsignal.Instrumentation.Decorators do
     transaction_event: 1,
     channel_action: 0
 
+  @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
+
   @doc false
   def transaction(body, context) do
     transaction(:http_request, body, context)
@@ -44,19 +46,11 @@ defmodule Appsignal.Instrumentation.Decorators do
   @doc false
   def transaction(namespace, body, context) do
     quote do
-      transaction =
-        Appsignal.Transaction.start(
-          Appsignal.Transaction.generate_id(),
-          unquote(namespace)
-        )
-        |> Appsignal.Transaction.set_action(unquote("#{context.module}##{context.name}"))
-
-      result = unquote(body)
-
-      Appsignal.Transaction.finish(transaction)
-      :ok = Appsignal.Transaction.complete(transaction)
-
-      result
+      Appsignal.Instrumentation.Decorators.in_transaction(
+        unquote(namespace),
+        unquote("#{context.module}##{context.name}"),
+        unquote(body)
+      )
     end
   end
 
@@ -91,5 +85,20 @@ defmodule Appsignal.Instrumentation.Decorators do
         fn -> unquote(body) end
       )
     end
+  end
+
+  @doc false
+  def in_transaction(namespace, action, body) do
+    transaction =
+      @transaction.generate_id()
+      |> @transaction.start(namespace)
+      |> @transaction.set_action(action)
+
+    result = body
+
+    @transaction.finish(transaction)
+    :ok = @transaction.complete(transaction)
+
+    result
   end
 end

test/appsignal/instrumentation/decorator_test.exs
@@ -43,102 +43,90 @@ end
 
 defmodule Appsignal.Instrumentation.DecoratorsTest do
   use ExUnit.Case
-  import Mock
+  alias Appsignal.FakeTransaction
 
-  alias Appsignal.Transaction
+  setup do
+    {:ok, fake_transaction} = FakeTransaction.start_link()
+    [fake_transaction: fake_transaction]
+  end
 
-  test_with_mock "instrument module function", Appsignal.Transaction, [:passthrough], [] do
-    t = Transaction.start("bar", :http_request)
+  test "instrument module function", %{fake_transaction: fake_transaction} do
+    transaction = FakeTransaction.start("123", :http_request)
     UsingAppsignalDecorators.bar(123)
-    assert called(Transaction.start_event(t))
+    assert [^transaction, ^transaction] = FakeTransaction.started_events(fake_transaction)
 
-    assert called(
-             Transaction.finish_event(t, "bar", "Elixir.UsingAppsignalDecorators.bar", "", 0)
-           )
-
-    assert called(
-             Transaction.finish_event(
-               t,
-               "nested",
-               "Elixir.UsingAppsignalDecorators.nested",
-               "",
-               0
-             )
-           )
+    assert [
+             %{
+               body: "",
+               body_format: 0,
+               name: "bar",
+               title: "Elixir.UsingAppsignalDecorators.bar",
+               transaction: ^transaction
+             },
+             %{
+               body: "",
+               body_format: 0,
+               name: "nested",
+               title: "Elixir.UsingAppsignalDecorators.nested",
+               transaction: ^transaction
+             }
+           ] = FakeTransaction.finished_events(fake_transaction)
   end
 
-  test_with_mock "instrument transaction", Appsignal.Transaction, [:passthrough], [] do
+  test "instrument transaction", %{fake_transaction: fake_transaction} do
     UsingAppsignalDecorators.transaction()
-    assert called(Transaction.start(:_, :http_request))
+    assert [{"123", :http_request}] = FakeTransaction.started_transactions(fake_transaction)
 
-    assert called(
-             Appsignal.Transaction.set_action(:_, "Elixir.UsingAppsignalDecorators#transaction")
-           )
+    assert "Elixir.UsingAppsignalDecorators#transaction" =
+             FakeTransaction.action(fake_transaction)
 
-    assert called(Appsignal.Transaction.finish(:_))
-    assert called(Appsignal.Transaction.complete(:_))
+    assert [transaction] = FakeTransaction.finished_transactions(fake_transaction)
+    assert [^transaction] = FakeTransaction.completed_transactions(fake_transaction)
   end
 
-  test_with_mock "instrument background transaction", Appsignal.Transaction, [:passthrough], [] do
+  test "instrument background transaction", %{fake_transaction: fake_transaction} do
     UsingAppsignalDecorators.background_transaction()
-    assert called(Transaction.start(:_, :background_job))
+    assert [{"123", :background_job}] = FakeTransaction.started_transactions(fake_transaction)
 
-    assert called(
-             Appsignal.Transaction.set_action(
-               :_,
-               "Elixir.UsingAppsignalDecorators#background_transaction"
-             )
-           )
+    assert "Elixir.UsingAppsignalDecorators#background_transaction" =
+             FakeTransaction.action(fake_transaction)
 
-    assert called(Appsignal.Transaction.finish(:_))
-    assert called(Appsignal.Transaction.complete(:_))
+    assert [transaction] = FakeTransaction.finished_transactions(fake_transaction)
+    assert [^transaction] = FakeTransaction.completed_transactions(fake_transaction)
   end
 
-  test_with_mock "instrument transaction with return value",
-                 Appsignal.Transaction,
-                 [:passthrough],
-                 [] do
+  test "instrument transaction with return value", %{fake_transaction: fake_transaction} do
     result = UsingAppsignalDecorators.transaction_with_return_value(123)
     assert 246 == result
-    assert called(Transaction.start(:_, :http_request))
+    assert [{"123", :http_request}] = FakeTransaction.started_transactions(fake_transaction)
 
-    assert called(
-             Appsignal.Transaction.set_action(
-               :_,
-               "Elixir.UsingAppsignalDecorators#transaction_with_return_value"
-             )
-           )
+    assert "Elixir.UsingAppsignalDecorators#transaction_with_return_value" =
+             FakeTransaction.action(fake_transaction)
 
-    assert called(Appsignal.Transaction.finish(:_))
-    assert called(Appsignal.Transaction.complete(:_))
+    assert [transaction] = FakeTransaction.finished_transactions(fake_transaction)
+    assert [^transaction] = FakeTransaction.completed_transactions(fake_transaction)
   end
 
-  test_with_mock "instrument module function with category",
-                 Appsignal.Transaction,
-                 [:passthrough],
-                 [] do
-    t = Transaction.start("bar", :http_request)
+  test "instrument module function with category", %{fake_transaction: fake_transaction} do
+    transaction = FakeTransaction.start("bar", :http_request)
     UsingAppsignalDecoratorsWithCustomNamespaces.bar(123)
-    assert called(Transaction.start_event(t))
+    assert [^transaction, ^transaction] = FakeTransaction.started_events(fake_transaction)
 
-    assert called(
-             Transaction.finish_event(
-               t,
-               "bar.http",
-               "Elixir.UsingAppsignalDecoratorsWithCustomNamespaces.bar",
-               "",
-               0
-             )
-           )
-
-    assert called(
-             Transaction.finish_event(
-               t,
-               "nested.db",
-               "Elixir.UsingAppsignalDecoratorsWithCustomNamespaces.nested",
-               "",
-               0
-             )
-           )
+    assert [
+             %{
+               body: "",
+               body_format: 0,
+               name: "bar.http",
+               title: "Elixir.UsingAppsignalDecoratorsWithCustomNamespaces.bar",
+               transaction: ^transaction
+             },
+             %{
+               body: "",
+               body_format: 0,
+               name: "nested.db",
+               title: "Elixir.UsingAppsignalDecoratorsWithCustomNamespaces.nested",
+               transaction: ^transaction
+             }
+           ] = FakeTransaction.finished_events(fake_transaction)
   end
 end

lib/appsignal/demo.ex
@@ -9,8 +9,8 @@ end
 
 defmodule Appsignal.Demo do
   import Appsignal.Instrumentation.Helpers, only: [instrument: 4]
-
   @behaviour Appsignal.DemoBehaviour
+  @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
 
   @doc false
   @spec create_transaction_error_request :: Appsignal.Transaction.t()
@@ -20,7 +20,7 @@ defmodule Appsignal.Demo do
     rescue
       error ->
         create_demo_transaction()
-        |> Appsignal.Transaction.set_error("TestError", error.message, System.stacktrace())
+        |> @transaction.set_error("TestError", error.message, System.stacktrace())
         |> finish_demo_transaction()
     end
   end
@@ -50,21 +50,21 @@ defmodule Appsignal.Demo do
   end
 
   defp create_demo_transaction do
-    Appsignal.Transaction.start(
-      Appsignal.Transaction.generate_id(),
+    @transaction.start(
+      @transaction.generate_id(),
       :http_request
     )
-    |> Appsignal.Transaction.set_action("DemoController#hello")
-    |> Appsignal.Transaction.set_meta_data("demo_sample", "true")
-    |> Appsignal.Transaction.set_sample_data(
+    |> @transaction.set_action("DemoController#hello")
+    |> @transaction.set_meta_data("demo_sample", "true")
+    |> @transaction.set_sample_data(
       "environment",
       %{request_path: "/hello", method: "GET"}
     )
   end
 
   defp finish_demo_transaction(transaction) do
-    Appsignal.Transaction.finish(transaction)
-    :ok = Appsignal.Transaction.complete(transaction)
+    @transaction.finish(transaction)
+    :ok = @transaction.complete(transaction)
 
     transaction
   end

lib/appsignal/instrumentation/helpers.ex
@@ -4,8 +4,8 @@ defmodule Appsignal.Instrumentation.Helpers do
   """
 
   alias Appsignal.{Transaction, TransactionRegistry}
-
   @type instrument_arg :: Transaction.t() | Plug.Conn.t() | pid() | nil
+  @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
 
   @doc """
   Execute the given function in start / finish event calls in the current
@@ -58,9 +58,9 @@ defmodule Appsignal.Instrumentation.Helpers do
   end
 
   def instrument(%Transaction{} = transaction, name, title, body, body_format, function) do
-    Transaction.start_event(transaction)
+    @transaction.start_event(transaction)
     result = function.()
-    Transaction.finish_event(transaction, name, title, body, body_format)
+    @transaction.finish_event(transaction, name, title, body, body_format)
     result
   end
 

mix.exs
@@ -122,7 +122,6 @@ defmodule Appsignal.Mixfile do
       {:decorator, "~> 1.2.3"},
       {:plug, ">= 1.1.0", optional: true},
       {:phoenix, ">= 1.2.0", optional: true, only: [:prod, :test_phoenix, :dev]},
-      {:mock, "~> 0.3.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:bypass, "~> 0.6.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:plug_cowboy, "~> 1.0", only: [:test, :test_phoenix, :test_no_nif]},
       {:ex_doc, "~> 0.12", only: :dev, runtime: false},

mix_helpers.exs
@@ -5,6 +5,7 @@ defmodule Mix.Appsignal.Helper do
   Helper functions for downloading and compiling the AppSignal agent library.
   """
   @os Application.get_env(:appsignal, :os, :os)
+  @system Application.get_env(:appsignal, :mix_system, System)
 
   @max_retries 5
 
@@ -283,7 +284,7 @@ defmodule Mix.Appsignal.Helper do
 
   defp agent_platform_by_ldd_version do
     try do
-      {output, _} = System.cmd("ldd", ["--version"], stderr_to_stdout: true)
+      {output, _} = @system.cmd("ldd", ["--version"], stderr_to_stdout: true)
 
       case String.contains?(output, "musl") do
         true ->

test/appsignal/demo_test.exs
@@ -1,44 +1,69 @@
 defmodule AppsignalDemoTest do
-  use ExUnit.Case, async: true
-  import Mock
+  use ExUnit.Case
+  alias Appsignal.FakeTransaction
 
-  test_with_mock "sends a demonstration error", Appsignal.Transaction, [:passthrough], [] do
-    t = Appsignal.Demo.create_transaction_error_request()
-
-    assert called(Appsignal.Transaction.set_action(t, "DemoController#hello"))
-
-    assert called(
-             Appsignal.Transaction.set_error(
-               t,
-               "TestError",
-               "Hello world! This is an error used for demonstration purposes.",
-               :_
-             )
-           )
-
-    assert called(Appsignal.Transaction.set_meta_data(t, "demo_sample", "true"))
-    assert called(Appsignal.Transaction.finish(t))
-    assert called(Appsignal.Transaction.complete(t))
+  setup do
+    {:ok, fake_transaction} = FakeTransaction.start_link()
+    [fake_transaction: fake_transaction]
   end
 
-  test_with_mock "sends a performance issue", Appsignal.Transaction, [:passthrough], [] do
-    t = Appsignal.Demo.create_transaction_performance_request()
+  test "sends a demonstration error", %{fake_transaction: fake_transaction} do
+    transaction = Appsignal.Demo.create_transaction_error_request()
 
-    assert called(Appsignal.Transaction.set_action(t, "DemoController#hello"))
-    assert called(Appsignal.Transaction.set_meta_data(t, "demo_sample", "true"))
-    assert called(Appsignal.Transaction.start_event(t))
+    assert "DemoController#hello" = FakeTransaction.action(fake_transaction)
 
-    assert called(
-             Appsignal.Transaction.finish_event(
-               t,
-               "render.phoenix_template",
-               "Rendering something slow",
-               "",
-               0
-             )
-           )
+    assert [
+             {^transaction, "TestError",
+              "Hello world! This is an error used for demonstration purposes.", _stacktrace}
+           ] = FakeTransaction.errors(fake_transaction)
 
-    assert called(Appsignal.Transaction.finish(t))
-    assert called(Appsignal.Transaction.complete(t))
+    assert %{"demo_sample" => "true"} = FakeTransaction.metadata(fake_transaction)
+
+    assert [transaction] = FakeTransaction.finished_transactions(fake_transaction)
+    assert [^transaction] = FakeTransaction.completed_transactions(fake_transaction)
+  end
+
+  test "sends a performance issue", %{fake_transaction: fake_transaction} do
+    transaction = Appsignal.Demo.create_transaction_performance_request()
+
+    assert "DemoController#hello" = FakeTransaction.action(fake_transaction)
+    assert %{"demo_sample" => "true"} = FakeTransaction.metadata(fake_transaction)
+
+    assert [^transaction, ^transaction, ^transaction, ^transaction] =
+             FakeTransaction.started_events(fake_transaction)
+
+    assert [
+             %{
+               body: "",
+               body_format: 0,
+               name: "render.phoenix_template",
+               title: "Rendering something slow",
+               transaction: ^transaction
+             },
+             %{
+               body: "",
+               body_format: 0,
+               name: "render.phoenix_template",
+               title: "Rendering something slow",
+               transaction: ^transaction
+             },
+             %{
+               body: "",
+               body_format: 0,
+               name: "query.ecto",
+               title: "Slow query",
+               transaction: ^transaction
+             },
+             %{
+               body: "",
+               body_format: 0,
+               name: "query.ecto",
+               title: "Slow query",
+               transaction: ^transaction
+             }
+           ] = FakeTransaction.finished_events(fake_transaction)
+
+    assert [transaction] = FakeTransaction.finished_transactions(fake_transaction)
+    assert [^transaction] = FakeTransaction.completed_transactions(fake_transaction)
   end
 end

test/appsignal/instrumentation/helpers_test.exs
@@ -1,22 +1,29 @@
 defmodule AppsignalHelpersTest do
   use ExUnit.Case, async: false
+  alias Appsignal.{Instrumentation.Helpers, FakeTransaction}
 
-  import Mock
-
-  alias Appsignal.{Transaction, Instrumentation.Helpers}
-
-  test_with_mock "instrument with transaction", Appsignal.Transaction, [:passthrough], [] do
-    t = Transaction.start("foo", :http_request)
-    call_instrument(t)
-    assert called(Transaction.start_event(t))
-    assert called(Transaction.finish_event(t, "name", "title", "", 0))
+  setup do
+    {:ok, fake_transaction} = FakeTransaction.start_link()
+    [fake_transaction: fake_transaction]
   end
 
-  test_with_mock "instrument with pid", Appsignal.Transaction, [:passthrough], [] do
-    t = Transaction.start("bar", :http_request)
+  test "instrument with transaction", %{fake_transaction: fake_transaction} do
+    transaction = FakeTransaction.start("123", :http_request)
+    call_instrument(transaction)
+
+    assert [^transaction] = FakeTransaction.started_events(fake_transaction)
+
+    assert [%{body: "", body_format: 0, name: "name", title: "title", transaction: ^transaction}] =
+             FakeTransaction.finished_events(fake_transaction)
+  end
+
+  test "instrument with pid", %{fake_transaction: fake_transaction} do
+    transaction = FakeTransaction.start("bar", :http_request)
     call_instrument(self())
-    assert called(Transaction.start_event(t))
-    assert called(Transaction.finish_event(t, "name", "title", "", 0))
+    assert [^transaction] = FakeTransaction.started_events(fake_transaction)
+
+    assert [%{body: "", body_format: 0, name: "name", title: "title", transaction: ^transaction}] =
+             FakeTransaction.finished_events(fake_transaction)
   end
 
   test "instrument with nil" do
@@ -26,7 +33,6 @@ defmodule AppsignalHelpersTest do
   defp call_instrument(arg) do
     r =
       Helpers.instrument(arg, "name", "title", fn ->
-        # some slow function
         :timer.sleep(100)
         :result
       end)

commit 9e5646ec10d894ab275fc83ee8ab4a0686c10069

Make sure events are added to transactions started with decorators
f319d28d9f4b3f6cd427aae973c7b6caa1d4886b introduced a regression where
functions decorated with the `transaction` decorator were run before
their transaction was started. This caused any nested events to be
dropped and results in empty transactions being reported.

This was caused by unquoting the body in the `transaction/3` without
wrapping it in an anonymous function, causing the function to run
immediately and its return value being added as the body parameter in
the `in_transaction/3` function.

This patch adds a regression test to make a function with the
`transaction` decorator that calls a function with the
`transaction_event` decorator produces a transaction with an event, and
wraps the function body in an anonymous function that's called in the
`in_transaction/3` function after starting and before finishing the
transaction.

lib/appsignal/instrumentation/decorators.ex
@@ -49,7 +49,7 @@ defmodule Appsignal.Instrumentation.Decorators do
       Appsignal.Instrumentation.Decorators.in_transaction(
         unquote(namespace),
         unquote("#{context.module}##{context.name}"),
-        unquote(body)
+        fn -> unquote(body) end
       )
     end
   end
@@ -94,7 +94,7 @@ defmodule Appsignal.Instrumentation.Decorators do
       |> @transaction.start(namespace)
       |> @transaction.set_action(action)
 
-    result = body
+    result = body.()
 
     @transaction.finish(transaction)
     :ok = @transaction.complete(transaction)

test/appsignal/instrumentation/decorator_test.exs
@@ -50,6 +50,17 @@ defmodule Appsignal.Instrumentation.DecoratorsTest do
     [fake_transaction: fake_transaction]
   end
 
+  test "instrument transaction with event", %{fake_transaction: fake_transaction} do
+    UsingAppsignalDecorators.transaction()
+
+    assert [{"123", :http_request}] = FakeTransaction.started_transactions(fake_transaction)
+
+    assert [
+             %{title: "Elixir.UsingAppsignalDecorators.bar"},
+             %{title: "Elixir.UsingAppsignalDecorators.nested"}
+           ] = FakeTransaction.finished_events(fake_transaction)
+  end
+
   test "instrument module function", %{fake_transaction: fake_transaction} do
     transaction = FakeTransaction.start("123", :http_request)
     UsingAppsignalDecorators.bar(123)