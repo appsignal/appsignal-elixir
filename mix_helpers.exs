appsignal/appsignal-elixir

commit 7f581c04a122d7f37ddc5fac8f59c36f6646f2f9

ldd version error handling (#487)
* ldd_version_output/0 returns ok- or error tuples

* agent_platform/0 handles cases where ldd --version doesn't return a version

* Add catch-all clause to extract_ldd_version/1

mix_helpers.exs
@@ -346,13 +346,18 @@ defmodule Mix.Appsignal.Helper do
   end
 
   defp library_dependencies do
-    ldd_version_output = ldd_version_output()
-    case extract_ldd_version(ldd_version_output) do
-      nil ->
-        %{}
+    case ldd_version_output() do
+      {:ok, output} ->
+        case extract_ldd_version(output) do
+          nil ->
+            %{}
 
-      ldd_version ->
-        %{libc: ldd_version}
+          ldd_version ->
+            %{libc: ldd_version}
+        end
+
+      _ ->
+        %{}
     end
   end
 
@@ -374,41 +379,52 @@ defmodule Mix.Appsignal.Helper do
 
   defp agent_platform_by_ldd_version do
     case ldd_version_output() do
-      nil ->
-        "linux"
-
-      output ->
+      {:ok, output} ->
         case String.contains?(output, "musl") do
           true ->
             "linux-musl"
 
           false ->
-            ldd_version = extract_ldd_version(output)
+            case extract_ldd_version(output) do
+              nil ->
+                "linux"
 
-            case Version.compare("#{ldd_version}.0", "2.15.0") do
-              :lt -> "linux-musl"
-              _ -> "linux"
+              ldd_version ->
+                case Version.compare("#{ldd_version}.0", "2.15.0") do
+                  :lt -> "linux-musl"
+                  _ -> "linux"
+                end
             end
         end
+
+      _ ->
+        "linux"
     end
-  rescue
-    _ -> "linux"
   end
 
   # Fetches the libc version number from the `ldd` command
   # If `ldd` is not found it returns `nil`
   defp ldd_version_output do
-    {output, _} = @system.cmd("ldd", ["--version"], stderr_to_stdout: true)
-    output
+    case @system.cmd("ldd", ["--version"], stderr_to_stdout: true) do
+      {output, 0} ->
+        {:ok, output}
+
+      {output, _} ->
+        {:error, output}
+    end
   rescue
-    _ -> nil
+    exception ->
+      {:error, exception}
   end
 
-  defp extract_ldd_version(nil), do: nil
-
-  defp extract_ldd_version(ldd_output) do
-    List.first(Regex.run(~r/\d+\.\d+/, ldd_output))
+  defp extract_ldd_version(ldd_output) when is_binary(ldd_output) do
+    case Regex.run(~r/\d+\.\d+/, ldd_output) do
+      [version | _tail] -> version
+      _ -> nil
+    end
   end
+
+  defp extract_ldd_version(_), do: nil
 
   defp initial_report do
     {_, os} = :os.type()

test/mix/helpers_test.exs
@@ -26,7 +26,7 @@ defmodule Mix.Appsignal.HelperTest do
 
     test "returns the musl build when on a musl system", %{fake_system: fake_system} do
       FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
-        {"musl libc (x86_64)\nVersion 1.1.16", 1}
+        {"musl libc (x86_64)\nVersion 1.1.16", 0}
       end)
 
       assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
@@ -34,7 +34,7 @@ defmodule Mix.Appsignal.HelperTest do
 
     test "returns the libc build when on a libc linux system", %{fake_system: fake_system} do
       FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
-        {"ldd (Debian GLIBC 2.15-18+deb8u7) 2.15", 1}
+        {"ldd (Debian GLIBC 2.15-18+deb8u7) 2.15", 0}
       end)
 
       assert Mix.Appsignal.Helper.agent_platform() == "linux"
@@ -42,7 +42,7 @@ defmodule Mix.Appsignal.HelperTest do
 
     test "returns the musl build when on an old libc linux system", %{fake_system: fake_system} do
       FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
-        {"ldd (Debian GLIBC 2.14-18+deb8u7) 2.14", 1}
+        {"ldd (Debian GLIBC 2.14-18+deb8u7) 2.14", 0}
       end)
 
       assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
@@ -52,10 +52,28 @@ defmodule Mix.Appsignal.HelperTest do
       fake_system: fake_system
     } do
       FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
-        {"ldd (Debian GLIBC 2.5-18+deb8u7) 2.5", 1}
+        {"ldd (Debian GLIBC 2.5-18+deb8u7) 2.5", 0}
       end)
 
       assert Mix.Appsignal.Helper.agent_platform() == "linux-musl"
+    end
+
+    test "agent_platform returns libc build when ldd doesn't return a version number", %{
+      fake_system: fake_system
+    } do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"", 0}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux"
+    end
+
+    test "defaults to the libc build when ldd fails", %{fake_system: fake_system} do
+      FakeSystem.update(fake_system, :cmd, fn _, _, _ ->
+        {"ldd: command not found", 1}
+      end)
+
+      assert Mix.Appsignal.Helper.agent_platform() == "linux"
     end
 
     test "returns the darwin build when on a darwin system", %{

commit 115ab418c4ce3415eb10768a2cb8c8b1ec04dd9a

Fix musl detection on installation
The command `ldd --version` always returns exit code `1`.
This is because `--version` is not a supported option, it always prints
the version number when it runs into an issue when starting.
It's mostly there so we know what it's doing.

Because it always returns exit code `1`, don't listen to the exit code.
Only check if the command does not exist. That raises an error which we
catch instead and only then return an error tuple.

This behavior broke in #487

mix_helpers.exs
@@ -410,11 +410,8 @@ defmodule Mix.Appsignal.Helper do
   # If `ldd` is not found it returns `nil`
   defp ldd_version_output do
     case @system.cmd("ldd", ["--version"], stderr_to_stdout: true) do
-      {output, 0} ->
-        {:ok, output}
-
       {output, _} ->
-        {:error, output}
+        {:ok, output}
     end
   rescue
     exception ->