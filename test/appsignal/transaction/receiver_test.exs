5fe27cd65450bc24c04fa5b430b72ea1745ee19e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-04 12:46:21 +0100

    Run receiver monitors in receiver process (#525)
    As reported in https://github.com/appsignal/appsignal-elixir/issues/524,
    a regression in version 1.11.0 caused an issue where rows aren't
    properly deleted from the ETS table. This causes slowdowns in
    `Appsignal.TransactionRegistry.pids_and_monitor_references/1`.

---
+++ test/appsignal/transaction/receiver_test.exs
@@ -0,0 +1,22 @@
+defmodule Appsignal.Transaction.ReceiverTest do
+  use ExUnit.Case
+  import AppsignalTest.Utils
+  alias Appsignal.{Transaction, Transaction.Receiver, Transaction.ETS}
+
+  test "monitors a process" do
+    task =
+      %Task{pid: pid} =
+      Task.async(fn ->
+        ETS.insert({self(), %Transaction{}, Receiver.monitor(self())})
+      end)
+
+    Task.await(task)
+
+    assert [{^pid, %Appsignal.Transaction{}, reference}] = ETS.lookup(pid)
+    assert is_reference(reference)
+
+    until(fn ->
+      assert [] = ETS.lookup(pid)
+    end)
+  end
+end
