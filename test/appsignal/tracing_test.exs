6ecc67da1ec62e704dd0d2d813569e3eabac0c51
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-07 17:56:14 +0100

    Add basic test for tracing additions

---
+++ test/appsignal/tracing_test.exs
@@ -0,0 +1,25 @@
+defmodule AppsignalTracingTest do
+  use ExUnit.Case
+
+  test "creates and closes a span with attributes and a child span" do
+    reference =
+      "name"
+      |> Appsignal.Span.create()
+      |> Appsignal.Span.set_attribute("string", "AppsignalTracingTest#action")
+      |> Appsignal.Span.set_attribute("integer", 42)
+
+    {:ok, trace_id} = Appsignal.Span.trace_id(reference)
+    {:ok, span_id} = Appsignal.Span.span_id(reference)
+
+    child =
+      Appsignal.Span.create(
+        List.to_string(trace_id),
+        List.to_string(span_id),
+        "name"
+      )
+
+    Appsignal.Span.close(child)
+
+    Appsignal.Span.close(reference)
+  end
+end
