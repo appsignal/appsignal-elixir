27be700d312e3e7da30c2f02fd61c4f7913171c4
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-12 13:48:20 +0100

    Span.Registry is a duplicate_bag

--- lib/appsignal/span/registry.ex
+++ lib/appsignal/span/registry.ex
@@ -1,15 +1,20 @@
 defmodule Appsignal.Span.Registry do
   alias Appsignal.Span
 
-  @name __MODULE__
   @table :"$appsignal_span_registry"
 
   def start_link do
     Agent.start_link(
       fn ->
-        :ets.new(@table, [:set, :named_table, {:keypos, 1}, :public, {:write_concurrency, true}])
+        :ets.new(@table, [
+          :named_table,
+          :duplicate_bag,
+          :public,
+          read_concurrency: true,
+          write_concurrency: true
+        ])
       end,
-      name: @name
+      name: __MODULE__
     )
   end
 
