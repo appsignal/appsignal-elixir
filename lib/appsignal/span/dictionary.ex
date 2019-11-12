6d3a63952f473be6c8a4ede85e293418ca7d4afc
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-12 15:35:10 +0100

    Stack spans in Dictionary

--- lib/appsignal/span/dictionary.ex
+++ lib/appsignal/span/dictionary.ex
@@ -2,14 +2,24 @@ defmodule Appsignal.Span.Dictionary do
   alias Appsignal.Span
 
   def lookup() do
-    Process.get(:appsignal_span)
+    case Process.get(:appsignal_span) do
+      [span | _] -> span
+      _ -> nil
+    end
   end
 
   def insert(%Span{} = span) do
-    Process.put(:appsignal_span, span)
+    case Process.get(:appsignal_span) do
+      [_ | _] = spans -> Process.put(:appsignal_span, [span | spans])
+      _ -> Process.put(:appsignal_span, [span])
+    end
   end
 
   def delete() do
-    Process.delete(:appsignal_span)
+    case Process.get(:appsignal_span) do
+      [_] -> Process.delete(:appsignal_span)
+      [_ | tail] -> Process.put(:appsignal_span, tail)
+      _ -> nil
+    end
   end
 end
