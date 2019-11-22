1adae6b7c0159b87dd3d9c79ac68231bd07afacd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-22 12:49:08 +0100

    Automatically attach Appsignal.Phoenix.EventHandler

--- lib/appsignal/phoenix/event_handler.ex
+++ lib/appsignal/phoenix/event_handler.ex
@@ -1,6 +1,18 @@
 defmodule Appsignal.Phoenix.EventHandler do
   @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
 
+  def attach do
+    :telemetry.attach_many(
+      "appsignal_phoenix_event_handler",
+      [
+        [:phoenix, :endpoint, :start],
+        [:phoenix, :endpoint, :stop]
+      ],
+      &Appsignal.Phoenix.EventHandler.handle_event/4,
+      nil
+    )
+  end
+
   def handle_event([:phoenix, :endpoint, :start], _measurements, _metadata, _config) do
     @transaction.start_event()
   end
