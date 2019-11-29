e4cb0a3bb35b06e6f92bb7ca725889d6412e7c35
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-11-29 14:19:20 +0100

    Add Appsignal.Utils.module_name/1

---
+++ lib/appsignal/utils.ex
@@ -0,0 +1,19 @@
+defmodule Appsignal.Utils do
+  @doc """
+  Converts module name atoms to strings.
+
+  ## Examples
+
+      iex> Appsignal.Utils.module_name(MyModule)
+      "MyModule"
+      iex> Appsignal.Utils.module_name("MyModule")
+      "MyModule"
+
+  """
+  @spec module_name(atom() | String.t()) :: String.t()
+  def module_name("Elixir." <> module), do: module
+
+  def module_name(module) when is_binary(module), do: module
+
+  def module_name(module), do: module |> to_string() |> module_name()
+end
