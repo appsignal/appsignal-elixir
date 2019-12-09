05e765f6e3755b9cb8f1622d02aebb4e14e7890c
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-12-09 15:21:35 +0100

    Add Span.create/1 regression test

--- test/appsignal/span_test.exs
+++ test/appsignal/span_test.exs
@@ -16,6 +16,20 @@ defmodule AppsignalSpanTest do
     end
   end
 
+  describe ".create/1" do
+    setup do
+      [span: Span.create("test")]
+    end
+
+    test "creates a new span", %{
+      span: %Span{reference: reference, span_id: span_id, trace_id: trace_id}
+    } do
+      assert is_reference(reference)
+      assert is_list(span_id)
+      assert is_list(trace_id)
+    end
+  end
+
   describe ".trace_id/1" do
     test "returns a span's trace_id", %{span: %Span{reference: reference, trace_id: trace_id}} do
       assert {:ok, ^trace_id} = Span.trace_id(reference)
