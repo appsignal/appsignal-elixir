14fa5acafb190874ddf27c59a4a20b8e989de985
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-12-12 15:20:18 +0100

    Remove parens from if-statement in FakeTransaction

--- test/support/appsignal/fake_transaction.ex
+++ test/support/appsignal/fake_transaction.ex
@@ -197,7 +197,7 @@ defmodule Appsignal.FakeTransaction do
   end
 
   def start(id, namespace) do
-    if(Appsignal.Config.active?()) do
+    if Appsignal.Config.active?() do
       Agent.update(__MODULE__, fn state ->
         {_, new_state} =
           Map.get_and_update(state, :started_transactions, fn current ->
