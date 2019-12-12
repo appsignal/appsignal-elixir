5f8c769cd91c98e497f084c7dd4f0d214e44619a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2019-12-12 15:36:40 +0100

    Fix filter test assertions

--- test/phoenix/transaction/filter_test.exs
+++ test/phoenix/transaction/filter_test.exs
@@ -6,10 +6,7 @@ defmodule Appsignal.Phoenix.Transaction.FilterTest do
   describe "parameter filtering" do
     test "filter_values does not filter structs" do
       assert MapFilter.filter_values(%{"foo" => "bar", "file" => %Plug.Upload{}}, ["password"]) ==
-               %{"foo" => "bar", "file" => %Plug.Upload{}}
-
-      assert MapFilter.filter_values(%{"foo" => "bar", "file" => %{__struct__: "s"}}, ["password"]) ==
-               %{"foo" => "bar", "file" => %{:__struct__ => "s"}}
+               %{"foo" => "bar", "file" => %{content_type: nil, filename: nil, path: nil}}
     end
   end
 end
