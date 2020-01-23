6c37e7e926418c9f18012594f4723116512ac453
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-01-23 14:44:18 +0100

    Tracer.create_span/1 registers spans in ets

--- lib/appsignal/tracer.ex
+++ lib/appsignal/tracer.ex
@@ -1,13 +1,19 @@
 defmodule Appsignal.Tracer do
   alias Appsignal.{Span, Nif}
 
+  @table :"$appsignal_registry"
+
+  def start_link do
+    Agent.start_link(fn -> :ets.new(@table, [:named_table, :public]) end)
+  end
+
   @doc """
   Creates a new span.
   """
   @spec create_span(String.t()) :: Span.t()
   def create_span(name) do
     {:ok, reference} = Nif.create_root_span(name)
-    %Span{reference: reference}
+    register(%Span{reference: reference})
   end
 
   @doc """
@@ -16,5 +22,10 @@ defmodule Appsignal.Tracer do
   @spec current_span() :: Span.t() | nil
   def current_span do
     nil
+  end
+
+  defp register(span) do
+    :ets.insert(@table, {self(), span})
+    span
   end
 end
