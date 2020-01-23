58f30994a54140ce448c41ad46449bb0f165a77f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-01-23 14:44:18 +0100

    Tracer.create_span/1 registers spans in ets

--- test/appsignal/tracer_test.exs
+++ test/appsignal/tracer_test.exs
@@ -2,6 +2,11 @@ defmodule Appsignal.TracerTest do
   use ExUnit.Case
   alias Appsignal.{Span, Tracer}
 
+  setup do
+    Tracer.start_link()
+    :ok
+  end
+
   describe "create_span/1" do
     setup do
       [span: Tracer.create_span("root")]
@@ -13,6 +18,10 @@ defmodule Appsignal.TracerTest do
 
     test "sets the span's reference", %{span: span} do
       assert is_reference(span.reference)
+    end
+
+    test "registers the span", %{span: span} do
+      assert :ets.lookup(:"$appsignal_registry", self()) == [{self(), span}]
     end
   end
 
