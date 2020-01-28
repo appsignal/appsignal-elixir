f75aa30eb13af5299f2251b80b97d9fafce5306d
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-01-28 11:35:26 +0100

    Use Map.fetch!/2 instead of Map.get/2 in WrappedNif

--- test/support/appsignal/wrapped_nif.ex
+++ test/support/appsignal/wrapped_nif.ex
@@ -11,7 +11,7 @@ defmodule Appsignal.WrappedNif do
   end
 
   def get(key) do
-    Agent.get(__MODULE__, &Map.get(&1, key))
+    Agent.get(__MODULE__, &Map.fetch!(&1, key))
   end
 
   defp add(key, value) do
