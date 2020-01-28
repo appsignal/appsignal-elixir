0ac3fc00c2a67737531892b06a428fa750793b9a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-01-28 11:24:12 +0100

    Add test for calling Nif.create_root_span/1 in Tracer.create_span/1
    Although a reference is asserted in the tests for Tracer.create_span/1
    already, this patch adds a regression test to make sure the reference
    comes from the Nif.
    
    It adds Appsignal.WrappedNif, a test module that wraps Appsignal.Nif. It
    calls to the orignal module like normal, but registers the call to allow
    assertions on later. Currently, only WrappedNif.create_root_span/1 is
    implemented. Other functions will be implemented as needed.

---
+++ test/support/appsignal/wrapped_nif.ex
@@ -0,0 +1,27 @@
+defmodule Appsignal.WrappedNif do
+  alias Appsignal.Nif
+
+  def start_link do
+    {:ok, pid} = Agent.start_link(fn -> %{create_root_span: []} end, name: __MODULE__)
+  end
+
+  def create_root_span(name) do
+    add(:create_root_span, name)
+    Nif.create_root_span(name)
+  end
+
+  def get(key) do
+    Agent.get(__MODULE__, &Map.get(&1, key))
+  end
+
+  defp add(key, value) do
+    Agent.get_and_update(__MODULE__, fn state ->
+      Map.get_and_update(state, key, fn current ->
+        case current do
+          nil -> {nil, [value]}
+          _ -> {current, [value | current]}
+        end
+      end)
+    end)
+  end
+end
