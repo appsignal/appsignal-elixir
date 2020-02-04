dceb6a526c2869db510fbd7c9e40a89f793644b9
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-04 14:35:43 +0100

    Revert "Remove set_span_name"
    This reverts commit 4a69769208853d948ae378c8fb6c7c2fe80e29de.

--- c_src/appsignal_extension.c
+++ c_src/appsignal_extension.c
@@ -938,6 +938,26 @@ static ERL_NIF_TERM _set_span_namespace(ErlNifEnv* env, int argc, const ERL_NIF_
     return enif_make_atom(env, "ok");
 }
 
+static ERL_NIF_TERM _set_span_name(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    span_ptr *ptr;
+    ErlNifBinary name;
+
+    if (argc != 2) {
+      return enif_make_badarg(env);
+    }
+    if(!enif_get_resource(env, argv[0], appsignal_span_type, (void**) &ptr)) {
+      return enif_make_badarg(env);
+    }
+    if(!enif_inspect_iolist_as_binary(env, argv[1], &name)) {
+        return enif_make_badarg(env);
+    }
+
+    appsignal_set_span_name(ptr->span, make_appsignal_string(name));
+
+    return enif_make_atom(env, "ok");
+}
+
 static ERL_NIF_TERM _set_span_attribute_string(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     span_ptr *ptr;
@@ -1258,6 +1278,7 @@ static ErlNifFunc nif_funcs[] =
     {"_create_root_span", 1, _create_root_span, 0},
     {"_create_child_span", 3, _create_child_span, 0},
     {"_set_span_namespace", 2, _set_span_namespace, 0},
+    {"_set_span_name", 2, _set_span_name, 0},
     {"_set_span_attribute_string", 3, _set_span_attribute_string, 0},
     {"_set_span_attribute_int", 3, _set_span_attribute_int, 0},
     {"_set_span_attribute_bool", 3, _set_span_attribute_bool, 0},
