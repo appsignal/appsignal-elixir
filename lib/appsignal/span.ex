d397a04b0ac896aa8f32664fb714a68b2f13ffeb
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-05 14:37:03 +0100

    Drop span creation functions, Registry and Dictionary

--- lib/appsignal/span.ex
+++ lib/appsignal/span.ex
@@ -10,43 +10,6 @@ defmodule Appsignal.Span do
     Nif.span_id(reference)
   end
 
-  def create(name \\ "") do
-    case Dictionary.lookup() || parent() do
-      %Span{trace_id: trace_id, span_id: span_id} ->
-        create(name, trace_id, span_id)
-
-      _ ->
-        {:ok, reference} = Nif.create_root_span(name)
-        {:ok, trace_id} = trace_id(reference)
-        {:ok, span_id} = span_id(reference)
-
-        register(%Span{reference: reference, trace_id: trace_id, span_id: span_id})
-    end
-  end
-
-  def create(name, trace_id, parent_id) do
-    {:ok, reference} = Nif.create_child_span(trace_id, parent_id, name)
-    {:ok, span_id} = span_id(reference)
-
-    register(%Span{reference: reference, trace_id: trace_id, span_id: span_id})
-  end
-
-  defp register(span) do
-    Dictionary.insert(span)
-    Registry.insert(span)
-
-    span
-  end
-
-  defp parent do
-    {:dictionary, values} = :erlang.process_info(self(), :dictionary)
-
-    case values[:"$callers"] do
-      [parent | _] -> Registry.lookup(parent)
-      _ -> nil
-    end
-  end
-
   def set_name(%Span{reference: reference} = span, name) when is_binary(name) do
     :ok = Nif.set_span_name(reference, name)
     span
@@ -101,17 +64,6 @@ defmodule Appsignal.Span do
       |> Appsignal.Utils.DataEncoder.encode()
 
     :ok = Nif.add_span_error(reference, name, message, backtrace)
-    span
-  end
-
-  def close() do
-    Dictionary.lookup() |> close()
-  end
-
-  def close(%Span{reference: reference} = span) do
-    :ok = Nif.close_span(reference)
-    Dictionary.delete()
-    Registry.delete()
     span
   end
 end
