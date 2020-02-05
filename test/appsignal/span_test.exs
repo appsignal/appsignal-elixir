0a30387532e761f0f81cd195ebc4c246de8af67c
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-05 14:37:03 +0100

    Drop span creation functions, Registry and Dictionary

--- test/appsignal/span_test.exs
+++ test/appsignal/span_test.exs
@@ -1,44 +1,23 @@
 defmodule AppsignalSpanTest do
   use ExUnit.Case
-  alias Appsignal.Span
+  alias Appsignal.{Span, Tracer, WrappedNif}
 
   setup do
-    [span: Span.create()]
-  end
-
-  describe ".create/0" do
-    test "creates a new span", %{
-      span: %Span{reference: reference, span_id: span_id, trace_id: trace_id}
-    } do
-      assert is_reference(reference)
-      assert is_list(span_id)
-      assert is_list(trace_id)
-    end
-  end
-
-  describe ".create/1" do
-    setup do
-      [span: Span.create("test")]
-    end
-
-    test "creates a new span", %{
-      span: %Span{reference: reference, span_id: span_id, trace_id: trace_id}
-    } do
-      assert is_reference(reference)
-      assert is_list(span_id)
-      assert is_list(trace_id)
-    end
+    WrappedNif.start_link()
+    [span: Tracer.create_span("")]
   end
 
   describe ".trace_id/1" do
-    test "returns a span's trace_id", %{span: %Span{reference: reference, trace_id: trace_id}} do
-      assert {:ok, ^trace_id} = Span.trace_id(reference)
+    test "returns an ok-tuple with the trace_id as a list", %{span: %Span{reference: reference}} do
+      {:ok, trace_id} = Span.trace_id(reference)
+      assert is_list(trace_id)
     end
   end
 
   describe ".span_id/1" do
-    test "returns a span's span_id", %{span: %Span{reference: reference, span_id: span_id}} do
-      assert {:ok, ^span_id} = Span.span_id(reference)
+    test "returns an ok-tuple with the span_id as a list", %{span: %Span{reference: reference}} do
+      {:ok, span_id} = Span.span_id(reference)
+      assert is_list(span_id)
     end
   end
 
