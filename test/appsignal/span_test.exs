f59ce8ee52df99e5268d8b1a78ceb32124d5ee39
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-05 14:45:06 +0100

    Span.(trace|span)_id take Span structs

--- test/appsignal/span_test.exs
+++ test/appsignal/span_test.exs
@@ -8,15 +8,15 @@ defmodule AppsignalSpanTest do
   end
 
   describe ".trace_id/1" do
-    test "returns an ok-tuple with the trace_id as a list", %{span: %Span{reference: reference}} do
-      {:ok, trace_id} = Span.trace_id(reference)
+    test "returns an ok-tuple with the trace_id as a list", %{span: span} do
+      {:ok, trace_id} = Span.trace_id(span)
       assert is_list(trace_id)
     end
   end
 
   describe ".span_id/1" do
-    test "returns an ok-tuple with the span_id as a list", %{span: %Span{reference: reference}} do
-      {:ok, span_id} = Span.span_id(reference)
+    test "returns an ok-tuple with the span_id as a list", %{span: span} do
+      {:ok, span_id} = Span.span_id(span)
       assert is_list(span_id)
     end
   end
