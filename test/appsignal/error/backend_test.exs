790aca69bab5c9951bb3ccb7b4368e9f04132425
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-06 16:55:20 +0100

    Error.Backend.handle_event/3 adds errors to existing spans

--- test/appsignal/error/backend_test.exs
+++ test/appsignal/error/backend_test.exs
@@ -2,7 +2,7 @@ defmodule Appsignal.Error.BackendTest do
   use ExUnit.Case, async: true
   import AppsignalTest.Utils
   import ExUnit.CaptureIO
-  alias Appsignal.{Error.Backend, Span, Test, WrappedNif}
+  alias Appsignal.{Error.Backend, Span, Test, Tracer, WrappedNif}
 
   setup do
     WrappedNif.start_link()
@@ -39,6 +39,39 @@ defmodule Appsignal.Error.BackendTest do
     test "closes the created span", %{pid: pid} do
       until(fn ->
         assert {:ok, [{%Span{}}]} = Test.Tracer.get(:close_span)
+      end)
+    end
+  end
+
+  describe "handle_event/3, with an open span" do
+    setup do
+      parent = self()
+
+      pid =
+        spawn(fn ->
+          span = Tracer.create_span("")
+          send(parent, span)
+          raise "Exception"
+        end)
+
+      span =
+        receive do
+          span -> span
+        end
+
+      [pid: pid, span: span]
+    end
+
+    test "adds an error to the existing span", %{span: span} do
+      until(fn ->
+        assert {:ok, [{^span, %RuntimeError{message: "Exception"}, _stack}]} =
+                 Test.Span.get(:add_error)
+      end)
+    end
+
+    test "closes the existing span", %{span: span, pid: pid} do
+      until(fn ->
+        assert {:ok, [{^span}]} = Test.Tracer.get(:close_span)
       end)
     end
   end
