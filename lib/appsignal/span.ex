8e71ee5756f7cde7855735ff052c3ecd6ac66c92
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-06 14:19:36 +0100

    Add Span.to_map/1

--- lib/appsignal/span.ex
+++ lib/appsignal/span.ex
@@ -109,4 +109,9 @@ defmodule Appsignal.Span do
   end
 
   def close(nil), do: nil
+
+  def to_map(%Span{reference: reference}) do
+    {:ok, json} = Nif.span_to_json(reference)
+    Appsignal.Json.decode!(json)
+  end
 end
