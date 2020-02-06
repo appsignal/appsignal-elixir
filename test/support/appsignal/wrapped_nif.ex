47e9f7366940cc20c7a85d147ede110146f671b1
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-06 15:32:00 +0100

    Move shared wrapper functions to Wrapper module

--- test/support/appsignal/wrapped_nif.ex
+++ test/support/appsignal/wrapped_nif.ex
@@ -1,17 +1,6 @@
 defmodule Appsignal.WrappedNif do
-  import ExUnit.Assertions
+  use Wrapper
   alias Appsignal.Nif
-
-  def start_link do
-    {:ok, pid} = Agent.start_link(fn -> %{} end, name: __MODULE__)
-
-    ExUnit.Callbacks.on_exit(fn ->
-      ref = Process.monitor(pid)
-      assert_receive {:DOWN, ^ref, _, _, _}, 500
-    end)
-
-    {:ok, pid}
-  end
 
   def create_root_span(name) do
     add(:create_root_span, {name})
@@ -31,20 +20,5 @@ defmodule Appsignal.WrappedNif do
   def close_span(reference) do
     add(:close_span, {reference})
     Nif.close_span(reference)
-  end
-
-  def get(key) do
-    Agent.get(__MODULE__, &Map.fetch!(&1, key))
-  end
-
-  defp add(key, value) do
-    Agent.get_and_update(__MODULE__, fn state ->
-      Map.get_and_update(state, key, fn current ->
-        case current do
-          nil -> {nil, [value]}
-          _ -> {current, [value | current]}
-        end
-      end)
-    end)
   end
 end
