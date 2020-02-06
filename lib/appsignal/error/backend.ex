f3b03f20ea74a94aa2081e73ee3fcf5c0d86f52a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-06 15:53:24 +0100

    Error.Backend.handle_event/3 adds errors to created spans

--- lib/appsignal/error/backend.ex
+++ lib/appsignal/error/backend.ex
@@ -1,13 +1,17 @@
 defmodule Appsignal.Error.Backend do
   @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+  @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
   @behaviour :gen_event
 
   def init(opts), do: {:ok, opts}
 
   def handle_event({:error, gl, {_, _, _, metadata} = event}, state) when node(gl) == node() do
+    {error, stacktrace} = metadata[:crash_reason]
+
     ""
     |> @tracer.create_span(nil, metadata[:pid])
+    |> @span.add_error(error, stacktrace)
     |> @tracer.close_span()
 
     {:ok, state}
