3c103d9071a3a25f762c8762190ede97d4f00036
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-06 14:42:27 +0100

    Backend.handle_event/3 creates spans

--- lib/appsignal/error/backend.ex
+++ lib/appsignal/error/backend.ex
@@ -1,5 +1,17 @@
 defmodule Appsignal.Error.Backend do
+  @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+
   @behaviour :gen_event
 
   def init(opts), do: {:ok, opts}
+
+  def handle_event({:error, gl, {_, _, _, metadata} = event}, state) when node(gl) == node() do
+    @tracer.create_span("", nil, metadata[:pid])
+
+    {:ok, state}
+  end
+
+  def handle_event(_event, state) do
+    {:ok, state}
+  end
 end
