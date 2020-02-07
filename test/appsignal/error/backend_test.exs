aa5e1ede80fefb36f5b1402f25f29497103bbd3f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-07 12:00:59 +0100

    Add regression test for :badargs

--- test/appsignal/error/backend_test.exs
+++ test/appsignal/error/backend_test.exs
@@ -75,4 +75,36 @@ defmodule Appsignal.Error.BackendTest do
       end)
     end
   end
+
+  describe "handle_event/3, with a :badarg" do
+    setup do
+      pid =
+        spawn(fn ->
+          :erlang.error(:badarg)
+        end)
+
+      [pid: pid]
+    end
+
+    test "creates a span", %{pid: pid} do
+      until(fn ->
+        assert {:ok, [{"", nil, ^pid}]} = Test.Tracer.get(:create_span)
+      end)
+    end
+
+    test "adds an error to the created span", %{pid: pid} do
+      until(fn ->
+        assert {:ok, [{%Span{}, %ArgumentError{message: "argument error"}, stack}]} =
+                 Test.Span.get(:add_error)
+
+        assert is_list(stack)
+      end)
+    end
+
+    test "closes the created span", %{pid: pid} do
+      until(fn ->
+        assert {:ok, [{%Span{}}]} = Test.Tracer.get(:close_span)
+      end)
+    end
+  end
 end
