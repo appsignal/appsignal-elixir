b7a991e14ff2482131daef0071f27b3d6b32b649
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-02-21 10:13:53 +0100

    Release 1.12.0-beta.3

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Appsignal.Mixfile do
   def project do
     [
       app: :appsignal,
-      version: "1.12.0-beta.2",
+      version: "1.12.0-beta.3",
       name: "AppSignal",
       description: description(),
       package: package(),
