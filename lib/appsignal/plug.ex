8943d7a6712559af405ed8d79cc0ba36506f7642
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-03 14:23:19 +0100

    Remove tests depending on ErrorHandler

--- lib/appsignal/plug.ex
+++ lib/appsignal/plug.ex
@@ -74,7 +74,6 @@ if Appsignal.plug?() do
            %Plug.Conn{private: %{appsignal_transaction: transaction}} = conn
          ) do
       Appsignal.TransactionRegistry.ignore(self())
-      Appsignal.ErrorHandler.set_error(transaction, error, stack)
       finish_with_conn(transaction, conn)
     end
 
