f421149e4a1a55a0920a2cec3762ca64a90d223f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-09 15:39:41 +0100

    Expose WrappedTracer as Appsignal.Test.Tracer

--- test/support/appsignal/wrapped_tracer.ex
+++
@@ -1,9 +0,0 @@
-defmodule Appsignal.WrappedTracer do
-  use Wrapper
-  alias Appsignal.Tracer
-
-  def create_span(name, parent, pid) do
-    add(:create_span, {name, parent, pid})
-    Tracer.create_span(name, parent, pid)
-  end
-end
