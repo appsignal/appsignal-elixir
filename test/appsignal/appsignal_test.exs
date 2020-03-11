d1cddfb92307404db6a91fa4ad52a0725c0299bb
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-11 16:31:15 +0100

    Remove Plug integration

--- test/appsignal/appsignal_test.exs
+++ test/appsignal/appsignal_test.exs
@@ -32,12 +32,6 @@ defmodule AppsignalTest do
     Appsignal.add_distribution_value("dist_key", 10, %{:a => "b"})
   end
 
-  describe "plug?/0" do
-    test "is true when Plug is loaded" do
-      assert Appsignal.plug?() == true
-    end
-  end
-
   describe "phoenix?/0" do
     @tag :skip_env_test
     @tag :skip_env_test_no_nif
