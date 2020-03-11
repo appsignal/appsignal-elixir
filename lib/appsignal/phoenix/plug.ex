dc08afed5a1813e6dfc0c45dab662c02ef6ac759
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-11 16:31:15 +0100

    Remove Plug integration

--- lib/appsignal/phoenix/plug.ex
+++ lib/appsignal/phoenix/plug.ex
@@ -1,18 +1,16 @@
-if Appsignal.plug?() do
-  defmodule Appsignal.Phoenix.Plug do
-    defmacro __using__(_) do
-      IO.warn("Appsignal.Phoenix.Plug is deprecated. Use Appsignal.Plug instead.")
+defmodule Appsignal.Phoenix.Plug do
+  defmacro __using__(_) do
+    IO.warn("Appsignal.Phoenix.Plug is deprecated. Use Appsignal.Plug instead.")
 
-      quote do
-        use Appsignal.Plug
-      end
+    quote do
+      use Appsignal.Plug
     end
+  end
 
-    @deprecated "Use Appsignal.Error.metadata/1 instead."
-    def extract_error_metadata(reason, conn, _stack) do
-      {exception, stack} = Appsignal.Error.normalize(reason, [])
-      {name, message} = Appsignal.Error.metadata(exception)
-      {name, message, stack, conn}
-    end
+  @deprecated "Use Appsignal.Error.metadata/1 instead."
+  def extract_error_metadata(reason, conn, _stack) do
+    {exception, stack} = Appsignal.Error.normalize(reason, [])
+    {name, message} = Appsignal.Error.metadata(exception)
+    {name, message, stack, conn}
   end
 end
