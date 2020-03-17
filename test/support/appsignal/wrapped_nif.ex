bcce72c0b03ce0727b151e8750185457cb162814
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-17 15:39:55 +0100

    Add regression test for Span.set_name/2

--- test/support/appsignal/wrapped_nif.ex
+++ test/support/appsignal/wrapped_nif.ex
@@ -15,6 +15,11 @@ defmodule Appsignal.WrappedNif do
     Nif.create_child_span(trace_id, span_id, name)
   end
 
+  def set_span_name(reference, name) do
+    add(:set_span_name, {reference, name})
+    Nif.set_span_name(reference, name)
+  end
+
   def add_span_error(reference, name, message, stacktrace) do
     add(:add_span_error, {reference, name, message, stacktrace})
     Nif.add_span_error(reference, name, message, stacktrace)
