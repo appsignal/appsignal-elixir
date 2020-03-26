eff929d8a713cb60e653f417f1375f427bb349a1
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-26 10:56:46 +0100

    Monitor deletes entries from registry on process DOWN

--- test/appsignal/monitor_test.exs
+++ test/appsignal/monitor_test.exs
@@ -1,7 +1,12 @@
 defmodule Appsignal.MonitorTest do
   use ExUnit.Case
   import AppsignalTest.Utils
-  alias Appsignal.Monitor
+  alias Appsignal.{Monitor, Span, Tracer, WrappedNif}
+
+  setup do
+    WrappedNif.start_link()
+    :ok
+  end
 
   test "is started by the main supervisor" do
     assert is_pid(monitor_pid())
@@ -12,6 +17,23 @@ defmodule Appsignal.MonitorTest do
 
     until(fn ->
       assert Process.info(monitor_pid(), :monitors) == {:monitors, [{:process, self()}]}
+    end)
+  end
+
+  test "removes entries from the registry when their processes exit" do
+    pid =
+      spawn(fn ->
+        Tracer.create_span("root")
+        Monitor.add()
+        :timer.sleep(2)
+      end)
+
+    until(fn ->
+      assert %Span{} = Tracer.current_span(pid)
+    end)
+
+    until(fn ->
+      assert Tracer.current_span(pid) == nil
     end)
   end
 
