54fbe75d0b803568585241b9ae6b60ee3a891d03
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-26 10:18:59 +0100

    Monitor monitors processes

--- lib/appsignal/monitor.ex
+++ lib/appsignal/monitor.ex
@@ -8,4 +8,13 @@ defmodule Appsignal.Monitor do
   def init(state) do
     {:ok, state}
   end
+
+  def add do
+    GenServer.cast(__MODULE__, {:monitor, self()})
+  end
+
+  def handle_cast({:monitor, pid}, state) do
+    Process.monitor(pid)
+    {:noreply, state}
+  end
 end
