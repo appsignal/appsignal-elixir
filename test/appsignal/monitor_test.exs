2dc5a58d2a17e76cefbf6b5d41ef5894c53675e6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-03-26 10:18:59 +0100

    Monitor monitors processes

--- test/appsignal/monitor_test.exs
+++ test/appsignal/monitor_test.exs
@@ -1,10 +1,21 @@
 defmodule Appsignal.MonitorTest do
   use ExUnit.Case
+  import AppsignalTest.Utils
   alias Appsignal.Monitor
 
   test "is started by the main supervisor" do
-    assert Monitor
-           |> Process.whereis()
-           |> is_pid()
+    assert is_pid(monitor_pid())
+  end
+
+  test "monitors a process" do
+    Monitor.add()
+
+    until(fn ->
+      assert Process.info(monitor_pid(), :monitors) == {:monitors, [{:process, self()}]}
+    end)
+  end
+
+  defp monitor_pid do
+    Process.whereis(Monitor)
   end
 end
