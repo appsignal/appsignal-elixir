88ab8797687c5f2f56ada635bebf19af62d782e9
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-09 10:03:19 +0200

    Error.metadata/3 handles erlang errors

--- lib/appsignal/error.ex
+++ lib/appsignal/error.ex
@@ -1,8 +1,10 @@
 defmodule Appsignal.Error do
   def metadata(:error, reason, stack) do
+    exception = Exception.normalize(:error, reason, stack)
+
     {
-      inspect(reason.__struct__),
-      Exception.format_banner(:error, reason, stack),
+      inspect(exception.__struct__),
+      Exception.format_banner(:error, exception, stack),
       Appsignal.Stacktrace.format(stack)
     }
   end
