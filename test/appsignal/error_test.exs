e2b09b4ff91795ed2b36f2f0f7e861ba2086b82f
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-09 10:03:19 +0200

    Error.metadata/3 handles erlang errors

--- test/appsignal/error_test.exs
+++ test/appsignal/error_test.exs
@@ -1,7 +1,7 @@
 defmodule Appsignal.ErrorTest do
   use ExUnit.Case
 
-  describe "metadata/3" do
+  describe "metadata/3, with an exception" do
     setup do
       try do
         raise "Exception!"
@@ -16,6 +16,31 @@ defmodule Appsignal.ErrorTest do
 
     test "extracts the error's message", %{metadata: metadata} do
       assert {_name, "** (RuntimeError) Exception!", _stack} = metadata
+    end
+
+    test "format's the error's stack trace", %{metadata: metadata} do
+      {_name, _message, stack} = metadata
+      assert is_list(stack)
+      assert length(stack) > 0
+      assert Enum.all?(stack, &is_binary(&1))
+    end
+  end
+
+  describe "metadata/3, with a badarg" do
+    setup do
+      try do
+        _ = String.to_integer("one")
+      catch
+        kind, reason -> %{metadata: Appsignal.Error.metadata(kind, reason, __STACKTRACE__)}
+      end
+    end
+
+    test "extracts the error's name", %{metadata: metadata} do
+      assert {"ArgumentError", _message, _stack} = metadata
+    end
+
+    test "extracts the error's message", %{metadata: metadata} do
+      assert {_name, "** (ArgumentError) argument error", _stack} = metadata
     end
 
     test "format's the error's stack trace", %{metadata: metadata} do
