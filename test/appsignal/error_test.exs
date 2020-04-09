e91fe69b60c3f31508ae9dc47b133c8065a33f67
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-09 09:46:54 +0200

    Replace Error implementation with new Error.metadata/3

---
+++ test/appsignal/error_test.exs
@@ -0,0 +1,28 @@
+defmodule Appsignal.ErrorTest do
+  use ExUnit.Case
+
+  describe "metadata/3" do
+    setup do
+      try do
+        raise "Exception!"
+      catch
+        kind, reason -> %{metadata: Appsignal.Error.metadata(kind, reason, __STACKTRACE__)}
+      end
+    end
+
+    test "extracts the error's name", %{metadata: metadata} do
+      assert {"RuntimeError", _message, _stack} = metadata
+    end
+
+    test "extracts the error's message", %{metadata: metadata} do
+      assert {_name, "** (RuntimeError) Exception!", _stack} = metadata
+    end
+
+    test "format's the error's stack trace", %{metadata: metadata} do
+      {_name, _message, stack} = metadata
+      assert is_list(stack)
+      assert length(stack) > 0
+      assert Enum.all?(stack, &is_binary(&1))
+    end
+  end
+end
