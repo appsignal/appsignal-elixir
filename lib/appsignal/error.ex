ac1576e2f05cd53ef33ec6ad57775d04b54b4111
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-09 09:46:54 +0200

    Replace Error implementation with new Error.metadata/3

--- lib/appsignal/error.ex
+++ lib/appsignal/error.ex
@@ -1,61 +1,9 @@
 defmodule Appsignal.Error do
-  @moduledoc """
-  Functions for extracting information from Elixir exceptions and Erlang errors.
-  """
-  import Appsignal.Utils
-
-  @spec metadata(Exception.t()) :: {String.t(), String.t()}
-  def metadata(exception) do
-    {name(exception), Exception.message(exception)}
+  def metadata(:error, reason, stack) do
+    {
+      inspect(reason.__struct__),
+      Exception.format_banner(:error, reason, stack),
+      Appsignal.Stacktrace.format(stack)
+    }
   end
-
-  @spec normalize(any, Exception.stacktrace()) :: {Exception.t(), list(String.t())}
-  def normalize({%_{__exception__: true} = exception, stacktrace}, _) do
-    normalize(exception, stacktrace)
-  end
-
-  def normalize({{%_{__exception__: true} = exception, stacktrace}, _}, _) do
-    normalize(exception, stacktrace)
-  end
-
-  def normalize({maybe_error, maybe_stacktrace} = error, stacktrace) do
-    {error, stacktrace} =
-      if stacktrace?(maybe_stacktrace) do
-        {maybe_error, maybe_stacktrace}
-      else
-        {error, stacktrace}
-      end
-
-    do_normalize(error, stacktrace)
-  end
-
-  def normalize(error, stacktrace), do: do_normalize(error, stacktrace)
-
-  @spec do_normalize(any, Exception.stacktrace()) :: {Exception.t(), list(String.t())}
-  defp do_normalize(error, stacktrace) do
-    exception = Exception.normalize(:error, error, stacktrace)
-    {exception, stacktrace}
-  end
-
-  @spec name(Exception.t()) :: String.t()
-  defp name(%ErlangError{original: {name, _}}) when is_atom(name) do
-    inspect(name)
-  end
-
-  defp name(%ErlangError{original: name}) when is_atom(name) do
-    inspect(name)
-  end
-
-  defp name(%module{}) do
-    module_name(module)
-  end
-
-  defp stacktrace?(stacktrace) when is_list(stacktrace) do
-    Enum.all?(stacktrace, &stacktrace_line?/1)
-  end
-
-  defp stacktrace?(_), do: false
-
-  defp stacktrace_line?({_, _, _, [file: _, line: _]}), do: true
-  defp stacktrace_line?(_), do: false
 end
