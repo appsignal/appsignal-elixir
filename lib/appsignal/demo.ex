fc6c9b48560e220e34a4bb1c4ced53a1fc5dadde
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-10 20:32:38 +0200

    Add Appsignal.Demo.send_performance_sample/0

--- lib/appsignal/demo.ex
+++ lib/appsignal/demo.ex
@@ -1,68 +1,32 @@
-defmodule Appsignal.DemoBehaviour do
-  @callback create_transaction_error_request :: Appsignal.Transaction.t()
-  @callback create_transaction_performance_request :: Appsignal.Transaction.t()
-end
-
-defmodule TestError do
-  defexception message: "Hello world! This is an error used for demonstration purposes."
-end
-
 defmodule Appsignal.Demo do
-  import Appsignal.Instrumentation.Helpers, only: [instrument: 4]
-  require Appsignal.Stacktrace
-  @behaviour Appsignal.DemoBehaviour
-  @transaction Application.get_env(:appsignal, :appsignal_transaction, Appsignal.Transaction)
+  import Appsignal, only: [instrument: 2]
+  @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
+  @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
-  @doc false
-  @spec create_transaction_error_request :: Appsignal.Transaction.t()
-  def create_transaction_error_request do
-    raise TestError
-  rescue
-    error ->
-      create_demo_transaction()
-      |> @transaction.set_error("TestError", error.message, Appsignal.Stacktrace.get())
-      |> finish_demo_transaction()
-  end
+  def send_performance_sample() do
+    span =
+      "http_request"
+      |> @tracer.create_span()
+      |> @span.set_name("DemoController#hello")
+      |> @span.set_attribute("demo_sample", true)
+      |> @span.set_sample_data("environment", %{"method" => "GET", "request_path" => "/"})
 
-  @doc false
-  @spec create_transaction_performance_request :: Appsignal.Transaction.t()
-  def create_transaction_performance_request do
-    transaction = create_demo_transaction()
+    instrument("call.phoenix_endpoint", fn ->
+      :timer.sleep(100)
 
-    instrument(transaction, "render.phoenix_template", "Rendering something slow", fn ->
-      :timer.sleep(1000)
-
-      instrument(transaction, "query.ecto", "Slow query", fn ->
-        :timer.sleep(300)
+      instrument("query.ecto", fn ->
+        :timer.sleep(30)
       end)
 
-      instrument(transaction, "query.ecto", "Slow query", fn ->
-        :timer.sleep(500)
+      instrument("query.ecto", fn ->
+        :timer.sleep(50)
       end)
 
-      instrument(transaction, "render.phoenix_template", "Rendering something slow", fn ->
-        :timer.sleep(100)
+      instrument("render.phoenix_template", fn ->
+        :timer.sleep(10)
       end)
     end)
 
-    finish_demo_transaction(transaction)
-  end
-
-  defp create_demo_transaction do
-    @transaction.generate_id()
-    |> @transaction.start(:http_request)
-    |> @transaction.set_action("DemoController#hello")
-    |> @transaction.set_meta_data("demo_sample", "true")
-    |> @transaction.set_sample_data(
-      "environment",
-      %{request_path: "/hello", method: "GET"}
-    )
-  end
-
-  defp finish_demo_transaction(transaction) do
-    @transaction.finish(transaction)
-    @transaction.complete(transaction)
-
-    transaction
+    @tracer.close_span(span)
   end
 end
