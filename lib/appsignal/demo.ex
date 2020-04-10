d00373ee625d93d4bb11df0284e037386a6bec9e
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-10 20:43:55 +0200

    Add Appsignal.Demo.send_error_sample/0

--- lib/appsignal/demo.ex
+++ lib/appsignal/demo.ex
@@ -1,15 +1,14 @@
+defmodule TestError do
+  defexception message: "Hello world! This is an error used for demonstration purposes."
+end
+
 defmodule Appsignal.Demo do
   import Appsignal, only: [instrument: 2]
   @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
   @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
   def send_performance_sample() do
-    span =
-      "http_request"
-      |> @tracer.create_span()
-      |> @span.set_name("DemoController#hello")
-      |> @span.set_attribute("demo_sample", true)
-      |> @span.set_sample_data("environment", %{"method" => "GET", "request_path" => "/"})
+    span = create_demo_span()
 
     instrument("call.phoenix_endpoint", fn ->
       :timer.sleep(100)
@@ -28,5 +27,22 @@ defmodule Appsignal.Demo do
     end)
 
     @tracer.close_span(span)
+  end
+
+  def send_error_sample() do
+    raise TestError
+  catch
+    kind, reason ->
+      create_demo_span()
+      |> @span.add_error(kind, reason, __STACKTRACE__)
+      |> @tracer.close_span()
+  end
+
+  defp create_demo_span() do
+    "http_request"
+    |> @tracer.create_span()
+    |> @span.set_name("DemoController#hello")
+    |> @span.set_attribute("demo_sample", true)
+    |> @span.set_sample_data("environment", %{"method" => "GET", "request_path" => "/"})
   end
 end
