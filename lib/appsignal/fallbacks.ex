091acff6c87e9ef2240b6966846aa819cd2de5f3
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-29 13:24:24 +0200

    Crash compilation on Appsignal.Phoenix.__using__/1

---
+++ lib/appsignal/fallbacks.ex
@@ -0,0 +1,25 @@
+unless Code.ensure_loaded?(Appsignal.Phoenix) do
+  defmodule Appsignal.Phoenix.NotAvailableError do
+    defexception []
+
+    def message(_) do
+      """
+      Appsignal.Phoenix was used but not present in the project's dependencies.
+
+      Since :appsignal 2.0, Appsignal's Phoenix integration was moved to a new library named :appsignal_phoenix. Please replace the project's dependency on :appsignal to depend on :appsignal_phoenix instead:
+
+        defp deps do
+          [
+            {:appsignal_phoenix, "~> 1.0"}
+          ]
+        end
+      """
+    end
+  end
+
+  defmodule Appsignal.Phoenix do
+    def __using__(_) do
+      raise Appsignal.Phoenix.NotAvailableError
+    end
+  end
+end
