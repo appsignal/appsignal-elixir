6ee17a4e90a3698b0d35853fc3ba7384d1431085
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-04-29 14:11:44 +0200

    Crash compilation on Appsignal.Plug.__using__/1

--- test/appsignal/fallbacks_test.exs
+++ test/appsignal/fallbacks_test.exs
@@ -1,6 +1,16 @@
 defmodule Appsignal.FallbacksTest do
   use ExUnit.Case
 
+  test "crashes when using Appsignal.Plug" do
+    try do
+      Appsignal.Plug.__using__([])
+    catch
+      :error, %Appsignal.Plug.NotAvailableError{} -> :ok
+    else
+      _ -> flunk("Expected an Appsignal.Plug.NotAvailableError to be raised.")
+    end
+  end
+
   test "crashes when using Appsignal.Phoenix" do
     try do
       Appsignal.Phoenix.__using__([])
