7d443f9a3e6c854f768f0f8319d2f747b8e5a39a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-05-14 15:26:30 +0200

    Ecto.attach/2 attaches to repos with custom prefixes

--- lib/appsignal/ecto.ex
+++ lib/appsignal/ecto.ex
@@ -1,21 +1,32 @@
 defmodule Appsignal.Ecto do
   def attach do
-    :appsignal
-    |> Application.get_env(:config, %{})
-    |> Map.get(:otp_app)
+    otp_app =
+      :appsignal
+      |> Application.get_env(:config, %{})
+      |> Map.get(:otp_app, nil)
+
+    otp_app
     |> Application.get_env(:ecto_repos, [])
-    |> Enum.each(&attach/1)
+    |> Enum.each(&attach(otp_app, &1))
   end
 
-  def attach(repo) do
-    event = telemetry_prefix(repo) ++ [:query]
+  def attach(otp_app, repo) do
+    event = telemetry_prefix(otp_app, repo) ++ [:query]
     :telemetry.attach({__MODULE__, event}, event, &query/4, :ok)
   end
 
-  defp telemetry_prefix(repo) do
-    repo
-    |> Module.split()
-    |> Enum.map(&(&1 |> Macro.underscore() |> String.to_atom()))
+  defp telemetry_prefix(otp_app, repo) do
+    case otp_app
+         |> Application.get_env(repo, [])
+         |> Keyword.get(:telemetry_prefix) do
+      prefix when is_list(prefix) ->
+        prefix
+
+      _ ->
+        repo
+        |> Module.split()
+        |> Enum.map(&(&1 |> Macro.underscore() |> String.to_atom()))
+    end
   end
 
   defp query(_event, _measurements, __metadata, _config) do
--- test/appsignal/ecto_test.exs
+++ test/appsignal/ecto_test.exs
@@ -1,8 +1,18 @@
 defmodule Appsignal.EctoTest do
   use ExUnit.Case
+  alias Appsignal.Ecto
 
   test "is attached to the repo query event automatically" do
     assert attached?([:appsignal, :test, :repo, :query])
+  end
+
+  test "attach/2 attaches to events with custom prefixes" do
+    Application.put_env(:appsignal, Appsignal.Test.Repo, telemetry_prefix: [:my_repo])
+    Ecto.attach(:appsignal, Appsignal.Test.Repo)
+
+    assert attached?([:my_repo, :query])
+
+    Application.delete_env(:appsignal, Appsignal.Test.Repo, telemetry_prefix: :my_repo)
   end
 
   defp attached?(event) do
