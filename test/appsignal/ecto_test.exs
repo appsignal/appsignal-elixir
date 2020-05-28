a8062a844018c3a7c236be1501111829b4b07461
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-05-28 14:34:07 +0200

    Add Ecto integration (#565)
    * Add set_sql method to Span
    
    * Add _with_timestamp functions to Nif
    
    * Alias Appsignal.Nif in NifTest
    
    * Move pids to option lists in Tracer.create_span/3
    
    * Move Test.Nif to test_nif
    
    * Add Span.create_root/3
    
    * Allow passing a start_time to Tracer.create_span/3
    
    * Add Span.create_child/3
    
    * Allow passing a start_time to Tracer.create_span/3 when creating a child span
    
    * Add Span.close/2 and Tracer.close_span/2
    
    * Add :otp_app configuration option and set it on install (#561)
    
    * Add otp_app configuration to installer
    
    * Map APPSIGNAL_OTP_APP to :otp_app in config
    
    * Installer sets APPSIGNAL_OTP_APP
    
    * Add default configuration method option
    
    * Use otp_name as default app name option
    
    * Refactor appsignal_config_file_contents/1
    
    * Add Mix.Project call to dialyzer ignore file
    
    * Switch to :extra_applications in mix.exs
    
    * Add Appsignal.Ecto.attach/0
    
    * Ecto.attach/2 attaches to repos with custom prefixes
    
    * Bump agent
    
    * Ecto.query/4 creates spans
    
    * Ecto.query/4 sets the span's names
    
    * Ecto.query/4 sets the span's categories
    
    * Ecto.query/4 sets the span's body
    
    * Ecto.query/4 closes the span
    
    * Add regression test for Span.close/2
    
    * Add Appsignal.Test.close_span/2
    
    * Ecto.query/4 sets start and end times
    
    * Ecto.query/4 does not create spans for sourceless query events
    
    * Use set_sql/1 from Ecto.query/4
    
    Co-authored-by: Adam Yeats <adam@appsignal.com>
    Co-authored-by: Thijs Cadier <thijs@appsignal.com>

---
+++ test/appsignal/ecto_test.exs
@@ -0,0 +1,101 @@
+defmodule Appsignal.EctoTest do
+  use ExUnit.Case
+  alias Appsignal.{Ecto, Test, Span}
+
+  test "is attached to the repo query event automatically" do
+    assert attached?([:appsignal, :test, :repo, :query])
+  end
+
+  test "attach/2 attaches to events with custom prefixes" do
+    Application.put_env(:appsignal, Appsignal.Test.Repo, telemetry_prefix: [:my_repo])
+    Ecto.attach(:appsignal, Appsignal.Test.Repo)
+
+    assert attached?([:my_repo, :query])
+
+    Application.delete_env(:appsignal, Appsignal.Test.Repo, telemetry_prefix: :my_repo)
+  end
+
+  describe "query/4" do
+    setup do
+      Test.Nif.start_link()
+      Test.Tracer.start_link()
+      Test.Span.start_link()
+
+      :telemetry.execute(
+        [:appsignal, :test, :repo, :query],
+        %{
+          decode_time: 2_204_000,
+          query_time: 5_386_000,
+          queue_time: 1_239_000,
+          total_time: 8_829_000
+        },
+        %{
+          params: [],
+          query:
+            "SELECT u0.\"id\", u0.\"name\", u0.\"inserted_at\", u0.\"updated_at\" FROM \"users\" AS u0",
+          repo: Appsignal.Test.Repo,
+          result: :ok,
+          source: "users",
+          type: :ecto_sql_query
+        }
+      )
+    end
+
+    test "creates a span with a start time" do
+      {:ok, [{"http_request", nil, start_time: time}]} = Test.Tracer.get(:create_span)
+      assert is_integer(time)
+    end
+
+    test "sets the span's name" do
+      assert {:ok, [{%Span{}, "Query Appsignal.Test.Repo"}]} = Test.Span.get(:set_name)
+    end
+
+    test "sets the span's category" do
+      assert attribute("appsignal:category", "ecto.query")
+    end
+
+    test "sets the span's body" do
+      assert {:ok,
+              [
+                {%Span{},
+                 "SELECT u0.\"id\", u0.\"name\", u0.\"inserted_at\", u0.\"updated_at\" FROM \"users\" AS u0"}
+              ]} = Test.Span.get(:set_sql)
+    end
+
+    test "closes the span with an end time" do
+      {:ok, [{_, _, start_time: start_time}]} = Test.Tracer.get(:create_span)
+      {:ok, [{%Span{}, end_time: end_time}]} = Test.Tracer.get(:close_span)
+      assert end_time - start_time == 8_829_000
+    end
+  end
+
+  describe "query/4, for a query without a source" do
+    setup do
+      Test.Nif.start_link()
+      Test.Tracer.start_link()
+      Test.Span.start_link()
+
+      :telemetry.execute([:appsignal, :test, :repo, :query], %{}, %{source: nil})
+    end
+
+    test "does not create a span" do
+      assert Test.Tracer.get(:create_span) == :error
+    end
+  end
+
+  defp attribute(asserted_key, asserted_data) do
+    {:ok, attributes} = Test.Span.get(:set_attribute)
+
+    Enum.any?(attributes, fn {%Span{}, key, data} ->
+      key == asserted_key and data == asserted_data
+    end)
+  end
+
+  defp attached?(event) do
+    event
+    |> :telemetry.list_handlers()
+    |> Enum.any?(fn %{id: id} ->
+      id == {Appsignal.Ecto, event}
+    end)
+  end
+end
