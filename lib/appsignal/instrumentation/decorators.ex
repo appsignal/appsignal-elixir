bdaa9be3f4abb9bfdcf9f353704e54e31701a047
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-06-11 10:18:39 +0200

    Add Decorators.instrument/3

--- lib/appsignal/instrumentation/decorators.ex
+++ lib/appsignal/instrumentation/decorators.ex
@@ -1,6 +1,20 @@
 defmodule Appsignal.Instrumentation.Decorators do
-  use Decorator.Define, instrument: 0
+  @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
+
+  use Decorator.Define, instrument: 0, instrument: 1
   import Appsignal.Utils, only: [module_name: 1]
+
+  def instrument(namespace, body, %{module: module, name: name, arity: arity}) do
+    quote do
+      Appsignal.instrument(
+        "#{module_name(unquote(module))}.#{unquote(name)}/#{unquote(arity)}",
+        fn span ->
+          unquote(@span).set_namespace(span, unquote(namespace))
+          unquote(body)
+        end
+      )
+    end
+  end
 
   def instrument(body, %{module: module, name: name, arity: arity}) do
     quote do
--- test/appsignal/instrumentation_test.exs
+++ test/appsignal/instrumentation_test.exs
@@ -5,18 +5,26 @@ defmodule InstrumentedModule do
   def call do
     :ok
   end
+
+  @decorate instrument("background_job")
+  def background_job do
+    :ok
+  end
 end
 
 defmodule Appsignal.InstrumentationTest do
   use ExUnit.Case
   alias Appsignal.{Span, Test}
 
+  setup do
+    Test.Nif.start_link()
+    Test.Tracer.start_link()
+    Test.Span.start_link()
+    :ok
+  end
+
   describe "instrument/2" do
     setup do
-      Test.Nif.start_link()
-      Test.Tracer.start_link()
-      Test.Span.start_link()
-
       %{return: InstrumentedModule.call()}
     end
 
@@ -30,6 +38,32 @@ defmodule Appsignal.InstrumentationTest do
 
     test "sets the span's name" do
       assert {:ok, [{%Span{}, "InstrumentedModule.call/0"}]} = Test.Span.get(:set_name)
+    end
+
+    test "closes the span" do
+      assert {:ok, [{%Span{}}]} = Test.Tracer.get(:close_span)
+    end
+  end
+
+  describe "instrument/3, with a custom namespace" do
+    setup do
+      %{return: InstrumentedModule.background_job()}
+    end
+
+    test "calls the passed function, and returns its return", %{return: return} do
+      assert return == :ok
+    end
+
+    test "creates a root span" do
+      assert Test.Tracer.get(:create_span) == {:ok, [{"http_request", nil}]}
+    end
+
+    test "sets the span's name" do
+      assert {:ok, [{%Span{}, "InstrumentedModule.background_job/0"}]} = Test.Span.get(:set_name)
+    end
+
+    test "sets the span's namespace" do
+      assert {:ok, [{%Span{}, "background_job"}]} = Test.Span.get(:set_namespace)
     end
 
     test "closes the span" do
