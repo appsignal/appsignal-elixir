2f0dfc68bca86abf7793967a6b280d1f4c250027
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-07-10 14:11:35 +0200

    Instrumentation fallbacks (#573)
    * Move instrumentation helpers to Appsignal.Instrumentation.Helpers
    
    * Add Appsignal.Instrumentation.Helpers.instrument/3
    
    * Delegate Appsignal.instrument/1-2 to Appsignal.Instrumentation.Helpers

--- lib/appsignal/instrumentation/decorators.ex
+++ lib/appsignal/instrumentation/decorators.ex
@@ -22,7 +22,7 @@ defmodule Appsignal.Instrumentation.Decorators do
   def instrument(namespace, body, %{module: module, name: name, arity: arity})
       when is_binary(namespace) do
     quote do
-      Appsignal.instrument(
+      Appsignal.Instrumentation.Helpers.instrument(
         "#{module_name(unquote(module))}.#{unquote(name)}/#{unquote(arity)}",
         fn span ->
           unquote(@span).set_namespace(span, unquote(namespace))
@@ -34,7 +34,7 @@ defmodule Appsignal.Instrumentation.Decorators do
 
   def instrument(body, %{module: module, name: name, arity: arity}) do
     quote do
-      Appsignal.instrument(
+      Appsignal.Instrumentation.Helpers.instrument(
         "#{module_name(unquote(module))}.#{unquote(name)}/#{unquote(arity)}",
         fn -> unquote(body) end
       )
@@ -43,7 +43,7 @@ defmodule Appsignal.Instrumentation.Decorators do
 
   def instrument(body, %{module: module, name: name}) do
     quote do
-      Appsignal.instrument(
+      Appsignal.Instrumentation.Helpers.instrument(
         "#{module_name(unquote(module))}.#{unquote(name)}",
         fn -> unquote(body) end
       )
