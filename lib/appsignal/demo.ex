5999fdfb2434d40286744667eebb7b9d2775aa04
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-08-13 10:55:10 +0200

    Use instrumentation helpers in Appsignal.Demo.send_performance_sample/0

--- lib/appsignal/demo.ex
+++ lib/appsignal/demo.ex
@@ -5,30 +5,33 @@ end
 
 defmodule Appsignal.Demo do
   @moduledoc false
-  import Appsignal.Instrumentation.Helpers, only: [instrument: 2]
+  import Appsignal.Instrumentation, only: [instrument: 2, instrument: 3]
   @tracer Application.get_env(:appsignal, :appsignal_tracer, Appsignal.Tracer)
   @span Application.get_env(:appsignal, :appsignal_span, Appsignal.Span)
 
   def send_performance_sample do
-    span = create_demo_span()
+    instrument("DemoController#hello", "call.phoenix", fn span ->
+      span
+      |> @span.set_namespace("http_request")
+      |> @span.set_attribute("demo_sample", true)
+      |> @span.set_sample_data("environment", %{"method" => "GET", "request_path" => "/"})
 
-    instrument("call.phoenix_endpoint", fn ->
-      :timer.sleep(100)
+      instrument("call.phoenix_endpoint", fn ->
+        :timer.sleep(100)
 
-      instrument("query.ecto", fn ->
-        :timer.sleep(30)
-      end)
+        instrument("query.ecto", fn ->
+          :timer.sleep(30)
+        end)
 
-      instrument("query.ecto", fn ->
-        :timer.sleep(50)
-      end)
+        instrument("query.ecto", fn ->
+          :timer.sleep(50)
+        end)
 
-      instrument("render.phoenix_template", fn ->
-        :timer.sleep(10)
+        instrument("render.phoenix_template", fn ->
+          :timer.sleep(10)
+        end)
       end)
     end)
-
-    @tracer.close_span(span)
   end
 
   def send_error_sample do
