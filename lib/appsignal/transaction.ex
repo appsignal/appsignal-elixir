0c62aee7e34b8647315538882de745ae861b6621
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-10 11:19:20 +0100

    Add fallback modules with deprecation warnings for Transaction and TransactionRegistry

---
+++ lib/appsignal/transaction.ex
@@ -0,0 +1,27 @@
+defmodule Appsignal.Transaction do
+  @doc false
+
+  @deprecated "Use Appsignal.Tracer instead."
+  def start(_id, _namespace), do: nil
+
+  @deprecated
+  def generate_id, do: nil
+
+  @deprecated "Use Appsignal.Span instead."
+  def set_action(_action), do: nil
+
+  @deprecated "Use Appsignal.Span instead."
+  def set_sample_data(_key, _values), do: nil
+
+  @deprecated "Use Appsignal.Span instead."
+  def set_error(_transaction, _name, _message, _backtrace), do: nil
+
+  @deprecated "Use Appsignal.Span instead."
+  def set_error(_name, _message, _backtrace), do: nil
+
+  @deprecated "Use Appsignal.Tracer instead."
+  def finish(_), do: nil
+
+  @deprecated "Use Appsignal.Tracer instead."
+  def complete(_), do: nil
+end
---
+++ lib/appsignal/transaction_registry.ex
@@ -0,0 +1,8 @@
+defmodule Appsignal.TransactionRegistry do
+  @doc false
+  @deprecated "Use Appsignal.Span instead."
+
+  def lookup(pid) do
+    nil
+  end
+end
