845c69283570517f5e80347b2d157377fb342ba6
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2020-11-17 10:05:11 +0100

    Release 2.0.0

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Appsignal.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/appsignal/appsignal-elixir"
-  @version "2.0.0-beta.11"
+  @version "2.0.0"
 
   def project do
     [
