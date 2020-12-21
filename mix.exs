a2b7ee69b86f9558f70b93e875c2ab694a7c7ea5
Thijs Cadier <thijs@appsignal.com>
2020-12-21 09:27:01 +0100

    Bump to 2.0.6

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Appsignal.Mixfile do
   use Mix.Project
 
   @source_url "https://github.com/appsignal/appsignal-elixir"
-  @version "2.0.6-beta.1"
+  @version "2.0.6"
 
   def project do
     [
