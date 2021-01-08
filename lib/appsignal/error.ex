cf10c01507518bad87d316c33416c1a7f13bf1bd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-01-08 14:28:36 +0100

    Add Error.metadata/2 to accept Elixir exceptions

--- lib/appsignal/error.ex
+++ lib/appsignal/error.ex
@@ -1,13 +1,17 @@
 defmodule Appsignal.Error do
   @moduledoc false
-  def metadata(:error, reason, stack) do
-    exception = Exception.normalize(:error, reason, stack)
-
+  def metadata(%_{__exception__: true} = exception, stack) do
     {
       inspect(exception.__struct__),
       Exception.format_banner(:error, exception, stack),
       Appsignal.Stacktrace.format(stack)
     }
+  end
+
+  def metadata(:error, reason, stack) do
+    :error
+    |> Exception.normalize(reason, stack)
+    |> metadata(stack)
   end
 
   def metadata(kind, reason, stack) do
--- test/appsignal/error_test.exs
+++ test/appsignal/error_test.exs
@@ -1,6 +1,31 @@
 defmodule Appsignal.ErrorTest do
   use ExUnit.Case
 
+  describe "metadata/2, with an exception" do
+    setup do
+      try do
+        raise "Exception!"
+      rescue
+        exception -> %{metadata: Appsignal.Error.metadata(exception, __STACKTRACE__)}
+      end
+    end
+
+    test "extracts the error's name", %{metadata: metadata} do
+      assert {"RuntimeError", _message, _stack} = metadata
+    end
+
+    test "extracts the error's message", %{metadata: metadata} do
+      assert {_name, "** (RuntimeError) Exception!", _stack} = metadata
+    end
+
+    test "format's the error's stack trace", %{metadata: metadata} do
+      {_name, _message, stack} = metadata
+      assert is_list(stack)
+      assert length(stack) > 0
+      assert Enum.all?(stack, &is_binary(&1))
+    end
+  end
+
   describe "metadata/3, with an exception" do
     setup do
       try do
