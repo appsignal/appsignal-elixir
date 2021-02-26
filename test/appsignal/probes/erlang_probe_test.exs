e0ed6abb08cce451a954f70854e888f7335ac837
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2021-02-26 12:50:33 +0100

    Use one assertion per metric in ErlangProbeTest

--- test/appsignal/probes/erlang_probe_test.exs
+++ test/appsignal/probes/erlang_probe_test.exs
@@ -17,118 +17,239 @@ defmodule Appsignal.Probes.ErlangProbeTest do
     end
 
     test "gathers IO metrics", %{fake_appsignal: fake_appsignal} do
-      assert [
-               %{
-                 key: "erlang_io",
-                 tags: %{type: "output", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_io",
-                 tags: %{type: "input", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               }
-             ] = FakeAppsignal.get_gauges(fake_appsignal, "erlang_io")
+      metrics = FakeAppsignal.get_gauges(fake_appsignal, "erlang_io")
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_io",
+                   tags: %{type: "output", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_io",
+                   tags: %{type: "input", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
     end
 
     test "gathers scheduler metrics", %{fake_appsignal: fake_appsignal} do
-      assert [
-               %{
-                 key: "erlang_schedulers",
-                 tags: %{type: "online", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_schedulers",
-                 tags: %{type: "total", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               }
-             ] = FakeAppsignal.get_gauges(fake_appsignal, "erlang_schedulers")
+      metrics = FakeAppsignal.get_gauges(fake_appsignal, "erlang_schedulers")
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_schedulers",
+                   tags: %{type: "online", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_schedulers",
+                   tags: %{type: "total", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
     end
 
     test "gathers process metrics", %{fake_appsignal: fake_appsignal} do
-      assert [
-               %{
-                 key: "erlang_processes",
-                 tags: %{type: "count", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_processes",
-                 tags: %{type: "limit", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               }
-             ] = FakeAppsignal.get_gauges(fake_appsignal, "erlang_processes")
+      metrics = FakeAppsignal.get_gauges(fake_appsignal, "erlang_processes")
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_processes",
+                   tags: %{type: "count", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_processes",
+                   tags: %{type: "limit", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
     end
 
     test "gathers memory metrics", %{fake_appsignal: fake_appsignal} do
-      assert [
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "ets", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "code", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "binary", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "atom_used", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "atom", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "system", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "processes_used", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "processes", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               },
-               %{
-                 key: "erlang_memory",
-                 tags: %{type: "total", hostname: "Bobs-MBP.example.com"},
-                 value: _
-               }
-             ] = FakeAppsignal.get_gauges(fake_appsignal, "erlang_memory")
+      metrics = FakeAppsignal.get_gauges(fake_appsignal, "erlang_memory")
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "ets", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "code", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "binary", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "atom_used", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "atom", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "system", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "processes_used", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "processes", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "erlang_memory",
+                   tags: %{type: "total", hostname: "Bobs-MBP.example.com"},
+                   value: _
+                 },
+                 &1
+               )
+             )
     end
 
     test "gathers run queue lengths", %{fake_appsignal: fake_appsignal} do
-      assert [
-               %{
-                 key: "total_run_queue_lengths",
-                 tags: %{type: "io"},
-                 value: _
-               },
-               %{
-                 key: "total_run_queue_lengths",
-                 tags: %{type: "cpu"},
-                 value: _
-               },
-               %{
-                 key: "total_run_queue_lengths",
-                 tags: %{type: "total"},
-                 value: _
-               }
-             ] = FakeAppsignal.get_gauges(fake_appsignal, "total_run_queue_lengths")
+      metrics = FakeAppsignal.get_gauges(fake_appsignal, "total_run_queue_lengths")
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "total_run_queue_lengths",
+                   tags: %{type: "io"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "total_run_queue_lengths",
+                   tags: %{type: "cpu"},
+                   value: _
+                 },
+                 &1
+               )
+             )
+
+      assert Enum.any?(
+               metrics,
+               &match?(
+                 %{
+                   key: "total_run_queue_lengths",
+                   tags: %{type: "total"},
+                   value: _
+                 },
+                 &1
+               )
+             )
     end
   end
 
