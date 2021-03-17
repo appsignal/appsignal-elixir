appsignal/appsignal-elixir

commit c0b0dfb1e2feb9b597bab37d54d8a56134ece907

Add `Appsignal.Logger` to only log debug messages when the `:debug` configuration is turned on (#642)
* Add Config.debug?/0

* Add Appsignal.Logger to only log in :debug mode

* Use Appsignal.Logger for debug logs

* Ensure Appsignal.Logger.debug/1 returns `:ok`

lib/appsignal/config.ex
@@ -113,6 +113,14 @@ defmodule Appsignal.Config do
   defp do_active?(%{valid: true, active: true}), do: true
   defp do_active?(_), do: false
 
+  @doc """
+  Returns true if debug mode is turned on, false otherwise.
+  """
+  @spec debug?() :: boolean
+  def debug? do
+    Application.fetch_env!(:appsignal, :config)[:debug] || false
+  end
+
   def request_headers do
     Application.fetch_env!(:appsignal, :config)[:request_headers] || []
   end

config/config.exs
@@ -6,6 +6,7 @@ if Mix.env() in [:bench, :test, :test_no_nif] do
   config :appsignal, appsignal_diagnose_report: Appsignal.Diagnose.FakeReport
   config :appsignal, appsignal: Appsignal.FakeAppsignal
   config :appsignal, inet: FakeInet
+  config :appsignal, logger: Appsignal.Test.Logger
 
   config :appsignal, appsignal_tracer_nif: Appsignal.Test.Nif
   config :appsignal, appsignal_tracer: Appsignal.Test.Tracer

lib/appsignal.ex
@@ -39,7 +39,7 @@ defmodule Appsignal do
 
   @doc false
   def stop(_state) do
-    Logger.debug("AppSignal stopping.")
+    Appsignal.Logger.debug("AppSignal stopping.")
   end
 
   @doc false
@@ -61,12 +61,12 @@ defmodule Appsignal do
         Logger.info("AppSignal disabled.")
 
       {:ok, true} ->
-        Logger.debug("AppSignal starting.")
+        Appsignal.Logger.debug("AppSignal starting.")
         Config.write_to_environment()
         Appsignal.Nif.start()
 
         if Appsignal.Nif.loaded?() do
-          Logger.debug("AppSignal started.")
+          Appsignal.Logger.debug("AppSignal started.")
         else
           Logger.error(
             "Failed to start AppSignal. Please run the diagnose task " <>

lib/appsignal/ecto.ex
@@ -29,7 +29,7 @@ defmodule Appsignal.Ecto do
 
     case :telemetry.attach({__MODULE__, event}, event, &handle_event/4, :ok) do
       :ok ->
-        Logger.debug("Appsignal.Ecto attached to #{inspect(event)}")
+        Appsignal.Logger.debug("Appsignal.Ecto attached to #{inspect(event)}")
 
       {:error, _} = error ->
         Logger.warn("Appsignal.Ecto not attached to #{inspect(event)}: #{inspect(error)}")

lib/appsignal/error/backend.ex
@@ -12,7 +12,7 @@ defmodule Appsignal.Error.Backend do
   def attach do
     case Logger.add_backend(Appsignal.Error.Backend) do
       {:error, error} -> Logger.warn("Appsignal.Error.Backend not attached to Logger: #{error}")
-      _ -> Logger.debug("Appsignal.Error.Backend attached to Logger")
+      _ -> Appsignal.Logger.debug("Appsignal.Error.Backend attached to Logger")
     end
   end
 

lib/appsignal/logger.ex
@@ -1,0 +1,14 @@
+defmodule Appsignal.Logger do
+  require Logger
+  @logger Application.get_env(:appsignal, :logger, Logger)
+
+  @spec debug(any()) :: :ok
+  @doc """
+  Passes the debug message to `Logger.debug/1` if `Appsignal.Config.debug?/0`
+  is `true`.
+  """
+  def debug(log) do
+    Appsignal.Config.debug?() && @logger.debug(log)
+    :ok
+  end
+end

lib/appsignal/test/logger.ex
@@ -1,0 +1,10 @@
+defmodule Appsignal.Test.Logger do
+  @moduledoc false
+  use Appsignal.Test.Wrapper
+  require Logger
+
+  def debug(log) do
+    add(:debug, {log})
+    Logger.debug(log)
+  end
+end

test/appsignal/config_test.exs
@@ -115,6 +115,20 @@ defmodule Appsignal.ConfigTest do
     end
   end
 
+  describe "debug?" do
+    test "when debug mode is turned on" do
+      assert with_config(%{debug: true}, &Config.debug?/0) == true
+    end
+
+    test "when debug mode is turned off" do
+      assert with_config(%{debug: false}, &Config.debug?/0) == false
+    end
+
+    test "when debug mode is not configured" do
+      assert with_config(%{}, &Config.debug?/0) == false
+    end
+  end
+
   describe "request_headers" do
     test "returns an empty list by default" do
       assert with_config(%{}, &Config.request_headers/0) == []

commit b2c013dde3432d0f1c7c081c810557c2a57cd49d

fix: use get_env/3 to avoid compilation errors (#644)
Fixes bug occurring when no config was specified for AppSignal. The
`debug?` function would try to load the config even if no config was
loaded, which caused an error trying to fetch a key from an undefined
config.

```
** (ArgumentError) could not fetch application environment :config for
  application :appsignal because configuration at :config was not set
```

This issue was introduced by PR #642 which added a check to the logger
to see if the AppSignal config was set to `debug` is `true`. And would
occur in issue #643 when called by the plug and phoenix packages:

- https://github.com/appsignal/appsignal-elixir-phoenix/pull/12
- https://github.com/appsignal/appsignal-elixir-plug/pull/11

Fixes #643

An alternative solution was considered, to fall back on an empty list
instead, but this solution was chosen as it uses the `@default_config`
and thus replicates the default behavior as when the config was loaded
but not altered.

```
# Example alternative solutino
Application.get_env(:appsignal, :config, [])[:debug] || false
```

---

Original author of the change is @the-mikedavis.
Commit message updated before merge by @tombruijn to include all
information from issue #643 and debugging for future reference.

lib/appsignal/config.ex
@@ -118,7 +118,7 @@ defmodule Appsignal.Config do
   """
   @spec debug?() :: boolean
   def debug? do
-    Application.fetch_env!(:appsignal, :config)[:debug] || false
+    Application.get_env(:appsignal, :config, @default_config)[:debug] || false
   end
 
   def request_headers do