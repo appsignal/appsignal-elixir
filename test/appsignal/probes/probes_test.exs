d9c4cb8c36aa559151877943f999cb1aa4e0fa5a
Noemi <noemi@appsignal.com>
2021-10-07 14:41:21 +0200

    Simplify Appsignal.Probes and add tests
    This commit removes the logic that ensures the input of register/2
    is a function, since it can now take the shape of a supervisor
    child spec.
    
    It also includes some refactoring of Appsignal.Probes and its
    tests, and it adds some tests regarding the behaviour of the
    system when a second probe is registered under the same name.
    The behaviour should be the same as it was before the changes,
    that is, the new probe replaces the old one, but now it is
    under test coverage.

--- test/appsignal/probes/probes_test.exs
+++ test/appsignal/probes/probes_test.exs
@@ -6,10 +6,12 @@ defmodule Appsignal.Probes.ProbesTest do
   describe "register/2" do
     test "registers a probe when given a function as probe" do
       assert :ok == Probes.register(:some_probe, fn -> nil end)
+      Probes.unregister(:some_probe)
     end
 
-    test "returns an error tupple when probe is not a function" do
-      assert {:error, _} = Probes.register(:some_probe, :some_value)
+    test "registers a probe when given a module as probe" do
+      assert :ok == Probes.register(:some_probe, FakeServerProbe)
+      Probes.unregister(:some_probe)
     end
   end
 
@@ -21,7 +23,7 @@ defmodule Appsignal.Probes.ProbesTest do
     test "once a probe is registered, it is called by the probes system", %{
       fake_probe: fake_probe
     } do
-      Probes.register(:test_probe, &FakeFunctionProbe.call/0)
+      Probes.register(:test_probe, FakeFunctionProbe.call(fake_probe))
 
       refute FakeFunctionProbe.get(fake_probe, :probe_called)
 
@@ -32,11 +34,57 @@ defmodule Appsignal.Probes.ProbesTest do
       Probes.unregister(:test_probe)
     end
 
+    test "when a probe is unregistered, it is no longer called by the probes system", %{
+      fake_probe: fake_probe
+    } do
+      Probes.register(:test_probe, FakeFunctionProbe.call(fake_probe))
+
+      until(fn ->
+        assert FakeFunctionProbe.get(fake_probe, :probe_called)
+      end)
+
+      Probes.unregister(:test_probe)
+      FakeFunctionProbe.update(fake_probe, :probe_called, false)
+
+      repeatedly(fn ->
+        refute FakeFunctionProbe.get(fake_probe, :probe_called)
+      end)
+    end
+
+    test "when a probe with the same name is registered, the existing one is terminated", %{
+      fake_probe: fake_probe
+    } do
+      Probes.register(:test_probe, FakeFunctionProbe.call(fake_probe))
+
+      until(fn ->
+        assert FakeFunctionProbe.get(fake_probe, :probe_called)
+      end)
+
+      agent = start_supervised!({Agent, fn -> false end})
+
+      function = fn ->
+        Agent.update(agent, fn _ -> true end)
+      end
+
+      Probes.register(:test_probe, function)
+      FakeFunctionProbe.update(fake_probe, :probe_called, false)
+
+      until(fn ->
+        assert Agent.get(agent, fn state -> state end)
+      end)
+
+      repeatedly(fn ->
+        refute FakeFunctionProbe.get(fake_probe, :probe_called)
+      end)
+
+      Probes.unregister(:test_probe)
+    end
+
     test "a probe does not get called by the probes system if it's disabled", %{
       fake_probe: fake_probe
     } do
       AppsignalTest.Utils.with_config(%{enable_minutely_probes: false}, fn ->
-        Probes.register(:test_probe, &FakeFunctionProbe.call/0)
+        Probes.register(:test_probe, FakeFunctionProbe.call(fake_probe))
 
         repeatedly(fn ->
           refute FakeFunctionProbe.get(fake_probe, :probe_called)
@@ -47,7 +95,7 @@ defmodule Appsignal.Probes.ProbesTest do
     end
 
     test "handles non-exception errors", %{fake_probe: fake_probe} do
-      Probes.register(:test_probe, &FakeFunctionProbe.fail/0)
+      Probes.register(:test_probe, FakeFunctionProbe.fail(fake_probe))
 
       until(fn ->
         assert FakeFunctionProbe.get(fake_probe, :probe_called)
