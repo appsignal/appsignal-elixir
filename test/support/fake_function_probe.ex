6d0f0643931dd401e3fa91be3d90f7fda2c63621
Noemi <noemi@appsignal.com>
2021-10-11 17:32:04 +0200

    Turn FakeFunctionProbe into an Agent
    There is little benefit to `FakeFunctionProbe` being a
    `TestAgent`, and the specificity of the `TestAgent`'s `child_spec`
    gets in the way of spawning multiple `FakeFunctionProbe`s.
    
    This commit makes `FakeFunctionProbe` into a regular `Agent`, and
    uses it in the probe override test, which requires two different
    probes to be register. This test previously used a vanilla `Agent`
    as a workaround for not being able to spawn multiple
    `FakeFunctionProbe` instances.

--- test/support/fake_function_probe.ex
+++ test/support/fake_function_probe.ex
@@ -1,20 +1,32 @@
 defmodule FakeFunctionProbe do
-  use TestAgent
+  use Agent
+
+  def start_link(_) do
+    Agent.start_link(fn -> false end)
+  end
 
   def call(pid) do
     fn ->
-      if alive?() do
-        update(pid, :probe_called, true)
+      if Process.alive?(pid) do
+        Agent.update(pid, fn _ -> true end)
       end
     end
   end
 
   def fail(pid) do
     fn ->
-      if alive?() do
-        update(pid, :probe_called, true)
+      if Process.alive?(pid) do
+        Agent.update(pid, fn _ -> true end)
         raise("Failure")
       end
     end
+  end
+
+  def called?(pid) do
+    Agent.get(pid, fn state -> state end)
+  end
+
+  def clear(pid) do
+    Agent.update(pid, fn _ -> false end)
   end
 end
