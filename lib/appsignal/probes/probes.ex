5465d62054f12b6b448ea5fd9db28c5f7961caa2
Noemi <noemi@appsignal.com>
2021-10-28 18:50:53 +0200

    Implement scheduler utilization metric
    This commit implements the Erlang scheduler utilization metric
    on the minutely probes.
    
    In order to report on the scheduler utilization, the probe must
    keep state from its previous execution. That is, to measure the
    scheduler utilization between points in time A and B, you must
    call `:scheduler.sample/0` on point A, then pass the sample it
    returns to `:scheduler.utilization/1 on point B.
    
    To be able to keep state in the probe, we keep a map of states
    in `Appsignal.Probes` alongside the map of probes. If a probe
    takes an argument, the state returned by its previous run is
    retrieved from the states map and passed as the argument, and
    the probe's return value is stored in the states map to be used
    as the next run's state.

--- lib/appsignal/probes/probes.ex
+++ lib/appsignal/probes/probes.ex
@@ -7,10 +7,10 @@ defmodule Appsignal.Probes do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def register(name, probe) do
+  def register(name, probe, state \\ nil) do
     if genserver_running?() do
       if is_function(probe) do
-        GenServer.cast(__MODULE__, {name, probe})
+        GenServer.cast(__MODULE__, {:register, {name, probe, state}})
         :ok
       else
         Logger.debug(fn ->
@@ -25,43 +25,78 @@ defmodule Appsignal.Probes do
   end
 
   def unregister(name) do
-    GenServer.cast(__MODULE__, name)
+    GenServer.cast(__MODULE__, {:unregister, name})
     :ok
   end
 
   def init([]) do
     schedule_probes()
-    {:ok, %{}}
+    {:ok, {%{}, %{}}}
   end
 
-  def handle_cast({name, probe}, probes) do
+  def handle_cast({:register, {name, probe, state}}, {probes, states}) do
     if Map.has_key?(probes, name) do
       Logger.debug(fn -> "A probe with name '#{name}' already exists. Overriding that one." end)
     end
 
-    {:noreply, Map.put(probes, name, probe)}
+    {:noreply,
+     {
+       Map.put(probes, name, probe),
+       Map.put(states, name, state)
+     }}
   end
 
-  def handle_cast(name, probes) do
-    {:noreply, Map.delete(probes, name)}
+  def handle_cast({:unregister, name}, {probes, states}) do
+    {:noreply,
+     {
+       Map.delete(probes, name),
+       Map.delete(states, name)
+     }}
   end
 
-  def handle_info(:run_probes, probes) do
-    if Appsignal.Config.minutely_probes_enabled?() do
-      Enum.each(probes, fn {name, probe} ->
-        Task.start(fn ->
-          try do
-            probe.()
-          rescue
-            e ->
-              Logger.error("Error while calling probe #{name}: #{inspect(e)}")
+  def handle_info(:run_probes, {probes, states}) do
+    states =
+      if Appsignal.Config.minutely_probes_enabled?() do
+        stream =
+          Task.async_stream(
+            probes,
+            fn {name, probe} ->
+              state = Map.get(states, name)
+
+              try do
+                {name, call_probe(probe, state)}
+              rescue
+                e ->
+                  Logger.error("Error while calling probe #{name}: #{inspect(e)}")
+                  {name, state}
+              end
+            end,
+            ordered: false,
+            timeout: 30000,
+            on_timeout: :kill_task
+          )
+
+        Enum.reduce(stream, states, fn result, states ->
+          case result do
+            {:ok, {name, state}} -> Map.put(states, name, state)
+            {:exit, :timeout} -> states
           end
         end)
-      end)
-    end
+      else
+        states
+      end
 
     schedule_probes()
-    {:noreply, probes}
+    {:noreply, {probes, states}}
+  end
+
+  defp call_probe(probe, _state) when is_function(probe, 0) do
+    probe.()
+    nil
+  end
+
+  defp call_probe(probe, state) when is_function(probe, 1) do
+    probe.(state)
   end
 
   def child_spec(_) do
