9b6472440112771ab8ab66e009b8d5144540dfd4
Tom de Bruijn <tom@tomdebruijn.com>
2022-02-14 15:33:02 +0100

    Don't mock all of :erlang.system_info/1 in tests (#760)
    Only mock the key we need to mock. This way any other values we ask from
    `:erlang.system_info/1` isn't always the same host triple.
    
    When something other than `:system_architecture` is asked, return the
    real value.
    
    [skip changeset]

--- test/support/fake_erlang.ex
+++ test/support/fake_erlang.ex
@@ -1,5 +1,6 @@
 defmodule FakeErlang do
-  use TestAgent, %{system_info: 'x86_64-apple-darwin20.2.0'}
+  use TestAgent, %{system_architecture: 'x86_64-apple-darwin20.2.0'}
 
-  def system_info(_), do: get(__MODULE__, :system_info)
+  def system_info(:system_architecture), do: get(__MODULE__, :system_architecture)
+  def system_info(key), do: :erlang.system_info(key)
 end
