d1daea99a3ed54f1fb9eb6c37a38a74c2254e5f8
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2022-10-25 17:33:38 +0200

    Add prototype for stacktrace argument types
    Instead of stripping out arguments from exceptions like
    UndefinedFunctionErrors, this prototype replaces the arguments with
    their types.

--- lib/appsignal/stacktrace.ex
+++ lib/appsignal/stacktrace.ex
@@ -1,3 +1,22 @@
+defmodule ArgumentType do
+  defstruct [:value, :type]
+
+  def from_term(term) do
+    %ArgumentType{value: term, type: typeof(term)}
+  end
+
+  for type <-
+        ~w[boolean binary bitstring float function integer list map nil pid port reference tuple atom] do
+    defp typeof(x) when unquote(:"is_#{type}")(x), do: unquote(type)
+  end
+
+  defp typeof(_), do: "unknown"
+end
+
+defimpl Inspect, for: ArgumentType do
+  def inspect(%ArgumentType{type: type}, _opts), do: type
+end
+
 defmodule Appsignal.Stacktrace do
   @moduledoc false
 
@@ -24,11 +43,15 @@ defmodule Appsignal.Stacktrace do
 
   defp format_stacktrace_entry(entry) when is_binary(entry), do: entry
 
-  defp format_stacktrace_entry({module, function, arity, location}) when is_list(arity) do
-    format_stacktrace_entry({module, function, length(arity), location})
+  defp format_stacktrace_entry({module, function, arguments, location}) when is_list(arguments) do
+    Exception.format_stacktrace_entry({module, function, to_types(arguments), location})
   end
 
   defp format_stacktrace_entry(entry) do
     Exception.format_stacktrace_entry(entry)
+  end
+
+  defp to_types(arguments) do
+    Enum.map(arguments, &ArgumentType.from_term/1)
   end
 end
