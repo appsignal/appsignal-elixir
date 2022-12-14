c1a4cde5247112f9b938cc2810fc8dce83bbfef2
Noemi <45180344+unflxw@users.noreply.github.com>
2022-12-14 17:42:27 +0100

    Add FakeAppsignal counters and distribution values
    Implement counters and distribution values in the FakeAppsignal
    module, similarly to how gauges are implemented.

--- test/support/appsignal/fake_appsignal.ex
+++ test/support/appsignal/fake_appsignal.ex
@@ -1,29 +1,45 @@
 defmodule Appsignal.FakeAppsignal do
   use TestAgent, %{
-    gauges: []
+    gauges: [],
+    counters: [],
+    distribution_values: []
   }
 
+  def add_distribution_value(key, value, tags \\ %{}) do
+    add(:distribution_values, %{key: key, value: value, tags: tags})
+  end
+
+  def increment_counter(key, value \\ 1, tags \\ %{}) do
+    add(:counters, %{key: key, value: value, tags: tags})
+  end
+
   def set_gauge(key, value, tags \\ %{}) do
-    if alive?() do
-      Agent.update(__MODULE__, fn state ->
-        {_, new_state} =
-          Map.get_and_update(state, :gauges, fn current ->
-            gauge = %{key: key, value: value, tags: tags}
+    add(:gauges, %{key: key, value: value, tags: tags})
+  end
 
-            case current do
-              nil -> {nil, [gauge]}
-              _ -> {current, [gauge | current]}
-            end
-          end)
+  def get_distribution_values(key) do
+    Enum.filter(get(__MODULE__, :distribution_values), fn element ->
+      match?(%{key: ^key, tags: _, value: _}, element)
+    end)
+  end
 
-        new_state
-      end)
-    end
+  def get_counters(key) do
+    Enum.filter(get(__MODULE__, :counters), fn element ->
+      match?(%{key: ^key, tags: _, value: _}, element)
+    end)
   end
 
   def get_gauges(key) do
     Enum.filter(get(__MODULE__, :gauges), fn element ->
       match?(%{key: ^key, tags: _, value: _}, element)
     end)
+  end
+
+  defp add(key, event) do
+    if alive?() do
+      Agent.update(__MODULE__, fn state ->
+        Map.update!(state, key, fn current -> [event | current] end)
+      end)
+    end
   end
 end
