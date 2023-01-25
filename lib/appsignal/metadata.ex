72d4ed5d7ffeb3bc3ba8a880c6980d23f76cdc2a
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-01-25 19:54:14 +0100

    Add metadata functions for Plug/Phoenix libraries
    The metadata function in the Plug integration now includes a name,
    category, params and session data.
    
    This patch also implements the protocol for Any, with fallbacks. Other
    implementation can now delegate to these if they don't implement all
    functions.

--- lib/appsignal/metadata.ex
+++ lib/appsignal/metadata.ex
@@ -1,4 +1,43 @@
 defprotocol Appsignal.Metadata do
+  @fallback_to_any true
   @spec metadata(t) :: map()
   def metadata(value)
+
+  @fallback_to_any true
+  @spec name(t) :: nil | binary()
+  def name(value)
+
+  @fallback_to_any true
+  @spec category(t) :: nil | binary()
+  def category(value)
+
+  @fallback_to_any true
+  @spec params(t) :: map()
+  def params(value)
+
+  @fallback_to_any true
+  @spec session(t) :: map()
+  def session(value)
+end
+
+defimpl Appsignal.Metadata, for: Any do
+  def metadata(_) do
+    %{}
+  end
+
+  def name(_) do
+    nil
+  end
+
+  def category(_) do
+    nil
+  end
+
+  def params(_) do
+    %{}
+  end
+
+  def session(_) do
+    %{}
+  end
 end
