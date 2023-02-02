63c2de8ed4557e6b2a809339bf5a38dd177580fd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-02 14:13:17 +0100

    Don't match econnrefused in ReportTest exactly (#819)
    One of the error scenarios in the diagnose reports is when an unexpected
    error occurs. In the tests, this is simulated by taking the test
    diagnose server down, resulting in a :econnrefused.
    
    In some situations, the error thrown is a :closed, which made this test
    brittle. To fix that, the test no longer explicitly tests for a
    :econnrefused. Rather, it just tests if an error with a reason key is
    returned when this happens.

--- test/appsignal/diagnose/report_test.exs
+++ test/appsignal/diagnose/report_test.exs
@@ -77,7 +77,7 @@ defmodule Mix.Tasks.Appsignal.Diagnose.ReportTest do
     end
 
     test "sends the diagnostics report to AppSignal and returns an error" do
-      assert send() == {:error, %{reason: :econnrefused}}
+      assert {:error, %{reason: _}} = send()
     end
   end
 end
