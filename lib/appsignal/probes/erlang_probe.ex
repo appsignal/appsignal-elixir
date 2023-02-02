ae734cbbc2c3aa1856f3b33eb7f7b645076e3bdd
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-02 16:06:36 +0100

    Move call to set_gauge/3 to ErlangProbe.call/1
    To make testing easier, move the call to set_gauge/3 to one place. This
    way, the data that's passed to the gauges can be collected before its
    set.

--- lib/appsignal/probes/erlang_probe.ex
+++ lib/appsignal/probes/erlang_probe.ex
@@ -8,50 +8,59 @@ defmodule Appsignal.Probes.ErlangProbe do
   def call(sample \\ nil) do
     next_sample = sample_schedulers()
 
-    io_metrics()
-    scheduler_metrics()
-    process_metrics()
-    memory_metrics()
-    atom_metrics()
-    run_queue_lengths()
-    scheduler_utilization_metrics(sample, next_sample)
+    sample
+    |> metrics(next_sample)
+    |> Enum.each(&set_gauge/1)
 
     next_sample
   end
 
+  defp metrics(sample, next_sample) do
+    io_metrics() ++
+      scheduler_metrics() ++
+      process_metrics() ++
+      memory_metrics() ++
+      atom_metrics() ++
+      run_queue_lengths() ++
+      scheduler_utilization_metrics(sample, next_sample)
+  end
+
   defp io_metrics do
     {{:input, input}, {:output, output}} = :erlang.statistics(:io)
-    set_gauge("erlang_io", Kernel.div(input, 1024), %{type: "input"})
-    set_gauge("erlang_io", Kernel.div(output, 1024), %{type: "output"})
+
+    [
+      {"erlang_io", Kernel.div(input, 1024), %{type: "input"}},
+      {"erlang_io", Kernel.div(output, 1024), %{type: "output"}}
+    ]
   end
 
   defp scheduler_metrics do
-    set_gauge("erlang_schedulers", :erlang.system_info(:schedulers), %{type: "total"})
-
-    set_gauge(
-      "erlang_schedulers",
-      :erlang.system_info(:schedulers_online),
-      %{type: "online"}
-    )
+    [
+      {"erlang_schedulers", :erlang.system_info(:schedulers), %{type: "total"}},
+      {"erlang_schedulers", :erlang.system_info(:schedulers_online), %{type: "online"}}
+    ]
   end
 
   defp process_metrics do
-    set_gauge("erlang_processes", :erlang.system_info(:process_limit), %{type: "limit"})
-
-    set_gauge("erlang_processes", :erlang.system_info(:process_count), %{type: "count"})
+    [
+      {"erlang_processes", :erlang.system_info(:process_limit), %{type: "limit"}},
+      {"erlang_processes", :erlang.system_info(:process_count), %{type: "count"}}
+    ]
   end
 
   defp memory_metrics do
     memory = :erlang.memory()
 
-    Enum.each(memory, fn {key, value} ->
-      set_gauge("erlang_memory", Kernel.div(value, 1024), %{type: to_string(key)})
+    Enum.map(memory, fn {key, value} ->
+      {"erlang_memory", Kernel.div(value, 1024), %{type: to_string(key)}}
     end)
   end
 
   defp atom_metrics do
-    set_gauge("erlang_atoms", :erlang.system_info(:atom_limit), %{type: "limit"})
-    set_gauge("erlang_atoms", :erlang.system_info(:atom_count), %{type: "count"})
+    [
+      {"erlang_atoms", :erlang.system_info(:atom_limit), %{type: "limit"}},
+      {"erlang_atoms", :erlang.system_info(:atom_count), %{type: "count"}}
+    ]
   end
 
   defp run_queue_lengths do
@@ -72,12 +81,14 @@ defmodule Appsignal.Probes.ErlangProbe do
         :erlang.statistics(:total_run_queue_lengths)
       end
 
-    set_gauge("total_run_queue_lengths", total, %{type: "total"})
-    set_gauge("total_run_queue_lengths", cpu, %{type: "cpu"})
-    set_gauge("total_run_queue_lengths", total - cpu, %{type: "io"})
+    [
+      {"total_run_queue_lengths", total, %{type: "total"}},
+      {"total_run_queue_lengths", cpu, %{type: "cpu"}},
+      {"total_run_queue_lengths", total - cpu, %{type: "io"}}
+    ]
   end
 
-  defp scheduler_utilization_metrics(nil, _), do: nil
+  defp scheduler_utilization_metrics(nil, _), do: []
 
   defp scheduler_utilization_metrics(sample, next_sample) do
     utilization = scheduler_utilization(sample, next_sample)
@@ -85,8 +96,8 @@ defmodule Appsignal.Probes.ErlangProbe do
     utilization
     |> Enum.map(&Tuple.to_list/1)
     |> Enum.filter(fn [type | _] -> type == :normal end)
-    |> Enum.each(fn [_, id, value, _] ->
-      set_gauge("erlang_scheduler_utilization", value * 100, %{type: "normal", id: "#{id}"})
+    |> Enum.map(fn [_, id, value, _] ->
+      {"erlang_scheduler_utilization", value * 100, %{type: "normal", id: "#{id}"}}
     end)
   end
 
@@ -115,7 +126,7 @@ defmodule Appsignal.Probes.ErlangProbe do
     end
   end
 
-  defp set_gauge(name, value, tags) do
+  defp set_gauge({name, value, tags}) do
     @appsignal.set_gauge(
       name,
       value,
