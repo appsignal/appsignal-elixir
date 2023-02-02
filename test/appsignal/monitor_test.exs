29b771b0baf8b6fe24d25926c6758cb8243e3d46
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-02 14:39:24 +0100

    Update MonitorTest to fix brittleness (#816)
    * Remove nonexisting pids test from MonitorTest
    
    When starting a monitor with a PID that doesn't exist, the DOWN message
    is immediately sent. This is a core language feature which doesn't have
    any implementation on our end, so it doesn't need a test.
    
    * MonitorTest does not match assertions exactly
    
    Previously, tests in MonitorTest checked the exact result of the
    monitors in its internal state. This patch makes sure to only test
    what's asserted, meaning it only checks if a monitor is added, and if a
    monitor is only added once.

--- test/appsignal/monitor_test.exs
+++ test/appsignal/monitor_test.exs
@@ -13,25 +13,27 @@ defmodule Appsignal.MonitorTest do
   end
 
   test "monitors a process" do
-    {:monitors, previous} = Process.info(monitor_pid(), :monitors)
-
     Monitor.add()
 
     until(fn ->
-      assert Process.info(monitor_pid(), :monitors) ==
-               {:monitors, previous ++ [{:process, self()}]}
+      {:monitors, monitors} = Process.info(monitor_pid(), :monitors)
+
+      assert Enum.any?(monitors, fn {:process, pid} ->
+               pid == self()
+             end)
     end)
   end
 
   test "does not monitor a process more than once" do
-    {:monitors, previous} = Process.info(monitor_pid(), :monitors)
-
     Monitor.add()
     Monitor.add()
 
     until(fn ->
-      assert Process.info(monitor_pid(), :monitors) ==
-               {:monitors, previous ++ [{:process, self()}]}
+      {:monitors, monitors} = Process.info(monitor_pid(), :monitors)
+
+      assert Enum.count(monitors, fn {:process, pid} ->
+               pid == self()
+             end) == 1
     end)
   end
 
@@ -48,19 +50,6 @@ defmodule Appsignal.MonitorTest do
 
     until(fn ->
       assert lookup(pid) == []
-    end)
-  end
-
-  test "automatically removes pids that don't exist from the monitor list" do
-    pid = :erlang.list_to_pid('<0.999.0>')
-    GenServer.cast(Appsignal.Monitor, {:monitor, pid})
-
-    until(fn ->
-      assert MapSet.member?(:sys.get_state(Appsignal.Monitor), pid)
-    end)
-
-    until(fn ->
-      refute MapSet.member?(:sys.get_state(Appsignal.Monitor), pid)
     end)
   end
 
