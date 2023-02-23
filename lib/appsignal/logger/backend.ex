34c8dbfa745443d4eb4542dd109d9e9a679b56fa
Jeff Kreeftmeijer <jeffkreeftmeijer@gmail.com>
2023-02-23 15:48:52 +0100

    Add Appsignal.Logger.Backend
    Appsignal.Logger.Backend is attached to Elixir's logger to send logs to
    AppSignal. Use Logger.add_backend/1 to set it up:
    
        Logger.add_backend({Appsignal.Logger.Backend, "phoenix"})
    
    The second argument in the tuple is the log group name, which could be
    "phoenix" for a Phoenix application, for example.

---
+++ lib/appsignal/logger/backend.ex
@@ -0,0 +1,20 @@
+defmodule Appsignal.Logger.Backend do
+  @behaviour :gen_event
+
+  def init({__MODULE__, group}) do
+    {:ok, group}
+  end
+
+  def init(_) do
+    {:ok, "phoenix"}
+  end
+
+  def handle_event({level, _gl, {Logger, message, _timestamp, metadata}}, group) do
+    Appsignal.Logger.log(level, group, IO.chardata_to_string(message), Enum.into(metadata, %{}))
+    {:ok, group}
+  end
+
+  def handle_call(_messsage, group) do
+    {:ok, nil, group}
+  end
+end
---
+++ test/appsignal/logger/backend_test.exs
@@ -0,0 +1,39 @@
+defmodule Appsignal.Logger.BackendTest do
+  use ExUnit.Case
+
+  setup do
+    start_supervised!(Appsignal.Test.Nif)
+    :ok
+  end
+
+  test "handle_event/2 sends logs to the extension" do
+    Appsignal.Logger.Backend.handle_event(
+      {
+        :info,
+        self(),
+        {
+          Logger,
+          ~c"foo bar baz",
+          {{2023, 2, 23}, {14, 25, 56, 241}},
+          [
+            erl_level: :info,
+            application: :phoenix,
+            domain: [:elixir],
+            file: "lib/phoenix/logger.ex",
+            function: "phoenix_endpoint_start/4",
+            gl: self(),
+            line: 211,
+            mfa: {Phoenix.Logger, :phoenix_endpoint_start, 4},
+            module: Phoenix.Logger,
+            pid: self(),
+            request_id: "F0Z3jGx7KNZWD9gAAAFD",
+            time: 1_677_159_356_241_326
+          ]
+        }
+      },
+      "phoenix"
+    )
+
+    assert [{"phoenix", 3, "foo bar baz", _}] = Appsignal.Test.Nif.get!(:log)
+  end
+end
