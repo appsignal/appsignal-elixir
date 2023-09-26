68ec699ead0c82dff11c73dc2ec1cf1cd928d4d0
Noemi <45180344+unflxw@users.noreply.github.com>
2023-09-26 15:46:01 +0200

    Add Erlang :logger handler
    Add an Erlang-style :logger handler as recommended for Elixir 1.15
    and above. Include an `Appsignal.Logger.Handler.add/2` method for
    convenience to save users from manually declaring an appropriate
    configuration for the handler.
    
    Include a `.remove/0` method as well for symmetry.

---
+++ test/appsignal/logger/handler_test.exs
@@ -0,0 +1,33 @@
+defmodule Appsignal.Logger.HandlerTest do
+  use ExUnit.Case
+  require Logger
+
+  setup do
+    Appsignal.Logger.Handler.add("some_group")
+    start_supervised!(Appsignal.Test.Nif)
+    on_exit(fn -> Appsignal.Logger.Handler.remove() end)
+    :ok
+  end
+
+  case Version.compare(System.version(), "1.15.0") do
+    :lt ->
+      nil
+
+    _ ->
+      test "add/2 sets up a :logger handler" do
+        Logger.error("A bad thing happened!")
+
+        assert [
+                 {"some_group", 6, 0, "A bad thing happened!", _}
+               ] = Appsignal.Test.Nif.get!(:log)
+      end
+
+      test "remove/0 removes the :logger handler" do
+        :logger.remove_handler(:appsignal_log)
+
+        Logger.error("A bad thing happened!")
+
+        assert :error = Appsignal.Test.Nif.get(:log)
+      end
+  end
+end
