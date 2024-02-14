2d3130fd5d6bbdcdb91e35f581857e05c8ec292a
Tom de Bruijn <tom@tomdebruijn.com>
2024-02-14 16:24:01 +0100

    Bump agent to version 0.33.0 (#919)
    The agent update and changesets are updated automatically.
    
    [skip review]

--- agent.exs
+++ agent.exs
@@ -4,7 +4,7 @@
 # Modifications to this file will be overwritten with the next agent release.
 
 defmodule Appsignal.Agent do
-  def version, do: "0.31.2"
+  def version, do: "0.33.0"
 
   def mirrors do
     [
@@ -16,55 +16,55 @@ defmodule Appsignal.Agent do
   def triples do
     %{
       "x86_64-darwin" => %{
-        checksum: "42cdf814a89e5d6bd6e5cd9ba84103df82b43418012bb4f9251e98d0c3627759",
+        checksum: "aa026c58c6b13e09eaede5c362472e86f25c896ce9d33e6928a967372f0894be",
         filename: "appsignal-x86_64-darwin-all-static.tar.gz"
       },
       "universal-darwin" => %{
-        checksum: "42cdf814a89e5d6bd6e5cd9ba84103df82b43418012bb4f9251e98d0c3627759",
+        checksum: "aa026c58c6b13e09eaede5c362472e86f25c896ce9d33e6928a967372f0894be",
         filename: "appsignal-x86_64-darwin-all-static.tar.gz"
       },
       "aarch64-darwin" => %{
-        checksum: "8db9e31e090e767b1157d969521967f322be9dd73eb1b677b6192eb4c987af72",
+        checksum: "4318b13bba02f8a98226e9d747a42267f790932a0893b451567b7611fb09961b",
         filename: "appsignal-aarch64-darwin-all-static.tar.gz"
       },
       "arm64-darwin" => %{
-        checksum: "8db9e31e090e767b1157d969521967f322be9dd73eb1b677b6192eb4c987af72",
+        checksum: "4318b13bba02f8a98226e9d747a42267f790932a0893b451567b7611fb09961b",
         filename: "appsignal-aarch64-darwin-all-static.tar.gz"
       },
       "arm-darwin" => %{
-        checksum: "8db9e31e090e767b1157d969521967f322be9dd73eb1b677b6192eb4c987af72",
+        checksum: "4318b13bba02f8a98226e9d747a42267f790932a0893b451567b7611fb09961b",
         filename: "appsignal-aarch64-darwin-all-static.tar.gz"
       },
       "aarch64-linux" => %{
-        checksum: "72873d1c7ad2d4d744fe3dd4370fb07b4c9d8a4f4d87febb8dbe08c532eebff8",
+        checksum: "0eecbb8469d87a632a70c3c551e07b3c77e084c41438c9c165f6a439fe9f2b4f",
         filename: "appsignal-aarch64-linux-all-static.tar.gz"
       },
       "i686-linux" => %{
-        checksum: "8779fdd2f02b034463900456b5b65a92d3a9165b87ba896b01baded96729685a",
+        checksum: "75780f506b51be891e85e12e8d51023b82ec8300f8fc293396beeee41f0d357b",
         filename: "appsignal-i686-linux-all-static.tar.gz"
       },
       "x86-linux" => %{
-        checksum: "8779fdd2f02b034463900456b5b65a92d3a9165b87ba896b01baded96729685a",
+        checksum: "75780f506b51be891e85e12e8d51023b82ec8300f8fc293396beeee41f0d357b",
         filename: "appsignal-i686-linux-all-static.tar.gz"
       },
       "x86_64-linux" => %{
-        checksum: "36fc29655d13e4dfe7bcbe2c798bfc16d68194610ff354d43e977f3768f31458",
+        checksum: "6b15d0110ed9c13b6446bebdaa19e41810f0b2cb9da37b7c1a62a4876147de47",
         filename: "appsignal-x86_64-linux-all-static.tar.gz"
       },
       "x86_64-linux-musl" => %{
-        checksum: "59b6cef9797746da9d6717effc0892a2f2219767734a0e76f8b3d1578dc0d9e0",
+        checksum: "d4805021560de3971622a95af0dfb06b3f1c69fab3f8ae1bf3c40f1204a076fb",
         filename: "appsignal-x86_64-linux-musl-all-static.tar.gz"
       },
       "aarch64-linux-musl" => %{
-        checksum: "ec3ab8fcc20d1f31df6003e2ca3dcf257abfeddd1b7912fa6189f1f6905a89ab",
+        checksum: "28881989d5f3d3691f9dd96b04298aa6fcd34292a2d304b251617bfbd05b214e",
         filename: "appsignal-aarch64-linux-musl-all-static.tar.gz"
       },
       "x86_64-freebsd" => %{
-        checksum: "bd654d5c555f6006e4145d76e27f02c5d22b285f411675a520455d6db6c8e165",
+        checksum: "490f61a8b36e31dc21fd9e96975b75428f7d83f0842bb29a02c36f9f3745b07e",
         filename: "appsignal-x86_64-freebsd-all-static.tar.gz"
       },
       "amd64-freebsd" => %{
-        checksum: "bd654d5c555f6006e4145d76e27f02c5d22b285f411675a520455d6db6c8e165",
+        checksum: "490f61a8b36e31dc21fd9e96975b75428f7d83f0842bb29a02c36f9f3745b07e",
         filename: "appsignal-x86_64-freebsd-all-static.tar.gz"
       },
     }
