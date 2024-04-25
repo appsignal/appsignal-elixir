5a7810c81a20961418285b6af8da47694c4b5b16
Tom de Bruijn <tom@tomdebruijn.com>
2024-04-25 09:58:58 +0200

    Fix Phoenix filter_parameters partial keys support
    The Phoenix Logger filters out partial keys matches. Our implementation
    for Phoenix's filter_parameters did not follow this implementation. This
    change adds support for filtering out partial key matches when using the
    discard filtering strategy.
    
    Phoenix implementation:
    https://github.com/phoenixframework/phoenix/blob/dfb0c00d2077e10f8df6cc6e334e04924c4c2bcd/lib/phoenix/logger.ex#L157-L199
    
    Fixes #936

--- test/appsignal/utils/map_filter_test.exs
+++ test/appsignal/utils/map_filter_test.exs
@@ -8,19 +8,97 @@ defmodule Appsignal.Utils.MapFilterTest do
     end
   end
 
-  describe "filter/1, with a blocklist" do
-    test "returns the map as-is, and leaves filtering to the agent" do
-      Application.put_env(:phoenix, :filter_parameters, ~w(name))
-      assert %{id: 4, name: "David"} = MapFilter.filter(%{id: 4, name: "David"})
+  describe "filter/1" do
+    test "reads filter config from Phoenix config by default" do
+      Application.put_env(:phoenix, :filter_parameters, ["password"])
+      values = %{"foo" => "bar", "password" => "should_not_show"}
+
+      assert MapFilter.filter(values) ==
+               %{"foo" => "bar", "password" => "[FILTERED]"}
+
       Application.delete_env(:phoenix, :filter_parameters)
     end
   end
 
-  describe "filter/1, with a keeplist" do
-    test "returns the map as-is, and leaves filtering to the agent" do
-      Application.put_env(:phoenix, :filter_parameters, {:keep, [:name]})
-      assert %{id: "[FILTERED]", name: "David"} = MapFilter.filter(%{id: 4, name: "David"})
-      Application.delete_env(:phoenix, :filter_parameters)
+  describe "filter/2 with discard strategy" do
+    test "in top level map" do
+      values = %{"foo" => "bar", "password" => "should_not_show"}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"foo" => "bar", "password" => "[FILTERED]"}
+    end
+
+    test "discards keys with partial key match" do
+      values = %{"password" => "should_not_show", "my_password" => "should_not_show"}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"password" => "[FILTERED]", "my_password" => "[FILTERED]"}
+    end
+
+    test "when a map has secret key" do
+      values = %{"foo" => "bar", "map" => %{"password" => "should_not_show"}}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"foo" => "bar", "map" => %{"password" => "[FILTERED]"}}
+    end
+
+    test "when a list has a map with secret" do
+      values = %{"foo" => "bar", "list" => [%{"password" => "should_not_show"}]}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"foo" => "bar", "list" => [%{"password" => "[FILTERED]"}]}
+    end
+
+    test "does not filter structs" do
+      values = %{"foo" => "bar", "file" => %Plug.Upload{}}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"foo" => "bar", "file" => %Plug.Upload{}}
+
+      values = %{"foo" => "bar", "file" => %{__struct__: "s"}}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{"foo" => "bar", "file" => %{:__struct__ => "s"}}
+    end
+
+    test "does not fail on atomic keys" do
+      values = %{:foo => "bar", "password" => "should_not_show"}
+
+      assert MapFilter.filter(values, ["password"]) ==
+               %{:foo => "bar", "password" => "[FILTERED]"}
+    end
+  end
+
+  describe "filter/2 with keep strategy" do
+    test "discards values not specified in params" do
+      values = %{"foo" => "bar", "password" => "abc123", "file" => %Plug.Upload{}}
+
+      assert MapFilter.filter(values, {:keep, []}) ==
+               %{"foo" => "[FILTERED]", "password" => "[FILTERED]", "file" => "[FILTERED]"}
+    end
+
+    test "keeps values that are specified in params" do
+      values = %{"foo" => "bar", "password" => "abc123", "file" => %Plug.Upload{}}
+
+      assert MapFilter.filter(values, {:keep, ["foo", "file"]}) ==
+               %{"foo" => "bar", "password" => "[FILTERED]", "file" => %Plug.Upload{}}
+    end
+
+    test "keeps all values under keys that are kept" do
+      values = %{"foo" => %{"bar" => 1, "baz" => 2}}
+
+      assert MapFilter.filter(values, {:keep, ["foo"]}) ==
+               %{"foo" => %{"bar" => 1, "baz" => 2}}
+    end
+
+    test "only filters leaf values" do
+      values = %{"foo" => %{"bar" => 1, "baz" => 2}, "ids" => [1, 2]}
+
+      assert MapFilter.filter(values, {:keep, []}) ==
+               %{
+                 "foo" => %{"bar" => "[FILTERED]", "baz" => "[FILTERED]"},
+                 "ids" => ["[FILTERED]", "[FILTERED]"]
+               }
     end
   end
 end
