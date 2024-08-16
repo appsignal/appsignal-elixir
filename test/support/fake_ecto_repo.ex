3d9634c7a16999f45b8fcb64024f6b29cdaead4d
Noemi <45180344+unflxw@users.noreply.github.com>
2024-08-16 17:55:00 +0200

    Follow `@defoverridable` protocol in Ecto repo
    Our `Appsignal.Ecto.Repo`, which is meant to be a swap-in
    replacement for `Ecto.Repo`, should both call `super` on the
    original `Ecto.Repo` implementation of `default_options` that it
    overrides, and allow `super` to be called on it by modules that
    override `default_options`.

--- test/support/fake_ecto_repo.ex
+++ test/support/fake_ecto_repo.ex
@@ -4,6 +4,11 @@ defmodule FakeEctoRepo do
       def get_received_opts do
         unquote(opts)
       end
+
+      # As implemented in `Ecto.Repo`:
+      # https://github.com/elixir-ecto/ecto/blob/9df9b35044d74322cdd5c263b6d593ba98a19c44/lib/ecto/repo.ex#L260-L261
+      def default_options(_operation), do: []
+      defoverridable default_options: 1
     end
   end
 end
