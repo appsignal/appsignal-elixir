d5e132e0bdba6ae0256d336ce0c087e29c9efb4e
Noemi <45180344+unflxw@users.noreply.github.com>
2024-08-16 17:06:51 +0200

    Prefer the tracer span to the telemetry span
    When processing an Ecto event, if both a tracer span and a telemetry
    span are present, use the tracer span as the parent.
    
    This should fix an issue where, when an Ecto transaction takes place
    inside an Ecto parallel preload, when the transaction's commit or
    rollback event attempts to close both its own span and the
    overarching transaction span (which would be the tracer parent, within
    the parallel preload's sub-process) it instead closes the transaction
    span's parent (which would be the telemetry parent, from the process
    that spawned the parallel preload) leaving the transaction span
    unclosed and potentially cutting the transaction short.

--- test/appsignal/ecto_test.exs
+++ test/appsignal/ecto_test.exs
@@ -68,7 +68,7 @@ defmodule Appsignal.EctoTest do
     end
   end
 
-  describe "handle_event/4 and handle_query/4, with a root span" do
+  describe "handle_event/4 and handle_query/4, with a root span from the tracer" do
     setup do
       start_supervised!(Test.Nif)
       start_supervised!(Test.Tracer)
@@ -188,6 +188,70 @@ defmodule Appsignal.EctoTest do
     test "closes the span with an end time" do
       {:ok, [{_, _, start_time: start_time}]} = Test.Tracer.get(:create_span)
       {:ok, [{%Span{}, end_time: end_time}]} = Test.Tracer.get(:close_span)
+      assert end_time - start_time == 8_829_000
+    end
+  end
+
+  describe "handle_event/4 and handle_commit/4, with a root span from the tracer and a root span passed via telemetry options" do
+    setup do
+      start_supervised!(Test.Nif)
+      start_supervised!(Test.Tracer)
+      start_supervised!(Test.Span)
+      start_supervised!(Test.Monitor)
+
+      telemetry_span =
+        fn -> Appsignal.Tracer.create_span("http_request") end
+        |> Task.async()
+        |> Task.await()
+
+      tracer_span = Appsignal.Tracer.create_span("http_request")
+
+      :telemetry.execute(
+        [:appsignal, :test, :repo, :query],
+        %{
+          decode_time: 2_204_000,
+          query_time: 5_386_000,
+          queue_time: 1_239_000,
+          total_time: 8_829_000
+        },
+        %{
+          params: [],
+          query: "commit",
+          repo: Appsignal.Test.Repo,
+          result: :ok,
+          source: "users",
+          type: :ecto_sql_query
+        }
+      )
+
+      %{telemetry_span: telemetry_span, tracer_span: tracer_span}
+    end
+
+    test "creates a span with the tracer span as the parent and a start time", %{
+      tracer_span: parent
+    } do
+      {:ok, [{"http_request", ^parent, start_time: time}]} = Test.Tracer.get(:create_span)
+      assert is_integer(time)
+    end
+
+    test "sets the span's name" do
+      assert {:ok, [{%Span{}, "Commit Appsignal.Test.Repo"}]} = Test.Span.get(:set_name)
+    end
+
+    test "sets the span's category" do
+      assert attribute("appsignal:category", "commit.ecto")
+    end
+
+    test "does not set the span's body" do
+      assert Test.Span.get(:set_sql) == :error
+    end
+
+    test "closes the span and the parent tracer span with an end time", %{tracer_span: parent} do
+      {:ok, [{_, _, start_time: start_time}]} = Test.Tracer.get(:create_span)
+
+      {:ok, [{^parent, end_time: end_time}, {%Span{}, end_time: end_time}]} =
+        Test.Tracer.get(:close_span)
+
       assert end_time - start_time == 8_829_000
     end
   end
