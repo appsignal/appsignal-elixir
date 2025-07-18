699639d6b88ca73631cb95ad14051d20d722fdee
Tom de Bruijn <tom@tomdebruijn.com>
2025-07-18 18:06:54 +0200

    Add support for Phoenix 1.18's parameter filtering
    In Phoenix 1.18 the parameter filtering now uses compiled patterns.
    Update our implementation with the new Phoenix implementation so we can
    support both Phoenix 1.18 and newer, and older versions.
    We do this by checking if the value in
    `Application.get_env(:phoenix, :filter_parameters, [])` is a compiled
    pattern.
    
    Relevant commit in Phoenix: https://github.com/phoenixframework/phoenix/commit/359539f00cb28b84b43de046d2a1c4a1844400b7

--- lib/appsignal/utils/map_filter.ex
+++ lib/appsignal/utils/map_filter.ex
@@ -2,14 +2,58 @@ defmodule Appsignal.Utils.MapFilter do
   @moduledoc false
   require Logger
 
-  # Phoenix parameter filtering copied from to ensure we support the same filtering options:
-  # https://github.com/phoenixframework/phoenix/blob/dfb0c00d2077e10f8df6cc6e334e04924c4c2bcd/lib/phoenix/logger.ex#L157-L199
+  # Phoenix parameter filtering adapted from the Phoenix Framework.
+  # Copyright (c) 2014 Chris McCord - Licensed under MIT
+  #
+  # Phoenix < 1.18: https://github.com/phoenixframework/phoenix/blob/dfb0c00d2077e10f8df6cc6e334e04924c4c2bcd/lib/phoenix/logger.ex#L157-L199
+  # Phoenix >= 1.18: https://github.com/phoenixframework/phoenix/blob/8a6baa5e2ddc9cf7a2fc797ac907c40389139122/lib/phoenix/logger.ex#L180-L228
 
-  def filter(values, params \\ Application.get_env(:phoenix, :filter_parameters, []))
-  def filter(values, {:discard, params}), do: discard_values(values, params)
-  def filter(values, {:keep, params}), do: keep_values(values, params)
-  def filter(values, params), do: discard_values(values, params)
+  def filter(values, filter \\ Application.get_env(:phoenix, :filter_parameters, [])) do
+    case filter do
+      # Phoenix >= 1.18
+      {:compiled, key_match, value_match} ->
+        discard_values(values, key_match, value_match)
 
+      # Phoenix < 1.18
+      {:discard, params} ->
+        discard_values(values, params)
+
+      {:keep, match} ->
+        keep_values(values, match)
+
+      # Phoenix < 1.18
+      params ->
+        discard_values(values, params)
+    end
+  end
+
+  # Phoenix >= 1.18
+  defp discard_values(%{__struct__: mod} = struct, _key_match, _value_match) when is_atom(mod) do
+    struct
+  end
+
+  defp discard_values(%{} = map, key_match, value_match) do
+    Enum.into(map, %{}, fn {k, v} ->
+      cond do
+        is_binary(k) and String.contains?(k, key_match) ->
+          {k, "[FILTERED]"}
+
+        is_binary(v) and String.contains?(v, value_match) ->
+          {k, "[FILTERED]"}
+
+        true ->
+          {k, discard_values(v, key_match, value_match)}
+      end
+    end)
+  end
+
+  defp discard_values([_ | _] = list, key_match, value_match) do
+    Enum.map(list, &discard_values(&1, key_match, value_match))
+  end
+
+  defp discard_values(other, _key_match, _value_match), do: other
+
+  # Phoenix < 1.18
   defp discard_values(%{__struct__: mod} = struct, _params) when is_atom(mod) do
     struct
   end
@@ -30,21 +74,21 @@ defmodule Appsignal.Utils.MapFilter do
 
   defp discard_values(other, _params), do: other
 
-  defp keep_values(%{__struct__: mod}, _params) when is_atom(mod), do: "[FILTERED]"
+  defp keep_values(%{__struct__: mod}, _match) when is_atom(mod), do: "[FILTERED]"
 
-  defp keep_values(%{} = map, params) do
+  defp keep_values(%{} = map, match) do
     Enum.into(map, %{}, fn {k, v} ->
-      if is_binary(k) and k in params do
-        {k, discard_values(v, [])}
+      if is_binary(k) and k in match do
+        {k, v}
       else
-        {k, keep_values(v, params)}
+        {k, keep_values(v, match)}
       end
     end)
   end
 
-  defp keep_values([_ | _] = list, params) do
-    Enum.map(list, &keep_values(&1, params))
+  defp keep_values([_ | _] = list, match) do
+    Enum.map(list, &keep_values(&1, match))
   end
 
-  defp keep_values(_other, _params), do: "[FILTERED]"
+  defp keep_values(_other, _match), do: "[FILTERED]"
 end
